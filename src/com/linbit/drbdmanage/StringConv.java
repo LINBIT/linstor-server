e51538e8af3a66df9cc0a2379dd07c00d28d6e69
Robert Altnoeder <robert.altnoeder@linbit.com>
2016-11-08 14:52:16 +0100

    Added StringConv class and unit tests

---
+++ src/com/linbit/drbdmanage/StringConv.java
@@ -0,0 +1,206 @@
+package com.linbit.drbdmanage;
+
+public class StringConv
+{
+    public static final String BOOLEAN_TRUE = Boolean.toString(true);
+    public static final String BOOLEAN_FALSE = Boolean.toString(false);
+
+    /**
+     * Returns the boolean value represented by the String argument text or the
+     * specified default value.
+     *
+     * If text contains a valid representation of a boolean value, the boolean value
+     * represented by text is returned. Otherwise, the default value specified as
+     * dfltValue is returned.
+     *
+     * @param text String representation of a boolean value; may be null
+     * @param dfltValue Default result if text does not represent a boolean value
+     * @return The boolean value represented by text, if text is a valid representation
+     *     of a boolean value; otherwise the default value dfltValue
+     */
+    public static boolean getDfltBoolean(String text, boolean dfltValue)
+    {
+        boolean result = dfltValue;
+        if (text != null)
+        {
+            if (text.equalsIgnoreCase(BOOLEAN_TRUE))
+            {
+                result = true;
+            }
+            else
+            if (text.equalsIgnoreCase(BOOLEAN_FALSE))
+            {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns the byte value represented by the String argument text or the
+     * specified default value.
+     *
+     * If text contains a valid representation of a byte value, that value
+     * is returned. Otherwise, the value specified as dfltValue is returned.
+     *
+     * @param text String representation of a byte value
+     * @param dfltValue Default result if text is not a valid representation of a byte value
+     * @return The byte value represented by text, if text is a valid representation of a
+     *     byte value, otherwise the default value dfltValue
+     */
+    public static byte getDfltByte(String text, byte dfltValue)
+    {
+        byte value = dfltValue;
+        if (text != null)
+        {
+            try
+            {
+                value = Byte.parseByte(text);
+            }
+            catch (NumberFormatException ignored)
+            {
+            }
+        }
+        return value;
+    }
+
+    /**
+     * Returns the short value represented by the String argument text or the
+     * specified default value.
+     *
+     * If text contains a valid representation of a short value, that value
+     * is returned. Otherwise, the value specified as dfltValue is returned.
+     *
+     * @param text String representation of a short value
+     * @param dfltValue Default result if text is not a valid representation of a short value
+     * @return The short value represented by text, if text is a valid representation of a
+     *     short value, otherwise the default value dfltValue
+     */
+    public static short getDfltShort(String text, short dfltValue)
+    {
+        short value = dfltValue;
+        if (text != null)
+        {
+            try
+            {
+                value = Short.parseShort(text);
+            }
+            catch (NumberFormatException ignored)
+            {
+            }
+        }
+        return value;
+    }
+
+    /**
+     * Returns the int value represented by the String argument text or the
+     * specified default value.
+     *
+     * If text contains a valid representation of a int value, that value
+     * is returned. Otherwise, the value specified as dfltValue is returned.
+     *
+     * @param text String representation of a int value
+     * @param dfltValue Default result if text is not a valid representation of a int value
+     * @return The int value represented by text, if text is a valid representation of a
+     *     int value, otherwise the default value dfltValue
+     */
+    public static int getDfltInt(String text, int dfltValue)
+    {
+        int value = dfltValue;
+        if (text != null)
+        {
+            try
+            {
+                value = Integer.parseInt(text);
+            }
+            catch (NumberFormatException ignored)
+            {
+            }
+        }
+        return value;
+    }
+
+    /**
+     * Returns the long value represented by the String argument text or the
+     * specified default value.
+     *
+     * If text contains a valid representation of a long value, that value
+     * is returned. Otherwise, the value specified as dfltValue is returned.
+     *
+     * @param text String representation of a long value
+     * @param dfltValue Default result if text is not a valid representation of a long value
+     * @return The long value represented by text, if text is a valid representation of a
+     *     long value, otherwise the default value dfltValue
+     */
+    public static long getDfltLong(String text, long dfltValue)
+    {
+        long value = dfltValue;
+        if (text != null)
+        {
+            try
+            {
+                value = Long.parseLong(text);
+            }
+            catch (NumberFormatException ignored)
+            {
+            }
+        }
+        return value;
+    }
+
+    /**
+     * Returns the float value represented by the String argument text or the
+     * specified default value.
+     *
+     * If text contains a valid representation of a float value, that value
+     * is returned. Otherwise, the value specified as dfltValue is returned.
+     *
+     * @param text String representation of a float value
+     * @param dfltValue Default result if text is not a valid representation of a float value
+     * @return The float value represented by text, if text is a valid representation of a
+     *     float value, otherwise the default value dfltValue
+     */
+    public static float getDfltFloat(String text, float dfltValue)
+    {
+        float value = dfltValue;
+        if (text != null)
+        {
+            try
+            {
+                value = Float.parseFloat(text);
+            }
+            catch (NumberFormatException ignored)
+            {
+            }
+        }
+        return value;
+    }
+
+    /**
+     * Returns the double value represented by the String argument text or the
+     * specified default value.
+     *
+     * If text contains a valid representation of a double value, that value
+     * is returned. Otherwise, the value specified as dfltValue is returned.
+     *
+     * @param text String representation of a double value
+     * @param dfltValue Default result if text is not a valid representation of a double value
+     * @return The double value represented by text, if text is a valid representation of a
+     *     double value, otherwise the default value dfltValue
+     */
+    public static double getDfltDouble(String text, double dfltValue)
+    {
+        double value = dfltValue;
+        if (text != null)
+        {
+            try
+            {
+                value = Double.parseDouble(text);
+            }
+            catch (NumberFormatException ignored)
+            {
+            }
+        }
+        return value;
+    }
+}
---
+++ test/com/linbit/drbdmanage/StringConvTest.java
@@ -0,0 +1,331 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.linbit.drbdmanage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author raltnoeder
+ */
+public class StringConvTest
+{
+
+    public StringConvTest()
+    {
+    }
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of getDfltBoolean method, of class StringConv.
+     */
+    @Test
+    public void testGetDfltBoolean() throws Exception
+    {
+        if (StringConv.getDfltBoolean(Boolean.toString(true), false) != true)
+        {
+            fail("Failed to parse Boolean.toString(true) value");
+        }
+
+        if (StringConv.getDfltBoolean(Boolean.toString(false), true) != false)
+        {
+            fail("Failed to parse Boolean.toString(false) value");
+        }
+
+        if (StringConv.getDfltBoolean("TRUE", false) != true)
+        {
+            fail("Failed to parse \"TRUE\"");
+        }
+
+        if (StringConv.getDfltBoolean("FALSE", true) != false)
+        {
+            fail("Failed to parse \"FALSE\"");
+        }
+
+        if (StringConv.getDfltBoolean("true", false) != true)
+        {
+            fail("Failed to parse \"true\"");
+        }
+
+        if (StringConv.getDfltBoolean("false", true) != false)
+        {
+            fail("Failed to parse \"false\"");
+        }
+
+        if (StringConv.getDfltBoolean("True", false) != true)
+        {
+            fail("Failed to parse \"True\"");
+        }
+
+        if (StringConv.getDfltBoolean("False", true) != false)
+        {
+            fail("Failed to parse \"False\"");
+        }
+
+        if (StringConv.getDfltBoolean("tRuE", false) != true)
+        {
+            fail("Failed to parse \"tRuE\"");
+        }
+
+        if (StringConv.getDfltBoolean("FaLSe", true) != false)
+        {
+            fail("Failed to parse \"FaLSe\"");
+        }
+
+        if (StringConv.getDfltBoolean(null, true) != true)
+        {
+            fail("Failed to return true result for null input value");
+        }
+        if (StringConv.getDfltBoolean(null, false) != false)
+        {
+            fail("Failed to return false result for null input value");
+        }
+
+        if (StringConv.getDfltBoolean("There is an orange rubber duck in the treasure chest!!", true) != true)
+        {
+            fail("Failed to return true result for invalid input value");
+        }
+
+        if (StringConv.getDfltBoolean("There is an orange rubber duck in the treasure chest!!", false) != false)
+        {
+            fail("Failed to return false result for invalid input value");
+        }
+    }
+
+    /**
+     * Test of getDfltByte method, of class StringConv.
+     */
+    @Test
+    public void testGetDfltByte() throws Exception
+    {
+        if (StringConv.getDfltByte(Byte.toString((byte) 42), (byte) 0) != (byte) 42)
+        {
+            fail("Failed to parse Byte.toString((byte) 42)");
+        }
+
+        if (StringConv.getDfltByte(Byte.toString((byte) -99), (byte) 0) != (byte) -99)
+        {
+            fail("Failed to parse Byte.toString((byte) -99)");
+        }
+
+        if (StringConv.getDfltByte("+-41", (byte) 123) != (byte) 123)
+        {
+            fail("Failed to return default value for invalid input \"+-41\"");
+        }
+
+        if (StringConv.getDfltByte("-431", (byte) 65) != (byte) 65)
+        {
+            fail("Failed to return default value for invalid input \"-431\"");
+        }
+
+        if (StringConv.getDfltByte("", (byte) -76) != (byte) -76)
+        {
+            fail("Failed to return default value for empty input");
+        }
+
+        if (StringConv.getDfltByte(null, (byte) 0) != (byte) 0)
+        {
+            fail("Failed to return default value for null input value");
+        }
+    }
+
+    /**
+     * Test of getDfltShort method, of class StringConv.
+     */
+    @Test
+    public void testGetDfltShort() throws Exception
+    {
+        if (StringConv.getDfltShort(Short.toString((short) 31337), (short) 0) != (short) 31337)
+        {
+            fail("Failed to parse Short.toString((short) 31337)");
+        }
+
+        if (StringConv.getDfltShort(Short.toString((short) -1337), (short) 0) != (short) -1337)
+        {
+            fail("Failed to parse Short.toString((short) -1337)");
+        }
+
+        if (StringConv.getDfltShort("blubb", (short) 137) != (short) 137)
+        {
+            fail("Failed to return default value for invalid input \"blubb\"");
+        }
+
+        if (StringConv.getDfltShort("69111", (short) 21) != (short) 21)
+        {
+            fail("Failed to return default value for invalid input \"69111\"");
+        }
+
+        if (StringConv.getDfltShort("", (short) 101) != (short) 101)
+        {
+            fail("Failed to return default value for empty input");
+        }
+
+        if (StringConv.getDfltShort(null, (short) 101) != (short) 101)
+        {
+            fail("Failed to return default value for null input value");
+        }
+    }
+
+    /**
+     * Test of getDfltInt method, of class StringConv.
+     */
+    @Test
+    public void testGetDfltInt() throws Exception
+    {
+        if (StringConv.getDfltInt(Integer.toString(19191), 0) != 19191)
+        {
+            fail("Failed to parse Integer.toString(19191)");
+        }
+
+        if (StringConv.getDfltInt(Integer.toString(-72727), 0) != -72727)
+        {
+            fail("Failed to parse Integer.toString(-72727)");
+        }
+
+        if (StringConv.getDfltInt(" ++1", Integer.MIN_VALUE) != Integer.MIN_VALUE)
+        {
+            fail("Failed to return default value for invalid input \" ++1\"");
+        }
+
+        if (StringConv.getDfltInt("onehundredfortyseven", -1) != -1)
+        {
+            fail("Failed to return default value for invalid input \"onehundredfortyseven\"");
+        }
+
+        if (StringConv.getDfltInt("", 1870645533) != 1870645533)
+        {
+            fail("Failed to return default value for empty input");
+        }
+
+        if (StringConv.getDfltInt(null, 931777212) != 931777212)
+        {
+            fail("Failed to return default value for null input value");
+        }
+    }
+
+    /**
+     * Test of getDfltLong method, of class StringConv.
+     */
+    @Test
+    public void testGetDfltLong() throws Exception
+    {
+        if (StringConv.getDfltLong(Long.toString(100), 0L) != 100L)
+        {
+            fail("Failed to parse Long.toString(100L)");
+        }
+
+        if (StringConv.getDfltLong(Long.toString(-1), 0L) != -1L)
+        {
+            fail("Failed to parse Long.toString(-1L)");
+        }
+
+        if (StringConv.getDfltLong("--1", 0L) != 0L)
+        {
+            fail("Failed to return default value for invalid input \"--1\"");
+        }
+
+        if (StringConv.getDfltLong("three", -70L) != -70L)
+        {
+            fail("Failed to return default value for invalid input \"three\"");
+        }
+
+        if (StringConv.getDfltLong("", 7235728385971823L) != 7235728385971823L)
+        {
+            fail("Failed to return default value for empty input");
+        }
+
+        if (StringConv.getDfltLong(null, -562938497857234L) != -562938497857234L)
+        {
+            fail("Failed to return default value for null input value");
+        }
+    }
+
+    /**
+     * Test of getDfltFloat method, of class StringConv.
+     */
+    @Test
+    public void testGetDfltFloat() throws Exception
+    {
+        if (StringConv.getDfltFloat(Float.toString((float) 37.1), 0) != (float) 37.1)
+        {
+            fail("Failed to parse Float.toString((float) 37.1)");
+        }
+
+        if (StringConv.getDfltFloat(Float.toString((float) -1.37), 0) != (float) -1.37)
+        {
+            fail("Failed to parse Float.toString((float) -1.37)");
+        }
+
+        if (StringConv.getDfltFloat("--1", (float) 2) != (float) 2)
+        {
+            fail("Failed to return default value for invalid input \"--1\"");
+        }
+
+        if (StringConv.getDfltFloat("three", (float) -1455.5) != (float) -1455.5)
+        {
+            fail("Failed to return default value for invalid input \"three\"");
+        }
+
+        if (StringConv.getDfltFloat("", 0) != 0)
+        {
+            fail("Failed to return default value for empty input");
+        }
+
+        if (StringConv.getDfltFloat(null, (float) 0.1) != (float) 0.1)
+        {
+            fail("Failed to return default value for null input value");
+        }
+    }
+
+    /**
+     * Test of getDfltDouble method, of class StringConv.
+     */
+    @Test
+    public void testGetDfltDouble() throws Exception
+    {
+        if (StringConv.getDfltDouble(Double.toString(43.3), Double.MIN_NORMAL) != 43.3)
+        {
+            fail("Failed to parse Double.toString(43.3)");
+        }
+
+        if (StringConv.getDfltDouble(Double.toString(-65535.1), Double.MIN_NORMAL) != -65535.1)
+        {
+            fail("Failed to parse Double.toString(43.3)");
+        }
+
+        if (StringConv.getDfltDouble("Qapla'", -873.346) != -873.346)
+        {
+            fail("Failed to return default value for invalid input \"Qapla'\"");
+        }
+
+        if (StringConv.getDfltDouble("-+1 ", -65535.1) != -65535.1)
+        {
+            fail("Failed to return default value for invalid input \"-+1 \"");
+        }
+
+        if (StringConv.getDfltDouble("", 893) != 893)
+        {
+            fail("Failed to return default value for empty input");
+        }
+
+        if (StringConv.getDfltDouble(null, 713764.565) != 713764.565)
+        {
+            fail("Failed to return default value for null input value");
+        }
+    }
+}
