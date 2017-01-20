567b577cbfb7164dc1cd3980afde1158f29f51eb
Robert Altnoeder <robert.altnoeder@linbit.com>
2017-01-20 16:00:16 +0100

    Added size conversion methods
    - added SizeConv class
    - added Test for SizeConv methods

---
+++ src/com/linbit/SizeConv.java
@@ -0,0 +1,242 @@
+package com.linbit;
+
+import java.math.BigInteger;
+
+/**
+ * Methods for converting decimal and binary sizes of different magnitudes
+ *
+ * @author Robert Altnoeder &lt;robert.altnoeder@linbit.com&gt;
+ */
+public class SizeConv
+{
+    public static final BigInteger one = BigInteger.valueOf(1L);
+
+    public enum SizeUnit
+    {
+        UNIT_B,
+        UNIT_kiB,
+        UNIT_MiB,
+        UNIT_GiB,
+        UNIT_TiB,
+        UNIT_PiB,
+        UNIT_EiB,
+        UNIT_ZiB,
+        UNIT_YiB,
+        UNIT_kB,
+        UNIT_MB,
+        UNIT_GB,
+        UNIT_TB,
+        UNIT_PB,
+        UNIT_EB,
+        UNIT_ZB,
+        UNIT_YB;
+
+        protected BigInteger getFactor()
+        {
+            BigInteger factor = FACTOR_B;
+            switch (this)
+            {
+                case UNIT_B:
+                    factor = FACTOR_B;
+                    break;
+                case UNIT_kiB:
+                    factor = FACTOR_kiB;
+                    break;
+                case UNIT_MiB:
+                    factor = FACTOR_MiB;
+                    break;
+                case UNIT_GiB:
+                    factor = FACTOR_GiB;
+                    break;
+                case UNIT_TiB:
+                    factor = FACTOR_TiB;
+                    break;
+                case UNIT_PiB:
+                    factor = FACTOR_PiB;
+                    break;
+                case UNIT_EiB:
+                    factor = FACTOR_EiB;
+                    break;
+                case UNIT_ZiB:
+                    factor = FACTOR_ZiB;
+                    break;
+                case UNIT_YiB:
+                    factor = FACTOR_YiB;
+                    break;
+                case UNIT_kB:
+                    factor = FACTOR_kB;
+                    break;
+                case UNIT_MB:
+                    factor = FACTOR_MB;
+                    break;
+                case UNIT_GB:
+                    factor = FACTOR_GB;
+                    break;
+                case UNIT_TB:
+                    factor = FACTOR_TB;
+                    break;
+                case UNIT_PB:
+                    factor = FACTOR_PB;
+                    break;
+                case UNIT_EB:
+                    factor = FACTOR_EB;
+                    break;
+                case UNIT_ZB:
+                    factor = FACTOR_ZB;
+                    break;
+                case UNIT_YB:
+                    factor = FACTOR_YB;
+                    break;
+                default:
+                    throw new ImplementationError(
+                        String.format(
+                            "Missing case label for enum member %s",
+                            this.name()
+                        ),
+                        null
+                    );
+            }
+            return factor;
+        }
+    }
+
+    // Factor 1
+    public static final BigInteger FACTOR_B   = new BigInteger(
+        new byte[] { 1 }
+    );
+
+    // Factor 1,024
+    public static final BigInteger FACTOR_kiB = new BigInteger(
+        new byte[] { 0x04, 0x00 }
+    );
+
+    // Factor 1,048,576
+    public static final BigInteger FACTOR_MiB = new BigInteger(
+        new byte[] { 0x10, 0x00, 0x00 }
+    );
+
+    // Factor 1,073,741,824
+    public static final BigInteger FACTOR_GiB = new BigInteger(
+        new byte[] { 0x40, 0x00, 0x00, 0x00 }
+    );
+
+    // Factor 1,099,511,627,776
+    public static final BigInteger FACTOR_TiB = new BigInteger(
+        new byte[] { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00 }
+    );
+
+    // Factor 1,125,899,906,842,624
+    public static final BigInteger FACTOR_PiB = new BigInteger(
+        new byte[] { 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+    );
+
+    // Factor 1,152,921,504,606,846,976
+    public static final BigInteger FACTOR_EiB = new BigInteger(
+        new byte[] { 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+    );
+
+    // Factor 1,180,591,620,717,411,303,424
+    public static final BigInteger FACTOR_ZiB = new BigInteger(
+        new byte[] { 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+    );
+
+    // Factor 1,208,925,819,614,629,174,706,176
+    public static final BigInteger FACTOR_YiB = new BigInteger(
+        new byte[] { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+    );
+
+    // Factor 1,000
+    public static final BigInteger FACTOR_kB = new BigInteger(
+        new byte[] { 0x03, (byte) 0xE8 }
+    );
+
+    // Factor 1,000,000
+    public static final BigInteger FACTOR_MB = new BigInteger(
+        new byte[] { 0x0F, 0x42, 0x40 }
+    );
+
+    // Factor 1,000,000,000
+    public static final BigInteger FACTOR_GB = new BigInteger(
+        new byte[] { 0x3B, (byte) 0x9A, (byte) 0xCA, 0x00 }
+    );
+
+    // Factor 1,000,000,000,000
+    public static final BigInteger FACTOR_TB = new BigInteger(
+        new byte[]
+        {
+            0x00, (byte) 0xE8, (byte) 0xD4, (byte) 0xA5, 0x10, 0x00
+        }
+    );
+
+    // Factor 1,000,000,000,000,000
+    public static final BigInteger FACTOR_PB = new BigInteger(
+        new byte[]
+        {
+            0x03, (byte) 0x8D, 0x7E, (byte) 0xA4,
+            (byte) 0xC6, (byte) 0x80, 0x00
+        }
+    );
+
+    // Factor 1,000,000,000,000,000,000
+    public static final BigInteger FACTOR_EB = new BigInteger(
+        new byte[]
+        {
+            0x0D, (byte) 0xE0, (byte) 0xB6, (byte) 0xB3,
+            (byte) 0xA7, 0x64, 0x00, 0x00
+        }
+    );
+
+    // Factor 1,000,000,000,000,000,000,000
+    public static final BigInteger FACTOR_ZB = new BigInteger(
+        new byte[]
+        {
+            0x36, 0x35, (byte) 0xC9, (byte) 0xAD,
+            (byte) 0xC5, (byte) 0xDE, (byte) 0xA0,
+            0x00, 0x00
+        }
+    );
+
+    // Factor 1,000,000,000,000,000,000,000,000
+    public static final BigInteger FACTOR_YB = new BigInteger(
+        new byte[]
+        {
+            0x00, (byte) 0xD3, (byte) 0xC2, 0x1B,
+            (byte) 0xCE, (byte) 0xCC, (byte) 0xED, (byte) 0xA1,
+            0x00, 0x00, 0x00
+        }
+    );
+
+    public static long convert(long size, SizeUnit unitIn, SizeUnit unitOut)
+    {
+        return convert(BigInteger.valueOf(size), unitIn, unitOut).longValueExact();
+    }
+
+    public static long convertRoundUp(long size, SizeUnit unitIn, SizeUnit unitOut)
+    {
+        return convertRoundUp(BigInteger.valueOf(size), unitIn, unitOut).longValueExact();
+    }
+
+    public static BigInteger convert(BigInteger size, SizeUnit unitIn, SizeUnit unitOut)
+    {
+        BigInteger factorIn = unitIn.getFactor();
+        BigInteger divisorOut = unitOut.getFactor();
+        BigInteger result = size.multiply(factorIn);
+        result = result.divide(divisorOut);
+        return result;
+    }
+
+    public static BigInteger convertRoundUp(BigInteger size, SizeUnit unitIn, SizeUnit unitOut)
+    {
+        BigInteger factorIn = unitIn.getFactor();
+        BigInteger divisorOut = unitOut.getFactor();
+        BigInteger result = size.multiply(factorIn);
+        BigInteger[] quotients = result.divideAndRemainder(divisorOut);
+        result = quotients[0];
+        // Quick check for remainder > 0; if any bits are set, the remainder is not zero
+        if (quotients[1].bitLength() > 0)
+        {
+            result = result.add(one);
+        }
+        return result;
+    }
+}
---
+++ test/com/linbit/SizeConvTest.java
@@ -0,0 +1,238 @@
+package com.linbit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import static com.linbit.SizeConv.SizeUnit.UNIT_B;
+import static com.linbit.SizeConv.SizeUnit.UNIT_kiB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_MiB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_GiB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_TiB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_PiB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_EiB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_ZiB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_YiB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_kB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_MB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_GB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_TB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_PB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_EB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_ZB;
+import static com.linbit.SizeConv.SizeUnit.UNIT_YB;
+
+/**
+ * Tests for size conversion algorithms
+ *
+ * TODO: Tests for rounded conversion (using SizeConv.convertRoundUp())
+ *
+ * @author Robert Altnoeder &lt;robert.altnoeder@linbit.com&gt;
+ */
+public class SizeConvTest
+{
+    static final TestEntry[] TEST_DATA =
+    {
+        new TestEntry(1L, UNIT_B, UNIT_B, 1L, false),
+        new TestEntry(1L, UNIT_kB, UNIT_kB, 1L, false),
+        new TestEntry(1L, UNIT_MB, UNIT_MB, 1L, false),
+        new TestEntry(1L, UNIT_GB, UNIT_GB, 1L, false),
+        new TestEntry(1L, UNIT_TB, UNIT_TB, 1L, false),
+        new TestEntry(1L, UNIT_PB, UNIT_PB, 1L, false),
+        new TestEntry(1L, UNIT_EB, UNIT_EB, 1L, false),
+        new TestEntry(1L, UNIT_ZB, UNIT_ZB, 1L, false),
+        new TestEntry(1L, UNIT_YB, UNIT_YB, 1L, false),
+        new TestEntry(1L, UNIT_kiB, UNIT_kiB, 1L, false),
+        new TestEntry(1L, UNIT_MiB, UNIT_MiB, 1L, false),
+        new TestEntry(1L, UNIT_GiB, UNIT_GiB, 1L, false),
+        new TestEntry(1L, UNIT_TiB, UNIT_TiB, 1L, false),
+        new TestEntry(1L, UNIT_PiB, UNIT_PiB, 1L, false),
+        new TestEntry(1L, UNIT_EiB, UNIT_EiB, 1L, false),
+        new TestEntry(1L, UNIT_ZiB, UNIT_ZiB, 1L, false),
+        new TestEntry(1L, UNIT_YiB, UNIT_YiB, 1L, false),
+        new TestEntry(1L, UNIT_kB, UNIT_B, 1000L, false),
+        new TestEntry(1L, UNIT_MB, UNIT_B, 1000000L, false),
+        new TestEntry(1L, UNIT_GB, UNIT_B, 1000000000L, false),
+        new TestEntry(1L, UNIT_TB, UNIT_B, 1000000000000L, false),
+        new TestEntry(1L, UNIT_PB, UNIT_B, 1000000000000000L, false),
+        new TestEntry(1L, UNIT_EB, UNIT_B, 1000000000000000000L, false),
+        new TestEntry(1L, UNIT_ZB, UNIT_kB, 1000000000000000000L, false),
+        new TestEntry(1L, UNIT_YB, UNIT_MB, 1000000000000000000L, false),
+        new TestEntry(1L, UNIT_kiB, UNIT_B, 1024L, false),
+        new TestEntry(1L, UNIT_MiB, UNIT_B, 1048576L, false),
+        new TestEntry(1L, UNIT_GiB, UNIT_B, 1073741824L, false),
+        new TestEntry(1L, UNIT_TiB, UNIT_B, 1099511627776L, false),
+        new TestEntry(1L, UNIT_PiB, UNIT_B, 1125899906842624L, false),
+        new TestEntry(1L, UNIT_EiB, UNIT_B, 1152921504606846976L, false),
+        new TestEntry(1L, UNIT_ZiB, UNIT_kiB, 1152921504606846976L, false),
+        new TestEntry(1L, UNIT_YiB, UNIT_MiB, 1152921504606846976L, false),
+        new TestEntry(1L, UNIT_MiB, UNIT_B, 1048576L, false),
+        new TestEntry(1L, UNIT_MB, UNIT_kiB, 976L, true),
+        new TestEntry(262144L, UNIT_kiB, UNIT_MiB, 256L, false),
+        new TestEntry(63L, UNIT_GiB, UNIT_kiB, 66060288L, false),
+        new TestEntry(40516976640L, UNIT_MiB, UNIT_TiB, 38640L, false),
+        new TestEntry(48510, UNIT_GB, UNIT_kiB, 47373046875L, false),
+        new TestEntry(1023, UNIT_kiB, UNIT_MiB, 0, true),
+        new TestEntry(2047, UNIT_kiB, UNIT_MiB, 1, true),
+        new TestEntry(58273485671283L, UNIT_B, UNIT_kiB, 56907700850L, true),
+        new TestEntry(58273485671283L, UNIT_B, UNIT_MiB, 55573926L, true),
+        new TestEntry(58273485671283L, UNIT_B, UNIT_GiB, 54271L, true),
+        new TestEntry(58273485671283L, UNIT_B, UNIT_TiB, 52L, true),
+        new TestEntry(58273485671283L, UNIT_B, UNIT_kB, 58273485671L, true),
+        new TestEntry(58273485671283L, UNIT_B, UNIT_MB, 58273485L, true),
+        new TestEntry(58273485671283L, UNIT_B, UNIT_GB, 58273L, true),
+        new TestEntry(58273485671283L, UNIT_B, UNIT_TB, 58L, true),
+        new TestEntry(13454L, UNIT_PiB, UNIT_TiB, 13776896L, false),
+        new TestEntry(13454L, UNIT_EiB, UNIT_TiB, 14107541504L, false),
+        new TestEntry(13454L, UNIT_ZiB, UNIT_TiB, 14446122500096L, false),
+        new TestEntry(13454L, UNIT_YiB, UNIT_TiB, 14792829440098304L, false),
+        new TestEntry(13454L, UNIT_PiB, UNIT_TB, 15147857L, true),
+        new TestEntry(13454L, UNIT_EiB, UNIT_TB, 15511405922L, true),
+        new TestEntry(13454L, UNIT_ZiB, UNIT_TB, 15883679665132L, true),
+        new TestEntry(13454L, UNIT_YiB, UNIT_TB, 16264887977095220L, true),
+        new TestEntry(17641L, UNIT_PB, UNIT_TB, 17641000L, false),
+        new TestEntry(17641L, UNIT_EB, UNIT_TB, 17641000000L, false),
+        new TestEntry(17641L, UNIT_ZB, UNIT_TB, 17641000000000L, false),
+        new TestEntry(17641L, UNIT_YB, UNIT_TB, 17641000000000000L, false),
+        new TestEntry(17641L, UNIT_PB, UNIT_TiB, 16044396L, true),
+        new TestEntry(17641L, UNIT_EB, UNIT_TiB, 16044396033L, true),
+        new TestEntry(17641L, UNIT_ZB, UNIT_TiB, 16044396033976L, true),
+        new TestEntry(17641L, UNIT_YB, UNIT_TiB, 16044396033976227L, true),
+        new TestEntry(53157, UNIT_YiB, UNIT_TB, 64262869793254843L, true)
+    };
+
+    public SizeConvTest()
+    {
+    }
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of convert method, of class SizeConv.
+     */
+    @Test
+    public void testConvert()
+    {
+        for (int index = 0; index < TEST_DATA.length; ++index)
+        {
+            TestEntry entry = TEST_DATA[index];
+            try
+            {
+                long result = SizeConv.convert(entry.size, entry.unitIn, entry.unitOut);
+                if (result != entry.expectedResult)
+                {
+                    printConstants();
+                    fail(
+                        String.format(
+                            "Test %d failed, input size(%d), unitIn(%s), unitOut(%s), " +
+                            "expectedResult(%d), calculation result(%d)",
+                            index, entry.size, entry.unitIn, entry.unitOut, entry.expectedResult, result
+                        )
+                    );
+                }
+            }
+            catch (ArithmeticException arithExc)
+            {
+                printConstants();
+                System.err.printf(
+                    "Test %d failed, input size(%d), unitIn(%s), unitOut(%s), expectedResult(%d)",
+                    index, entry.size, entry.unitIn, entry.unitOut, entry.expectedResult
+                );
+                throw arithExc;
+            }
+        }
+    }
+
+    /**
+     * Test of convert method, of class SizeConv.
+     */
+    @Test
+    public void testConvertRoundUp()
+    {
+        for (int index = 0; index < TEST_DATA.length; ++index)
+        {
+            TestEntry entry = TEST_DATA[index];
+            try
+            {
+                long result = SizeConv.convertRoundUp(entry.size, entry.unitIn, entry.unitOut);
+                long expectedResult = entry.expectedRounded ? entry.expectedResult + 1 : entry.expectedResult;
+                if (result != expectedResult)
+                {
+                    printConstants();
+                    fail(
+                        String.format(
+                            "Test %d failed, input size(%d), unitIn(%s), unitOut(%s), " +
+                            "expectedResult(%d), calculation result(%d)",
+                            index, entry.size, entry.unitIn, entry.unitOut, entry.expectedResult, result
+                        )
+                    );
+                }
+            }
+            catch (ArithmeticException arithExc)
+            {
+                printConstants();
+                System.err.printf(
+                    "Test %d failed, input size(%d), unitIn(%s), unitOut(%s), expectedResult(%d)",
+                    index, entry.size, entry.unitIn, entry.unitOut, entry.expectedResult
+                );
+                throw arithExc;
+            }
+        }
+    }
+
+    public static void printConstants()
+    {
+        System.out.println("== Begin: Constants dump ==");
+        System.out.println("FACTOR_B = " + SizeConv.FACTOR_B);
+        System.out.println("FACTOR_kB = " + SizeConv.FACTOR_kB);
+        System.out.println("FACTOR_MB = " + SizeConv.FACTOR_MB);
+        System.out.println("FACTOR_GB = " + SizeConv.FACTOR_GB);
+        System.out.println("FACTOR_TB = " + SizeConv.FACTOR_TB);
+        System.out.println("FACTOR_PB = " + SizeConv.FACTOR_PB);
+        System.out.println("FACTOR_EB = " + SizeConv.FACTOR_EB);
+        System.out.println("FACTOR_ZB = " + SizeConv.FACTOR_ZB);
+        System.out.println("FACTOR_YB = " + SizeConv.FACTOR_YB);
+        System.out.println("FACTOR_kiB = " + SizeConv.FACTOR_kiB);
+        System.out.println("FACTOR_MiB = " + SizeConv.FACTOR_MiB);
+        System.out.println("FACTOR_GiB = " + SizeConv.FACTOR_GiB);
+        System.out.println("FACTOR_TiB = " + SizeConv.FACTOR_TiB);
+        System.out.println("FACTOR_PiB = " + SizeConv.FACTOR_PiB);
+        System.out.println("FACTOR_EiB = " + SizeConv.FACTOR_EiB);
+        System.out.println("FACTOR_ZiB = " + SizeConv.FACTOR_ZiB);
+        System.out.println("FACTOR_YiB = " + SizeConv.FACTOR_YiB);
+        System.out.println("== End: Constants dump ==\n");
+    }
+
+    static class TestEntry
+    {
+        TestEntry(
+            long size,
+            SizeConv.SizeUnit unitIn,
+            SizeConv.SizeUnit unitOut,
+            long expectedResult,
+            boolean expectedRounded
+        )
+        {
+            this.size = size;
+            this.unitIn = unitIn;
+            this.unitOut = unitOut;
+            this.expectedResult = expectedResult;
+            this.expectedRounded = expectedRounded;
+        }
+
+        long size;
+        SizeConv.SizeUnit unitIn;
+        SizeConv.SizeUnit unitOut;
+        long expectedResult;
+        boolean expectedRounded;
+    }
+}
