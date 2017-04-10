88422d50f6993a6b7312637294f75b7d7b4f0047
Robert Altnoeder <robert.altnoeder@linbit.com>
2017-04-10 11:23:32 +0200

    Added StorageDriverName, NetInterfaceName classes

---
+++ src/com/linbit/drbdmanage/NetInterfaceName.java
@@ -0,0 +1,25 @@
+package com.linbit.drbdmanage;
+
+import com.linbit.Checks;
+import com.linbit.GenericName;
+import com.linbit.InvalidNameException;
+
+/**
+ * Name of a network interface
+ *
+ * @author Robert Altnoeder &lt;robert.altnoeder@linbit.com&gt;
+ */
+public class NetInterfaceName extends GenericName
+{
+    public static final int MIN_LENGTH = 3;
+    public static final int MAX_LENGTH = 32;
+
+    public static final byte[] VALID_CHARS = { '_' };
+    public static final byte[] VALID_INNER_CHARS = { '-' };
+
+    public NetInterfaceName(String niName) throws InvalidNameException
+    {
+        super(niName);
+        Checks.nameCheck(niName, MIN_LENGTH, MAX_LENGTH, VALID_CHARS, VALID_INNER_CHARS);
+    }
+}
