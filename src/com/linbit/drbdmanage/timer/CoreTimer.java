4ce5058839e4afca74252c76f0a008af18ab8b42
Robert Altnoeder <robert.altnoeder@linbit.com>
2017-04-19 10:20:13 +0200

    Added CoreTimer, CoreTimerAction interfaces
    The timer components interfaces and implementations in
    com.linbit.timer are generic. These interfaces specify the
    Action type (and the type of the Action instances unique
    identifiers) for the core timer services.

---
+++ src/com/linbit/drbdmanage/timer/CoreTimer.java
@@ -0,0 +1,24 @@
+package com.linbit.drbdmanage.timer;
+
+import com.linbit.NegativeTimeException;
+import com.linbit.ValueOutOfRangeException;
+import com.linbit.timer.Action;
+import com.linbit.timer.Timer;
+
+/**
+ * drbdmanageNG core timer service interface
+ *
+ * @author Robert Altnoeder &lt;robert.altnoeder@linbit.com&gt;
+ */
+public interface CoreTimer extends Timer<String, Action<String>>
+{
+    @Override
+    public void addDelayedAction(Long delay, Action<String> actionObj)
+        throws NegativeTimeException, ValueOutOfRangeException;
+
+    @Override
+    public void addScheduledAction(Long scheduledTime, Action<String> actionObj);
+
+    @Override
+    public void cancelAction(String actionId);
+}
