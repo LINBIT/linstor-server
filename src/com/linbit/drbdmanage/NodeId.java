9085e858f86353bb0e9e934ddeb4c7d4c89067d7
Gabor Hernadi <gabor.hernadi@linbit.com>
2017-05-10 13:56:47 +0200

    Added NodeID and NodeIdAlloc classes

---
+++ src/com/linbit/drbdmanage/NodeId.java
@@ -0,0 +1,65 @@
+package com.linbit.drbdmanage;
+
+import com.linbit.Checks;
+import com.linbit.ValueOutOfRangeException;
+
+public class NodeId implements Comparable<NodeId>
+{
+    public static final int NODE_ID_MIN = 0;
+    public static final int NODE_ID_MAX = 31;
+
+    private static final String NODE_ID_EXC_FORMAT =
+        "Node id %d is out of range [%d - %d]";
+
+    public final int value;
+
+    public NodeId(final int value) throws ValueOutOfRangeException
+    {
+        nodeIdCheck(value);
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(NodeId other)
+    {
+        int result;
+        if (other == null)
+        {
+            // null sorts before any existing node id
+            result = 1;
+        }
+        else
+        {
+            result = Integer.compare(value, other.value);
+        }
+        return result;
+    }
+
+    public boolean equals(NodeId other)
+    {
+        return other != null && other.value == this.value;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return value;
+    }
+
+    @Override
+    public String toString()
+    {
+        return Integer.toString(value);
+    }
+
+    /**
+     * Checks the validity of a DRBD node id
+     *
+     * @param value The node id to check
+     * @throws ValueOutOfRangeException If the node id is out of range
+     */
+    public static void nodeIdCheck(int value) throws ValueOutOfRangeException
+    {
+        Checks.genericRangeCheck(value, NODE_ID_MIN, NODE_ID_MAX, NODE_ID_EXC_FORMAT);
+    }
+}
