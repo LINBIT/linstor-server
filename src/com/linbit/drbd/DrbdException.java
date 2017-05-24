d124b48ad0c58a36908025bef7102e7c6e83add7
Robert Altnoeder <robert.altnoeder@linbit.com>
2017-05-24 13:10:02 +0200

    Added class DrbdException

---
+++ src/com/linbit/drbd/DrbdException.java
@@ -0,0 +1,44 @@
+package com.linbit.drbd;
+
+import com.linbit.drbdmanage.DrbdManageException;
+
+/**
+ * Indicates failure to modify DRBD's state
+ *
+ * @author Robert Altnoeder &lt;robert.altnoeder@linbit.com&gt;
+ */
+public class DrbdException extends DrbdManageException
+{
+    public DrbdException(String message)
+    {
+        super(message);
+    }
+
+    public DrbdException(String message, Exception nestedException)
+    {
+        super(message, nestedException);
+    }
+
+    public DrbdException(
+        String message,
+        String descriptionText,
+        String causeText,
+        String correctionText,
+        String detailsText
+    )
+    {
+        super(message, descriptionText, causeText, correctionText, detailsText, null);
+    }
+
+    public DrbdException(
+        String message,
+        String descriptionText,
+        String causeText,
+        String correctionText,
+        String detailsText,
+        Throwable cause
+    )
+    {
+        super(message, descriptionText, causeText, correctionText, detailsText, cause);
+    }
+}
