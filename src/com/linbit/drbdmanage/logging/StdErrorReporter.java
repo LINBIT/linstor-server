bdfe6266504467d4011de1082f922b687019cbb9
Robert Altnoeder <robert.altnoeder@linbit.com>
2017-05-24 15:03:15 +0200

    Fixed error report time field: Use 24-hour clock instead of 12-hour clock

--- src/com/linbit/drbdmanage/logging/StdErrorReporter.java
+++ src/com/linbit/drbdmanage/logging/StdErrorReporter.java
@@ -456,7 +456,7 @@ public final class StdErrorReporter implements ErrorReporter
             month   = cal.get(Calendar.MONTH);
             day     = cal.get(Calendar.DAY_OF_MONTH);
 
-            hour    = cal.get(Calendar.HOUR);
+            hour    = cal.get(Calendar.HOUR_OF_DAY);
             minute  = cal.get(Calendar.MINUTE);
             second  = cal.get(Calendar.SECOND);
         }
