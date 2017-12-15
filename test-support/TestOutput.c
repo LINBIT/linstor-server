a15a85ebabb601351159fd9fc932a1995c0c4b07
Robert Altnoeder <robert.altnoeder@linbit.com>
2017-12-15 14:25:57 +0100

    Added test ExtCmdConcurrentTest

--- test-support/TestOutput.c
+++ test-support/TestOutput.c
@@ -3,26 +3,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include "loremipsum.h"
+#include <time.h>
+#include <loremipsum.h>
 
 const int IDX_EXIT_CODE = 1;
 const int IDX_LENGTH = 2;
 const int IDX_OUTPUT = 3;
 const int IDX_HANG = 4;
 
+const unsigned long long MIN_DELAY = 1;
+const unsigned long long MAX_DELAY = 120000;
+
 const size_t BUFFER_SIZE = 4096;
 
 void syntaxError(void);
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
     int exit_code = 1;
-    if (argc == 5)
+    if (argc == 5 || argc == 6)
     {
         bool have_exit_code = false;
         bool have_length = false;
         bool have_output = false;
         bool have_exit = false;
+        bool have_delay = false;
+        bool delay_valid = false;
 
         bool exit_flag = true;
 
@@ -45,7 +51,6 @@ int main(int argc, char *argv[])
 
         // Parse output
         if (strcmp("stdout", argv[3]) == 0)
-
         {
             have_output = true;
         }
@@ -68,8 +73,23 @@ int main(int argc, char *argv[])
             exit_flag = false;
         }
 
+        unsigned long long delay = 0;
+        if (argc >= 6)
+        {
+            have_delay = true;
+            // Parse delay
+            delay = strtoull(argv[5], &parse_end, 10);
+            if (*parse_end == '\0')
+            {
+                if (delay >= MIN_DELAY && delay <= MAX_DELAY)
+                {
+                    delay_valid = true;
+                }
+            }
+        }
+
         // Write output
-        if (have_exit_code && have_length && have_output && have_exit)
+        if (have_exit_code && have_length && have_output && have_exit && (!have_delay || delay_valid))
         {
             if (length > 0)
             {
@@ -80,6 +100,25 @@ int main(int argc, char *argv[])
                     (void) strncpy(buffer, LOREM_IPSUM, BUFFER_SIZE);
                     size_t blocks = length / BUFFER_SIZE;
                     size_t fraction = length % BUFFER_SIZE;
+
+                    if (have_delay && delay_valid)
+                    {
+                        struct timespec delay_timer = {delay / 1000, (delay % 1000) * 1000000};
+                        struct timespec remain_timer = {0, 0};
+                        int rc = 0;
+                        do
+                        {
+                            rc = nanosleep(&delay_timer, &remain_timer);
+                            if (rc != 0)
+                            {
+                                delay_timer = remain_timer;
+                                remain_timer.tv_sec = 0;
+                                remain_timer.tv_nsec = 0;
+                            }
+                        }
+                        while (rc != 0);
+                    }
+
                     for (size_t block_idx = 0; block_idx < blocks; ++block_idx)
                     {
                         fwrite(buffer, 1, BUFFER_SIZE, output);
@@ -131,5 +170,6 @@ int main(int argc, char *argv[])
 
 void syntaxError(void)
 {
-    fputs("Syntax: TestOutput <exit_code> <length> { stdout | stderr } { exit | hang }\n", stdout);
+    fputs("Syntax: TestOutput <exit_code> <length> { stdout | stderr } { exit | hang } [ delay_milliseconds ]\n",
+          stdout);
 }
---
+++ test/com/linbit/extproc/ExtCmdConcurrentTest.java
@@ -0,0 +1,86 @@
+package com.linbit.extproc;
+
+import com.linbit.ChildProcessTimeoutException;
+import com.linbit.extproc.ExtCmd.OutputData;
+import com.linbit.timer.GenericTimer;
+import com.linbit.timer.Action;
+import java.io.IOException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExtCmdConcurrentTest implements Runnable
+{
+    public static final int CONCURRENT_COUNT = 20;
+
+    public static final int OUTPUT_LENGTH = 14153;
+    public static final int EXIT_CODE = 120;
+    public static final int TESTOUTPUT_DELAY = 1500;
+
+    GenericTimer<String, Action<String>> intrTimer;
+
+    public ExtCmdConcurrentTest()
+    {
+        intrTimer = new GenericTimer<>();
+        intrTimer.start();
+    }
+
+    @Test
+    public void concurrentTest() throws Exception
+    {
+        Thread[] ecThr = new Thread[CONCURRENT_COUNT];
+        for (int thrIdx = 0; thrIdx < CONCURRENT_COUNT; ++thrIdx)
+        {
+            ecThr[thrIdx] = new Thread(new ExtCmdConcurrentTest());
+        }
+
+        for (int thrIdx = 0; thrIdx < CONCURRENT_COUNT; ++thrIdx)
+        {
+            ecThr[thrIdx].start();
+        }
+
+        for (int thrIdx = 0; thrIdx < CONCURRENT_COUNT; ++thrIdx)
+        {
+            ecThr[thrIdx].join();
+        }
+
+        intrTimer.shutdown();
+    }
+
+    @Override
+    public void run()
+    {
+        if (intrTimer == null)
+        {
+            fail("WTF?");
+        }
+        ExtCmd ec = new ExtCmd(intrTimer);
+        OutputData output;
+        try
+        {
+            output = ec.exec(
+                "test-support/TestOutput",
+                Integer.toString(EXIT_CODE), Integer.toString(OUTPUT_LENGTH),
+                "stdout", "exit",
+                Integer.toString(TESTOUTPUT_DELAY)
+            );
+            if (output.stdoutData.length != OUTPUT_LENGTH)
+            {
+                fail(String.format("Unexpected stdoutData length %d", output.stdoutData.length));
+            }
+            if (output.exitCode != EXIT_CODE)
+            {
+                fail(String.format("Unexpected exit code %d", output.exitCode));
+            }
+        }
+        catch (IOException ioExc)
+        {
+            fail("Test generated an IOException");
+            ioExc.printStackTrace(System.err);
+        }
+        catch (ChildProcessTimeoutException timeoutExc)
+        {
+            fail("Test generated a ChildProcessTimeoutException");
+            timeoutExc.printStackTrace(System.err);
+        }
+    }
+}
