25a6d251bd61c1e042ecae4e296e806457341317
Robert Altnoeder <robert.altnoeder@linbit.com>
2017-12-21 18:52:20 +0100

    MathUtils: Added bounds() function

--- src/com/linbit/utils/MathUtils.java
+++ src/com/linbit/utils/MathUtils.java
@@ -11,6 +11,25 @@ public class MathUtils
         return a + b;
     }
 
+    public static <T extends Comparable<T>> T bounds(
+        final T lowerBound,
+        final T value,
+        final T upperBound
+    )
+    {
+        T result = value;
+        if (value.compareTo(lowerBound) < 0)
+        {
+            result = lowerBound;
+        }
+        else
+        if (value.compareTo(upperBound) > 0)
+        {
+            result = upperBound;
+        }
+        return result;
+    }
+
     private MathUtils()
     {
     }
