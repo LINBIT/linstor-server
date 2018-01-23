1a58ae418595e63d2c20725bb633935aeb5f7a38
Rene Peinthor <rene.peinthor@linbit.com>
2018-01-23 15:08:21 +0100

    remove deprecated serializer interfaces

--- src/com/linbit/linstor/api/interfaces/serializer/CtrlSerializer.java
+++
@@ -1,8 +0,0 @@
-package com.linbit.linstor.api.interfaces.serializer;
-
-public interface CtrlSerializer<TYPE>
-{
-    public byte[] getChangedMessage(TYPE data);
-
-    public byte[] getDataMessage(int msgId, TYPE data);
-}
