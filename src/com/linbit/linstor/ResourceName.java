4b518c40df7397156fa740a19b9516c80f01e006
Robert Altnoeder <robert.altnoeder@linbit.com>
2018-02-05 15:08:17 +0100

    Added check for forbidden ResourceName value "all"
    This check prevents naming a resource "all", because "all"
    is a reserved keyword in drbd-utils.

--- src/com/linbit/linstor/ResourceName.java
+++ src/com/linbit/linstor/ResourceName.java
@@ -15,12 +15,22 @@ public class ResourceName extends GenericName
     public static final int MIN_LENGTH = 2;
     public static final int MAX_LENGTH = 48;
 
-    public static final byte[] VALID_CHARS = { '_' };
-    public static final byte[] VALID_INNER_CHARS = { '-' };
+    public static final byte[] VALID_CHARS = {'_'};
+    public static final byte[] VALID_INNER_CHARS = {'-'};
+
+    public static final String RESERVED_KEYWORD_ALL = "all";
 
     public ResourceName(String resName) throws InvalidNameException
     {
         super(resName);
+        if (resName.equalsIgnoreCase(RESERVED_KEYWORD_ALL))
+        {
+            throw new InvalidNameException(
+                "The specified name '" + resName +
+                "' cannot be used because it matches a keyword reserved for DRBD.",
+                resName
+            );
+        }
         Checks.nameCheck(resName, MIN_LENGTH, MAX_LENGTH, VALID_CHARS, VALID_INNER_CHARS);
     }
 }
