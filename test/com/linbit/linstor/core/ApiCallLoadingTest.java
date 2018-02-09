b257fa2de27a92ee33a1d5a81f6da30ddca950e7
Robert Altnoeder <robert.altnoeder@linbit.com>
2018-02-09 14:39:55 +0100

    TestExtCmd, ApiCallLoadingTest: Minor fixes

--- test/com/linbit/linstor/core/ApiCallLoadingTest.java
+++ test/com/linbit/linstor/core/ApiCallLoadingTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.linbit.linstor.core;
 
 
@@ -13,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 /**
  *
  * @author rpeinthor
