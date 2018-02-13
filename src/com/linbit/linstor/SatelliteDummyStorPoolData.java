94234581d2bd78f826063f4d8a44cf5355262d18
Gabor Hernadi <gabor.hernadi@linbit.com>
2018-02-13 11:26:36 +0100

    Added dummy Props to dummy StorPool
    All StorPools are queried for the preferred nic. Prior this commit
    the dummy StorPool threw an UnsupportedOperationException. Now a dummy
    implementation of Props is returned (always empty, stores nothing)

--- src/com/linbit/linstor/SatelliteDummyStorPoolData.java
+++ src/com/linbit/linstor/SatelliteDummyStorPoolData.java
@@ -2,8 +2,17 @@ package com.linbit.linstor;
 
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.UUID;
 
+import com.linbit.ImplementationError;
+import com.linbit.TransactionMgr;
+import com.linbit.linstor.propscon.InvalidKeyException;
+import com.linbit.linstor.propscon.InvalidValueException;
 import com.linbit.linstor.propscon.Props;
 import com.linbit.linstor.security.AccessContext;
 import com.linbit.linstor.security.AccessDeniedException;
@@ -17,6 +26,8 @@ public class SatelliteDummyStorPoolData extends StorPoolData
     {
         super();
     }
+
+    private static final Props PROPS = new DummyProps();
 
     private static final String EXC_MSG =
         "This is a dummy remote storPool. This instance's getter must not be called";
@@ -69,7 +80,7 @@ public class SatelliteDummyStorPoolData extends StorPoolData
     @Override
     public Props getProps(AccessContext accCtx) throws AccessDeniedException
     {
-        throw new UnsupportedOperationException(EXC_MSG);
+        return PROPS; // needed for querying Preferred_NIC
     }
 
     @Override
@@ -100,5 +111,176 @@ public class SatelliteDummyStorPoolData extends StorPoolData
     public void delete(AccessContext accCtx) throws AccessDeniedException, SQLException
     {
         // no-op
+    }
+
+    private static class DummyProps implements Props
+    {
+
+        @Override
+        public void initialized()
+        {
+            // no-op
+        }
+
+        @Override
+        public boolean isInitialized()
+        {
+            return true;
+        }
+
+        @Override
+        public void setConnection(TransactionMgr transMgr) throws ImplementationError
+        {
+            // no-op
+        }
+
+        @Override
+        public void commit()
+        {
+            // no-op
+        }
+
+        @Override
+        public void rollback()
+        {
+            // no-op
+        }
+
+        @Override
+        public boolean isDirty()
+        {
+            return false;
+        }
+
+        @Override
+        public boolean isDirtyWithoutTransMgr()
+        {
+            return false;
+        }
+
+        @Override
+        public boolean hasTransMgr()
+        {
+            return false;
+        }
+
+        @Override
+        public String getProp(String key) throws InvalidKeyException
+        {
+            return null;
+        }
+
+        @Override
+        public String getProp(String key, String namespace) throws InvalidKeyException
+        {
+            return null;
+        }
+
+        @Override
+        public String setProp(String key, String value)
+            throws InvalidKeyException, InvalidValueException, AccessDeniedException, SQLException
+        {
+            return null;
+        }
+
+        @Override
+        public String setProp(String key, String value, String namespace)
+            throws InvalidKeyException, InvalidValueException, AccessDeniedException, SQLException
+        {
+            return null;
+        }
+
+        @Override
+        public String removeProp(String key) throws InvalidKeyException, AccessDeniedException, SQLException
+        {
+            return null;
+        }
+
+        @Override
+        public String removeProp(String key, String namespace)
+            throws InvalidKeyException, AccessDeniedException, SQLException
+        {
+            return null;
+        }
+
+        @Override
+        public void clear() throws AccessDeniedException, SQLException
+        {
+            // no-op
+        }
+
+        @Override
+        public int size()
+        {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty()
+        {
+            return true;
+        }
+
+        @Override
+        public String getPath()
+        {
+            return null;
+        }
+
+        @Override
+        public Map<String, String> map()
+        {
+            return Collections.emptyMap();
+        }
+
+        @Override
+        public Set<Entry<String, String>> entrySet()
+        {
+            return Collections.emptySet();
+        }
+
+        @Override
+        public Set<String> keySet()
+        {
+            return Collections.emptySet();
+        }
+
+        @Override
+        public Collection<String> values()
+        {
+            return Collections.emptySet();
+        }
+
+        @Override
+        public Iterator<Entry<String, String>> iterator()
+        {
+            return Collections.<Entry<String, String>>emptySet().iterator();
+        }
+
+        @Override
+        public Iterator<String> keysIterator()
+        {
+            return Collections.<String> emptySet().iterator();
+        }
+
+        @Override
+        public Iterator<String> valuesIterator()
+        {
+            return Collections.<String> emptySet().iterator();
+        }
+
+        @Override
+        public Props getNamespace(String namespace) throws InvalidKeyException
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public Iterator<String> iterateNamespaces()
+        {
+            return Collections.<String> emptySet().iterator();
+        }
+
     }
 }
