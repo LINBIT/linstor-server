1b6bc29079be1c42a291d8bbc19dc9172f636ebe
Gabor Hernadi <gabor.hernadi@linbit.com>
2018-02-27 15:15:16 +0100

    Fixed target-package of MsgIntDelRsc.proto

--- src/com/linbit/linstor/api/protobuf/controller/NotifyResourceDeleted.java
+++ src/com/linbit/linstor/api/protobuf/controller/NotifyResourceDeleted.java
@@ -8,7 +8,7 @@ import com.linbit.linstor.api.protobuf.ProtobufApiCall;
 import com.linbit.linstor.core.CtrlApiCallHandler;
 import com.linbit.linstor.netcom.Peer;
 import com.linbit.linstor.proto.MsgDelRscOuterClass.MsgDelRsc;
-import com.linbit.linstor.proto.MsgIntDelRscOuterClass.MsgIntDelRsc;
+import com.linbit.linstor.proto.javainternal.MsgIntDelRscOuterClass.MsgIntDelRsc;
 
 import java.io.IOException;
 import java.io.InputStream;
