bd99e0d86abb6933d756a2dbc92122288c539146
Rene Peinthor <rene.peinthor@linbit.com>
2018-07-04 15:20:08 +0200

    gradle-rpm: add firewalld service files for linstor and drbd

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -7,6 +7,9 @@ DEF_DB="${DEF_LOCATION}/linstordb" # this is actually a directory
 DEF_DB_CFG="${DEF_LOCATION}/database.cfg"
 DEF_DB_TYPE="h2"
 
+# reload services files, firewalld if present
+[ -f "/usr/bin/firewall-cmd" ] && { /usr/bin/firewall-cmd -q --reload; }
+
 [ -f ${DEF_DB_CFG} ] && { echo "Database config already created, exiting"; exit 0; }
 
 ${DEF_LOCATION}/bin/linstor-config create-db-file --dbtype=${DEF_DB_TYPE} ${DEF_DB} > ${DEF_DB_CFG}
