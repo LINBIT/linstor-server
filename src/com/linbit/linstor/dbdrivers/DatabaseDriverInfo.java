3455b0547efc3db293fb6647a8bf9a3b4246c892
Robert Altnoeder <robert.altnoeder@linbit.com>
2018-07-10 12:19:02 +0200

    Replaced Class.newInstance() call, which has been marked deprecated in recent versions of Java
    Fixed by using reflection to find the default constructor and then calling newInstance()
    on the constructor.

--- src/com/linbit/linstor/dbdrivers/DatabaseDriverInfo.java
+++ src/com/linbit/linstor/dbdrivers/DatabaseDriverInfo.java
@@ -1,16 +1,41 @@
 package com.linbit.linstor.dbdrivers;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public interface DatabaseDriverInfo
 {
     static void loadDriver(final String driverClass)
     {
         try
         {
+            Class<?> loadedClass = Class.forName(driverClass);
+            Constructor<?> objConstr = loadedClass.getConstructor();
+            objConstr.newInstance();
             Class.forName(driverClass).newInstance();
         }
-        catch (final ClassNotFoundException | IllegalAccessException | InstantiationException ce)
+        catch (final ClassNotFoundException | IllegalAccessException | InstantiationException |
+               NoSuchMethodException | IllegalArgumentException | InvocationTargetException loadExc)
         {
-            System.err.println("Database driver could not be loaded.");
+            // FIXME: This function should probably have the ability to generate error reports
+            //        using LINSTOR's ErrorReporter, so that if the database driver cannot be
+            //        loaded, at least it will tell the system's administrator exactly
+            //        what went wrong
+            String detailsMsg = "Details:\n    ";
+            String errMsg = loadExc.getMessage();
+            if (errMsg != null)
+            {
+                detailsMsg += "The runtime environment provided the following error description:\n    " + errMsg;
+            }
+            else
+            {
+                detailsMsg += "The runtime environment did not provide additional " +
+                    "details about the cause of the error.";
+            }
+            System.err.println(
+                "Loading the database driver class '" + driverClass + "' failed." +
+                detailsMsg
+            );
         }
     }
 
