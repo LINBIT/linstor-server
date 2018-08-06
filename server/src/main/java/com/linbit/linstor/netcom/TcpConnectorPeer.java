ad151d49a384ad9ea8880c63343ea830133bce48
Rene Peinthor <rene.peinthor@linbit.com>
2018-08-06 11:47:31 +0200

    TcpConnectorPeer: use nextInMessage() method to update msg counter

--- server/src/main/java/com/linbit/linstor/netcom/TcpConnectorPeer.java
+++ server/src/main/java/com/linbit/linstor/netcom/TcpConnectorPeer.java
@@ -691,7 +691,7 @@ public class TcpConnectorPeer implements Peer
         if (state == ReadState.FINISHED)
         {
             addToQueue(msgIn);
-            msgIn = createMessage(false);
+            nextInMessage();
         }
         return state;
     }
