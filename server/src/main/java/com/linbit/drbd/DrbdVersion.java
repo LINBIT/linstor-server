d0f0563c0d68737976e38dcfa2fa0f875cd9df02
Robert Altnoeder <robert.altnoeder@linbit.com>
2018-10-12 12:02:59 +0200

    DrbdVersion: Indicate a failure to determine the DRBD version
    - If the instance failed to determine the DRBD version
      (this includes if DRBD is not installed)
      UNDETERMINED_VERSION (-1) is returned for all parts
      of the DRBD version
    - Added comments

--- server/src/main/java/com/linbit/drbd/DrbdVersion.java
+++ server/src/main/java/com/linbit/drbd/DrbdVersion.java
@@ -22,9 +22,11 @@ public class DrbdVersion
 
     public static final short DRBD9_MAJOR_VSN = 9;
 
-    private short majorVsn;
-    private short minorVsn;
-    private short patchLvl;
+    public static final short UNDETERMINED_VERSION = -1;
+
+    private short majorVsn = UNDETERMINED_VERSION;
+    private short minorVsn = UNDETERMINED_VERSION;
+    private short patchLvl = UNDETERMINED_VERSION;
 
     @Inject
     public DrbdVersion(CoreTimer timerRef, ErrorReporter errorLogRef)
@@ -70,26 +72,83 @@ public class DrbdVersion
         }
     }
 
+    /**
+     * Returns the DRBD major version
+     *
+     * E.g., for DRBD 9.0.15, the method will yield the value 9
+     *
+     * If the instance was unable to determine the DRBD version,
+     * the method returns {@code UNDETERMINED_VERSION (-1)}.
+     *
+     * @return DRBD major version (0 - 255), if successfully determined; UNDETERMINED_VERSION (-1) otherwise.
+     */
     public short getMajorVsn()
     {
         return majorVsn;
     }
 
+    /**
+     * Returns the DRBD minor version
+     *
+     * E.g., for DRBD 9.0.15, the method will yield the value 0
+     *
+     * If the instance was unable to determine the DRBD version,
+     * the method returns {@code UNDETERMINED_VERSION (-1)}.
+     *
+     * @return DRBD minor version (0 - 255), if successfully determined; UNDETERMINED_VERSION (-1) otherwise.
+     */
     public short getMinorVsn()
     {
         return minorVsn;
     }
 
+    /**
+     * Returns the DRBD patch level
+     *
+     * E.g., for DRBD 9.0.15, the method will yield the value 15
+     *
+     * If the instance was unable to determine the DRBD version,
+     * the method returns {@code UNDETERMINED_VERSION (-1)}.
+     *
+     * @return DRBD patch level (0 - 255), if successfully determined; UNDETERMINED_VERSION (-1) otherwise.
+     */
     public short getPatchLvl()
     {
         return patchLvl;
     }
 
+    /**
+     * Returns an array 3 of element type short, containing the DRBD version
+     *
+     * Array indexes of the DRBD version information parts:
+     *   0 DRBD major version
+     *   1 DRBD minor version
+     *   2 DRBD patch level
+     *
+     * E.g., for DRBD 9.0.15, a call of the form
+     *   {@code short[] drbdVsn = instance.getVsn();}
+     * will yield the following values:
+     *   drbdVsn[0] == 9
+     *   drbdVsn[1] == 0
+     *   drbdVsn[2] == 15
+     *
+     * If the instance was unable to determine the DRBD version, all elements of the array will
+     * have the value {@code UNDETERMINED_VERSION (-1)}.
+     *
+     * @return Array 3 of element type short, containing the DRBD version parts
+     */
     public short[] getVsn()
     {
         return new short[] { majorVsn, minorVsn, patchLvl };
     }
 
+    /**
+     * Indicates whether the DRBD version that was detected is at least DRBD 9
+     *
+     * If the instance was unable to determine the DRBD version, the method returns {@code false}.
+     *
+     * @return true if the DRBD major version is equal to or greater than 9, false otherwise
+     */
     public boolean isDrbd9()
     {
         return majorVsn >= DRBD9_MAJOR_VSN;
