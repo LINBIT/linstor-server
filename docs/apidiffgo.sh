e7edc34aab46dc0cf9951b9327d720639805cdff
Roland Kammerer <roland.kammerer@linbit.com>
2019-06-13 14:25:03 +0200

    apidiffgo.sh: add script to easy API updates
    So far it was a bit difficult to keep up in golinstor with API changes.
    This takes two git treeish, generates the corresponding go code and
    diffs it.

---
+++ docs/apidiffgo.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+APIYAML=rest_v1_openapi.yaml
+# the openapi generator writes some header. It looks like this has a static number of lines.
+NRHDR=10
+
+apigit=docs/$APIYAML
+dstdir=godst
+
+die() {
+	>&2 echo "$1"
+	exit 1
+}
+
+help() {
+cat <<EOF
+Usage:
+  $(basename "$0") v1-git-treeish v2-git-treeish
+EOF
+exit 0
+}
+
+gen_go() {
+	local d=$1
+	(
+		cd "$d" || die "Could not cd to $d"
+		docker run --rm -v "${PWD}":/local \
+			openapitools/openapi-generator-cli generate -i /local/$APIYAML --skip-validate-spec -g go -o /local/gosrc >/dev/null
+		mkdir "$dstdir"
+		cd gosrc || die "Could not cd to gosrc"
+		for f in model_*.go; do
+			tail +$NRHDR "$f" > "../${dstdir}/${f}"
+		done
+	)
+}
+
+[ $# -ne 2 ] && help
+
+v1="$1"; v2="$2"
+
+v1_dir=$(mktemp -d --suffix "$v1"); v2_dir=$(mktemp -d --suffix "$v2")
+
+git show "${v1}:${apigit}" > "${v1_dir}/${APIYAML}"
+git show "${v2}:${apigit}" > "${v2_dir}/${APIYAML}"
+
+gen_go "$v1_dir"
+gen_go "$v2_dir"
+
+d1="${v1_dir}/${dstdir}"; d2="${v2_dir}/${dstdir}"
+
+echo "diff -ruN $d1 $d2"
+diff -ruN "$d1" "$d2"
+
+echo "Feel free to rm -rf $v1_dir $v2_dir"
