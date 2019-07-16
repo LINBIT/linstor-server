6c4a2e8176bec7adbf741762c1121b7ace5ca4af
Rene Peinthor <rene.peinthor@linbit.com>
2019-07-16 11:42:52 +0200

    InvalidKeyException: extend from LinStorRuntimeException
    No more catching of the exception if we know the key source.

--- server/src/main/java/com/linbit/linstor/propscon/InvalidKeyException.java
+++ server/src/main/java/com/linbit/linstor/propscon/InvalidKeyException.java
@@ -1,11 +1,13 @@
 package com.linbit.linstor.propscon;
 
+import com.linbit.linstor.LinStorRuntimeException;
+
 /**
  * Thrown to indicate an invalid PropsContainer key
  *
  * @author Robert Altnoeder &lt;robert.altnoeder@linbit.com&gt;
  */
-public class InvalidKeyException extends Exception
+public class InvalidKeyException extends LinStorRuntimeException
 {
     private static final long serialVersionUID = -8876984313356533830L;
 
@@ -13,6 +15,7 @@ public class InvalidKeyException extends Exception
 
     public InvalidKeyException(String invalidKeyValue)
     {
+        super("Used key is invalid: " + invalidKeyValue);
         invalidKey = invalidKeyValue;
     }
 }
