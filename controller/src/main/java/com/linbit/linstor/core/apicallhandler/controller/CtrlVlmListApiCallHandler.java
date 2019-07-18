LINBIT/linstor-server

commit 22d7c42ad3505900d6bbb9c066151a28e4f280f7

Drbd: Fixed NPE when using external metadata
Mixing external and internal meta-data causes a resource having
in the data-storage-resource all expected storage-volume entries.
However, the meta-storage-resource only contains the necessary
storage-volumes (i.e. only the "external" ones, not the internals)
This led to a few NullPointerExceptions

controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/CtrlVlmListApiCallHandler.java
@@ -11,6 +11,7 @@ import com.linbit.linstor.ResourceName;
 import com.linbit.linstor.StorPoolName;
 import com.linbit.linstor.Volume;
 import com.linbit.linstor.VolumeDefinition;
+import com.linbit.linstor.VolumeNumber;
 import com.linbit.linstor.annotation.PeerContext;
 import com.linbit.linstor.api.ApiCallRc;
 import com.linbit.linstor.api.ApiCallRcImpl;
@@ -23,7 +24,9 @@ import com.linbit.linstor.netcom.Peer;
 import com.linbit.linstor.satellitestate.SatelliteState;
 import com.linbit.linstor.security.AccessContext;
 import com.linbit.linstor.security.AccessDeniedException;
-import com.linbit.linstor.storage.kinds.DeviceProviderKind;
+import com.linbit.linstor.storage.interfaces.categories.resource.RscLayerObject;
+import com.linbit.linstor.storage.kinds.DeviceLayerKind;
+import com.linbit.linstor.storage.utils.LayerUtils;
 import com.linbit.locks.LockGuardFactory;
 import com.linbit.locks.LockGuardFactory.LockObj;
 import com.linbit.locks.LockGuardFactory.LockType;
@@ -124,12 +127,30 @@ public class CtrlVlmListApiCallHandler
 
                             // build volume list filtered by storage pools (if provided)
                             List<Volume.VlmApi> volumes = new ArrayList<>();
+                            List<RscLayerObject> storageRscList = LayerUtils.getChildLayerDataByKind(
+                                rsc.getLayerData(peerAccCtx.get()),
+                                DeviceLayerKind.STORAGE
+                            );
                             Iterator<Volume> itVolumes = rsc.iterateVolumes();
                             while (itVolumes.hasNext())
                             {
                                 Volume vlm = itVolumes.next();
-                                if (storPoolsFilter.isEmpty() ||
-                                    storPoolsFilter.contains(vlm.getStorPool(peerAccCtx.get()).getName()))
+                                boolean addToList = storPoolsFilter.isEmpty();
+                                if (!addToList)
+                                {
+                                    VolumeNumber vlmNr = vlm.getVolumeDefinition().getVolumeNumber();
+                                    for (RscLayerObject storageRsc : storageRscList)
+                                    {
+                                        if (storPoolsFilter.contains(
+                                            storageRsc.getVlmProviderObject(vlmNr).getStorPool().getName())
+                                        )
+                                        {
+                                            addToList = true;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (addToList)
                                 {
                                     volumes.add(vlm.getApiData(
                                         getAllocated(vlmAllocatedCapacities, vlm),
@@ -215,8 +236,12 @@ public class CtrlVlmListApiCallHandler
     private Long getAllocated(Map<Volume.Key, Long> vlmAllocatedCapacities, Volume vlm)
         throws AccessDeniedException
     {
-        Long allocated;
-        DeviceProviderKind driverKind = vlm.getStorPool(peerAccCtx.get()).getDeviceProviderKind();
+        // TODO: test if this works as intended
+        return vlmAllocatedCapacities.get(vlm.getKey());
+
+        /*
+        Long allocated = null;
+        DeviceProviderKind driverKind = vlm.getStorPools(peerAccCtx.get()).getDeviceProviderKind();
         if (driverKind.hasBackingDevice())
         {
             allocated = getDiskAllocated(vlmAllocatedCapacities, vlm);
@@ -238,31 +263,32 @@ public class CtrlVlmListApiCallHandler
             allocated = maxAllocated;
         }
         return allocated;
+        */
     }
 
-    private Long getDiskAllocated(Map<Volume.Key, Long> vlmAllocatedCapacities, Volume vlm)
-        throws AccessDeniedException
-    {
-        Long allocated;
-        Long fetchedAllocated = vlmAllocatedCapacities.get(vlm.getKey());
-        if (fetchedAllocated != null)
-        {
-            allocated = fetchedAllocated;
-        }
-        else
-        {
-            DeviceProviderKind driverKind = vlm.getStorPool(peerAccCtx.get()).getDeviceProviderKind();
-            if (driverKind.usesThinProvisioning() || !driverKind.hasBackingDevice())
-            {
-                allocated = null;
-            }
-            else
-            {
-                allocated = vlm.getVolumeDefinition().getVolumeSize(peerAccCtx.get());
-            }
-        }
-        return allocated;
-    }
+//    private Long getDiskAllocated(Map<Volume.Key, Long> vlmAllocatedCapacities, Volume vlm)
+//        throws AccessDeniedException
+//    {
+//        Long allocated;
+//        Long fetchedAllocated = vlmAllocatedCapacities.get(vlm.getKey());
+//        if (fetchedAllocated != null)
+//        {
+//            allocated = fetchedAllocated;
+//        }
+//        else
+//        {
+//            DeviceProviderKind driverKind = vlm.getStorPools(peerAccCtx.get()).getDeviceProviderKind();
+//            if (driverKind.usesThinProvisioning() || !driverKind.hasBackingDevice())
+//            {
+//                allocated = null;
+//            }
+//            else
+//            {
+//                allocated = vlm.getVolumeDefinition().getVolumeSize(peerAccCtx.get());
+//            }
+//        }
+//        return allocated;
+//    }
 
     public static String getVlmDescriptionInline(Volume vlm)
     {

commit 104016b7e40e979561efc7ba2fe88a031c2a7c7a

ListVlm: Fixed reporting of thick-allocated-sizes
22d7c42ad3 introduced a bug where only the sizes of thinly allocated
volumes where returned in the list-volumes command.

controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/CtrlVlmListApiCallHandler.java
@@ -236,8 +236,29 @@ public class CtrlVlmListApiCallHandler
     private Long getAllocated(Map<Volume.Key, Long> vlmAllocatedCapacities, Volume vlm)
         throws AccessDeniedException
     {
-        // TODO: test if this works as intended
-        return vlmAllocatedCapacities.get(vlm.getKey());
+        // first, check if we just queried the data
+        Long allocated = vlmAllocatedCapacities.get(vlm);
+        if (allocated == null)
+        {
+            /*
+             * if the vlm was thinly provisioned, we should have found that in the map, or the
+             * satellite of the vlm is not reachable.
+             *
+             * here it would be quite cumbersome to check if the satellite is reachable, so we simply
+             * test if the vlm is thick-provisioned which would mean that the has already set its
+             * allocated size, as that will not change.
+             */
+            if (vlm.isAllocatedSizeSet(peerAccCtx.get()))
+            {
+                allocated = vlm.getAllocatedSize(peerAccCtx.get());
+            }
+            else
+            {
+                // the satellite is offline, but an appropriate message should already have been
+                // generated by our caller method
+            }
+        }
+        return allocated;
 
         /*
         Long allocated = null;