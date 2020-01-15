c0ae18ae6760061173c0bb9a65765c4251cb49bd
Gabor Hernadi <gabor.hernadi@linbit.com>
2020-01-15 12:24:48 +0100

    PairTest: Extended with null_null case

--- src/test/java/com/linbit/utils/PairTest.java
+++ src/test/java/com/linbit/utils/PairTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 
 public class PairTest
 {
+    private final Pair<String, String> null_null = new Pair<>(null, null);
     private final Pair<String, String> null_a = new Pair<>(null, "a");
     private final Pair<String, String> a_null = new Pair<>("a", null);
     private final Pair<String, String> a_a = new Pair<>("a", "a");
@@ -23,25 +24,27 @@ public class PairTest
     @Test
     public void compare() throws Exception
     {
-
         assertEquals(-1, null_a.compareTo(a_a));
         assertEquals(-1, null_a.compareTo(a_b));
         assertEquals(-1, null_a.compareTo(b_a));
         assertEquals(-1, null_a.compareTo(b_b));
         assertEquals(-1, null_a.compareTo(a_null));
         assertEquals(0, null_a.compareTo(null_a));
+        assertEquals(1, null_a.compareTo(null_null));
 
         assertEquals(-1, a_null.compareTo(a_a));
         assertEquals(-1, a_null.compareTo(a_b));
         assertEquals(-1, a_null.compareTo(b_a));
         assertEquals(-1, a_null.compareTo(b_b));
         assertEquals(0, a_null.compareTo(a_null));
+        assertEquals(1, a_null.compareTo(null_null));
         assertEquals(1, a_null.compareTo(null_a));
 
         assertEquals(-1, a_a.compareTo(a_b));
         assertEquals(-1, a_a.compareTo(b_a));
         assertEquals(-1, a_a.compareTo(b_b));
         assertEquals(0, a_a.compareTo(a_a));
+        assertEquals(1, a_a.compareTo(null_null));
         assertEquals(1, a_a.compareTo(null_a));
         assertEquals(1, a_a.compareTo(a_null));
 
@@ -49,6 +52,7 @@ public class PairTest
         assertEquals(-1, a_b.compareTo(b_b));
         assertEquals(0, a_b.compareTo(a_b));
         assertEquals(1, a_b.compareTo(a_a));
+        assertEquals(1, a_b.compareTo(null_null));
         assertEquals(1, a_b.compareTo(null_a));
         assertEquals(1, a_b.compareTo(a_null));
 
@@ -56,6 +60,7 @@ public class PairTest
         assertEquals(0, b_a.compareTo(b_a));
         assertEquals(1, b_a.compareTo(a_a));
         assertEquals(1, b_a.compareTo(a_b));
+        assertEquals(1, b_a.compareTo(null_null));
         assertEquals(1, b_a.compareTo(null_a));
         assertEquals(1, b_a.compareTo(a_null));
 
@@ -63,19 +68,22 @@ public class PairTest
         assertEquals(1, b_b.compareTo(a_a));
         assertEquals(1, b_b.compareTo(a_b));
         assertEquals(1, b_b.compareTo(b_a));
+        assertEquals(1, b_b.compareTo(null_null));
         assertEquals(1, b_b.compareTo(null_a));
         assertEquals(1, b_b.compareTo(a_null));
 
         List<Pair<String, String>> sortTest = new ArrayList<>();
         sortTest.add(b_b);
         sortTest.add(a_b);
+        sortTest.add(null_null);
         sortTest.add(a_a);
-        sortTest.add(b_a);
         sortTest.add(null_a);
+        sortTest.add(b_a);
         sortTest.add(a_null);
 
         Collections.sort(sortTest);
         List<Pair<String, String>> expectedOrder = Arrays.asList(
+            null_null,
             null_a,
             a_null,
             a_a,
