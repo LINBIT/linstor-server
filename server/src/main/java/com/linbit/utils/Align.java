9ea9b17eba0529f95cc62ff3c0e249312598fe7e
Robert Altnoeder <robert.altnoeder@linbit.com>
2020-02-27 14:46:49 +0100

    Added class Align
    Implements floor / ceiling alignment of values
    of type long with any base of type long that
    is >= 1 (1 being pretty useless for integral
    number alignment, but still valid).

---
+++ server/src/main/java/com/linbit/utils/Align.java
@@ -0,0 +1,53 @@
+package com.linbit.utils;
+
+public class Align
+{
+    public final long alignBase;
+    public final long maxAlignedValue;
+
+    public Align(final long base)
+    {
+        if (base < 1)
+        {
+            throw new IllegalArgumentException("Invalid base value for alignment, base = " + base);
+        }
+        
+        alignBase = base;
+        maxAlignedValue = (Long.MAX_VALUE / base) * base;
+    }
+
+    public long floor(final long value)
+    {
+        if (value < 0)
+        {
+            throw new ArithmeticException("Alignment of negative values is not supported");
+        }
+
+        return (value / alignBase) * alignBase;
+    }
+
+    public long ceiling(final long value)
+    {
+        if (value < 0)
+        {
+            throw new ArithmeticException("Alignment of negative values is not supported");
+        }
+
+        long result = value;
+        if (result <= maxAlignedValue)
+        {
+            if (result % alignBase != 0)
+            {
+                result = ((result / alignBase) + 1) * alignBase;
+            }
+        }
+        else
+        {
+            throw new ArithmeticException(
+                "Input value is out of range for ceiling alignment, " +
+                "value = " + value + ", base = " + alignBase
+            );
+        }
+        return result;
+    }
+}
