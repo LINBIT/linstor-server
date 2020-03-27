ff0c3e15a262e817f9d5ce3f124014d0426f8712
Robert Altnoeder <robert.altnoeder@linbit.com>
2020-03-27 16:21:57 +0100

    Added comments to com.linbit.utils.Align

--- server/src/main/java/com/linbit/utils/Align.java
+++ server/src/main/java/com/linbit/utils/Align.java
@@ -1,10 +1,26 @@
 package com.linbit.utils;
 
+/**
+ * Aligns positive values to the specified alignment.
+ *
+ * @author Robert Altnoeder &lt;robert.altnoeder@linbit.com&gt;
+ */
 public class Align
 {
     public final long alignBase;
     public final long maxAlignedValue;
 
+    /**
+     * Constructs a new instance of the {@code Align} class with the specified {@code base}.
+     *
+     * The specified {@code base} must be a positive value.
+     *
+     * Values aligned by the new instance will be aligned so that the result
+     * is a multiple of the specified base.
+     *
+     * @param base Base value for the alignment of values
+     * @throws IllegalArgumentException If the specified {@code base} is less than 1
+     */
     public Align(final long base)
     {
         if (base < 1)
@@ -16,6 +32,16 @@ public class Align
         maxAlignedValue = (Long.MAX_VALUE / base) * base;
     }
 
+    /**
+     * Returns the specified positive {@code value} floor-aligned according to the {@code alignBase}
+     * of this {@code Align} instance.
+     *
+     * The returned aligned value will be less than or equal to the specified value.
+     *
+     * @param value The value to align
+     * @return Aligned value
+     * @throws ArithmeticException If a negative {@code value} is specified
+     */
     public long floor(final long value)
     {
         if (value < 0)
@@ -26,6 +52,17 @@ public class Align
         return (value / alignBase) * alignBase;
     }
 
+    /**
+     * Returns the specified positive {@code value} ceiling-aligned according to the {@code alignBase}
+     * of this {@code Align} instance.
+     *
+     * The returned aligned value will be greater than or equal to the specified value.
+     *
+     * @param value The value to align
+     * @return Aligned value
+     * @throws ArithmeticException If a negative {@code value} is specified or
+     *                             the specified {@code value} is out of range
+     */
     public long ceiling(final long value)
     {
         if (value < 0)
