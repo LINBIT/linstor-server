6a55ed9a5234f5cf3a1acb6a7532dd7f9d49a400
Gabor Hernadi <gabor.hernadi@linbit.com>
2020-04-28 09:39:41 +0200

    Autoplacer: Added MinReservedSpace strategy

---
+++ controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/autoplacer/strategies/MinimumReservedSpaceStrategy.java
@@ -0,0 +1,67 @@
+package com.linbit.linstor.core.apicallhandler.controller.autoplacer.strategies;
+
+import com.linbit.linstor.annotation.SystemContext;
+import com.linbit.linstor.core.apicallhandler.controller.autoplacer.AutoplaceStrategy;
+import com.linbit.linstor.core.objects.Resource;
+import com.linbit.linstor.core.objects.Snapshot;
+import com.linbit.linstor.core.objects.StorPool;
+import com.linbit.linstor.security.AccessContext;
+import com.linbit.linstor.security.AccessDeniedException;
+import com.linbit.linstor.storage.interfaces.categories.resource.VlmProviderObject;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Singleton
+public class MinimumReservedSpaceStrategy implements AutoplaceStrategy
+{
+    private final AccessContext apiCtx;
+
+    @Inject
+    public MinimumReservedSpaceStrategy(
+        @SystemContext AccessContext apiCtxRef
+    )
+    {
+        apiCtx = apiCtxRef;
+    }
+
+    @Override
+    public Map<StorPool, Double> rate(List<StorPool> storPoolsRef, RatingAdditionalInfo additionalInfoRef)
+        throws AccessDeniedException
+    {
+        Map<StorPool, Double> ret = new HashMap<>();
+        for (StorPool sp : storPoolsRef)
+        {
+            double allocatedSum = 0;
+            for (VlmProviderObject<Resource> vlmObj : sp.getVolumes(apiCtx))
+            {
+                allocatedSum += vlmObj.getAllocatedSize();
+            }
+            for (VlmProviderObject<Snapshot> snapObj : sp.getSnapVolumes(apiCtx))
+            {
+                allocatedSum += snapObj.getAllocatedSize();
+            }
+
+            ret.put(sp, allocatedSum);
+        }
+
+        return ret;
+    }
+
+    @Override
+    public String getName()
+    {
+        return "MinReservedSpace";
+    }
+
+    @Override
+    public MinMax getMinMax()
+    {
+        return MinMax.MINIMIZE;
+    }
+
+}
