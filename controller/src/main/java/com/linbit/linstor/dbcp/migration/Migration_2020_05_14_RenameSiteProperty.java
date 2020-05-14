d28d95dad3f246d709e0c6cf9d19d038ab3f5934
Gabor Hernadi <gabor.hernadi@linbit.com>
2020-05-14 09:26:50 +0200

    AutoProxy: Added migration to rename Site property
    Renamed existing DB entries from 'DrbdProxy/Site' -> 'Site'

---
+++ controller/src/main/java/com/linbit/linstor/dbcp/migration/Migration_2020_05_14_RenameSiteProperty.java
@@ -0,0 +1,25 @@
+package com.linbit.linstor.dbcp.migration;
+
+import com.linbit.linstor.DatabaseInfo.DbProduct;
+import com.linbit.linstor.dbdrivers.SQLUtils;
+
+import java.sql.Connection;
+
+@SuppressWarnings("checkstyle:typename")
+@Migration(
+    version = "2020.05.14.13.37",
+    description = "Rename property 'DrbdProxy/Site' to 'Site'"
+)
+public class Migration_2020_05_14_RenameSiteProperty extends LinstorMigration
+{
+    @Override
+    protected void migrate(Connection connection, DbProduct dbProduct) throws Exception
+    {
+        SQLUtils.runSql(
+            connection,
+            "UPDATE PROPS_CONTAINERS " +
+                " SET PROP_KEY = 'Site' " +
+                " WHERE PROP_KEY = 'DrbdProxy/Site'"
+        );
+    }
+}
