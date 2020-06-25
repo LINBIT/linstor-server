d40343783dd6fd76d005ae63af3ccb891ddf871e
Gabor Hernadi <gabor.hernadi@linbit.com>
2020-06-25 18:14:32 +0200

    Fixed tests

--- src/test/java/com/linbit/linstor/ResourceTest.java
+++ src/test/java/com/linbit/linstor/ResourceTest.java
@@ -1,14 +1,14 @@
 package com.linbit.linstor;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
 import com.linbit.linstor.api.ApiConsts;
 import com.linbit.linstor.core.objects.Resource;
 
 import java.util.List;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -27,24 +27,29 @@ public class ResourceTest
             Resource.Flags.DISK_REMOVING.flagValue |
             Resource.Flags.DRBD_DISKLESS.flagValue |
             Resource.Flags.NVME_INITIATOR.flagValue |
-            Resource.Flags.TIE_BREAKER.flagValue;
+            Resource.Flags.TIE_BREAKER.flagValue |
+            Resource.Flags.INACTIVE.flagValue |
+            Resource.Flags.REACTIVATE.flagValue;
         List<String> strList = Resource.Flags.toStringList(mask);
         assertEquals(Resource.Flags.values().length, strList.size());
 
         assertArrayEquals(
-                new String[]{
-                    ApiConsts.FLAG_CLEAN,
-                    ApiConsts.FLAG_DELETE,
-                    ApiConsts.FLAG_DISKLESS,
-                    ApiConsts.FLAG_DISK_ADD_REQUESTED,
-                    ApiConsts.FLAG_DISK_ADDING,
-                    ApiConsts.FLAG_DISK_REMOVE_REQUESTED,
-                    ApiConsts.FLAG_DISK_REMOVING,
-                    ApiConsts.FLAG_DRBD_DISKLESS,
-                    ApiConsts.FLAG_TIE_BREAKER,
-                    ApiConsts.FLAG_NVME_INITIATOR
-                },
-                strList.toArray());
+            new String[]{
+                ApiConsts.FLAG_CLEAN,
+                ApiConsts.FLAG_DELETE,
+                ApiConsts.FLAG_DISKLESS,
+                ApiConsts.FLAG_DISK_ADD_REQUESTED,
+                ApiConsts.FLAG_DISK_ADDING,
+                ApiConsts.FLAG_DISK_REMOVE_REQUESTED,
+                ApiConsts.FLAG_DISK_REMOVING,
+                ApiConsts.FLAG_DRBD_DISKLESS,
+                ApiConsts.FLAG_TIE_BREAKER,
+                ApiConsts.FLAG_NVME_INITIATOR,
+                ApiConsts.FLAG_RSC_INACTIVE,
+                "REACTIVATE" // internal
+            },
+            strList.toArray()
+        );
         assertEquals(mask, Resource.Flags.fromStringList(strList));
     }
 
