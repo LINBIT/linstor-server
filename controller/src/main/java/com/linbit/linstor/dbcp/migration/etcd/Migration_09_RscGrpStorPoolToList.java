dc03ec59c90e07b4f54980936f80ed089c6e8509
Rene Peinthor <rene.peinthor@linbit.com>
2020-06-30 08:22:11 +0200

    etcd: Add missing resource group-storage pool migration
    storage pool was just a string value prior to the auto-placer rework
    now it can contain a list of strings

---
+++ controller/src/main/java/com/linbit/linstor/dbcp/migration/etcd/Migration_09_RscGrpStorPoolToList.java
@@ -0,0 +1,28 @@
+package com.linbit.linstor.dbcp.migration.etcd;
+
+import com.linbit.linstor.transaction.EtcdTransaction;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+@EtcdMigration(
+    description = "Old resource group storpool strings should be string arrays",
+    version = 36
+)
+public class Migration_09_RscGrpStorPoolToList extends BaseEtcdMigration
+{
+    @Override
+    public void migrate(EtcdTransaction tx) throws Exception
+    {
+        TreeMap<String, String> allRscGrps = tx.get("/LINSTOR/RESOURCE_GROUPS", true);
+
+        for (Map.Entry<String, String> entry : allRscGrps.entrySet())
+        {
+            if (entry.getKey().endsWith("POOL_NAME") && !entry.getValue().startsWith("["))
+            {
+                final String newVal = entry.getValue().isEmpty() ? "[]" : "[\"" + entry.getValue() + "\"]";
+                tx.put(entry.getKey(), newVal);
+            }
+        }
+    }
+}
