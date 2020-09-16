7818abe6480edf71f5baf56e27d6f509ad2ae22f
Gabor Hernadi <gabor.hernadi@linbit.com>
2020-09-16 09:51:32 +0200

    NameShortener: added implementation
    Shorten resource names uniquely but also trying to stay
    as close to the original as possible.
    
    This is done by replacing as many characters as needed at the end
    of the resulting string with an incremental number.

---
+++ controller/src/main/java/com/linbit/linstor/utils/NameShortener.java
@@ -0,0 +1,306 @@
+package com.linbit.linstor.utils;
+
+import com.linbit.ExhaustedPoolException;
+import com.linbit.ImplementationError;
+import com.linbit.linstor.core.objects.ResourceDefinition;
+import com.linbit.linstor.dbdrivers.DatabaseException;
+import com.linbit.linstor.numberpool.BitmapPool;
+import com.linbit.linstor.numberpool.NumberPool;
+import com.linbit.linstor.propscon.InvalidKeyException;
+import com.linbit.linstor.propscon.InvalidValueException;
+import com.linbit.linstor.propscon.Props;
+import com.linbit.linstor.security.AccessContext;
+import com.linbit.linstor.security.AccessDeniedException;
+import com.linbit.utils.Pair;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This class's purpose is to shorten the resource name uniquely but also trying to stay
+ * as close to the original as possible.
+ *
+ * This is done by replacing as many characters as needed at the end of the resulting string
+ * with an incremental number.
+ * Example: maxLen = 5, "resourceNameTooLong" will result in "res_1"
+ * The 10th resource starting with "res.*" will increase the digit-count, resulting in "re_10"
+ */
+// purposely not singleton and not injectable
+public class NameShortener
+{
+    public static final String OPENFLEX = "OpenFlex";
+
+    public static final int OPENFLEX_MAX_LEN = 32;
+
+    /**
+     * The property key used for lookup of already shortened name
+     */
+    private final String key;
+    /**
+     * Maximal length of shortened (result) String
+     */
+    private final int maxLen;
+    /**
+     * Delimiter used between baseString and the incremental number
+     */
+    private final String delimiter;
+    private final int delimiterLen;
+
+    private final AccessContext accCtx;
+
+    /**
+     * Map of <BaseString, digitCount> to specific numberPools
+     */
+    private final Map<Pair<String, Integer>, NumberPool> namePools;
+    /**
+     * Set will contain shortened as well as rscNames that did not have to
+     * be shortened. Used to detect any conflicting resource names
+     */
+    private final Set<String> existingNames;
+    /**
+     * delimiter + "(\\d+)$"
+     */
+    private final Pattern numberPattern;
+
+    /**
+     * "[^" + validCharacters (from constructor parameter) + "]"
+     * Used to replace everything that matches this regex with empty string.
+     * If null, the replacement is skipped
+     */
+    private final String invalidCharRegex;
+
+    public NameShortener(
+        String keyRef,
+        int maxLenRef,
+        AccessContext accCtxRef,
+        String delimiterRef,
+        String validCharacters
+    )
+    {
+        key = keyRef;
+        maxLen = maxLenRef;
+        accCtx = accCtxRef;
+        delimiter = delimiterRef;
+        delimiterLen = delimiterRef.length();
+
+        existingNames = new TreeSet<>();
+        namePools = new TreeMap<>();
+        numberPattern = Pattern.compile(delimiterRef + "(\\d+)$");
+
+        invalidCharRegex = validCharacters == null ? null : "[^" + validCharacters + "]";
+    }
+
+    public String shorten(ResourceDefinition rscDfn, String rscSuffix)
+        throws AccessDeniedException, DatabaseException
+    {
+        String shortName = null;
+
+        try
+        {
+            Props rscDfnProps = rscDfn.getProps(accCtx);
+            String fullName = rscDfnProps.getProp(key);
+            if (fullName == null)
+            {
+                // rscDfn does not have a property with the shortendName yet
+
+                fullName = rscDfn.getName().displayValue + rscSuffix;
+                if (invalidCharRegex != null)
+                {
+                    fullName = fullName.replaceAll(invalidCharRegex, "");
+                }
+
+                if (fullName.length() > maxLen || existingNames.contains(fullName))
+                {
+                    // either too long or conflicting with existing / known names
+
+                    int digitCount = 1;
+                    if (existingNames.contains(fullName))
+                    {
+                        /*
+                         * if the name is already reserved, we need to check if that name
+                         * ends with our number pattern. This only helps to calculate a better
+                         * starting digitCount, rather than starting with 1
+                         */
+                        Matcher m = numberPattern.matcher(fullName);
+                        if (m.find())
+                        {
+                            digitCount = m.group(1).length();
+                        }
+                    }
+                    boolean retry = true;
+                    while (retry)
+                    {
+                        NumberPool numberPool = getNumberPool(fullName, digitCount);
+
+                        int poolMin = base10pow(digitCount);
+                        int poolMax = poolMin * 10;
+
+                        try
+                        {
+                            // try to allocate new number or throw exception when no numbers are available
+                            int num = numberPool.autoAllocate(poolMin, poolMax - 1);
+
+                            // if succeeded set property and reserve the name
+                            String baseName = getBaseName(fullName, digitCount);
+                            shortName = baseName + delimiter + num;
+                            existingNames.add(shortName);
+                            rscDfnProps.setProp(key, shortName);
+
+                            // we are done, we could basically return here
+                            retry = false;
+                        }
+                        catch (ExhaustedPoolException exc)
+                        {
+                            // retry with higher digitCount
+                            digitCount++;
+                        }
+                    }
+                }
+                else
+                {
+                    // fullName is short enough and is not already reserved
+
+                    // reserve name
+                    existingNames.add(fullName);
+                    shortName = fullName;
+                    // make sure property is set
+                    rscDfnProps.setProp(key, shortName);
+
+                    // allocate number in case it might conflict our numberPattern
+                    Matcher m = numberPattern.matcher(fullName);
+                    if (m.find())
+                    {
+                        int digitCount = m.group(1).length();
+                        int num = Integer.parseInt(m.group(1));
+
+                        allocate(fullName, digitCount, num);
+                    }
+                }
+            }
+            else
+            {
+                // property is already set. mostly noop here
+                shortName = fullName;
+
+                // in case the property was set in a previous controller-session
+                existingNames.add(shortName);
+
+                // make sure the number is allocated
+                Matcher m = numberPattern.matcher(fullName);
+                if (m.find())
+                {
+                    int digitCount = m.group(1).length();
+                    int num = Integer.parseInt(m.group(1));
+
+                    allocate(fullName, digitCount, num);
+                }
+            }
+        }
+        catch (InvalidKeyException | InvalidValueException exc)
+        {
+            throw new ImplementationError(exc);
+        }
+        return shortName;
+    }
+
+    /**
+     * Returns the baseName of a given fullName.
+     * Example: fullName="resourceTooLong", maxLen=5, delimiter="_", digitCount=1
+     * Results: baseName="res".
+     *
+     * @param fullName
+     * @param digitCount
+     *
+     * @return
+     */
+    private String getBaseName(String fullName, int digitCount)
+    {
+        return fullName.substring(
+            0,
+            Math.min(maxLen - digitCount - delimiterLen, fullName.length())
+        );
+    }
+
+    /**
+     * Allocates the given number in the numberPool returned by {@link #getNumberPool(String, int)}.
+     * This method silently ignores the case if the number was already allocated.
+     *
+     * @param fullName
+     * @param digitCount
+     * @param num
+     */
+    private void allocate(String fullName, int digitCount, int num)
+    {
+        NumberPool numberPool = getNumberPool(fullName, digitCount);
+        numberPool.allocate(num);
+    }
+
+    private void deallocate(String fullName, int digitCount, int num)
+    {
+        NumberPool numberPool = getNumberPool(fullName, digitCount);
+        numberPool.deallocate(num);
+    }
+
+    /**
+     * Returns the NumberPool for the Pair of <baseName, digitCount> with lazy initialization.
+     * The baseName is calculated from the given parameters.
+     *
+     * @param fullName
+     * @param digitCount
+     *
+     * @return
+     */
+    private NumberPool getNumberPool(String fullName, int digitCount)
+    {
+        String baseName = getBaseName(fullName, digitCount);
+        Pair<String, Integer> namePoolKey = new Pair<>(baseName, digitCount);
+        NumberPool numberPool = namePools.get(namePoolKey);
+        if (numberPool == null)
+        {
+            numberPool = new BitmapPool(base10pow(digitCount + 1));
+            namePools.put(namePoolKey, numberPool);
+        }
+        return numberPool;
+    }
+
+    private int base10pow(int exp)
+    {
+        int tmp = 1;
+        for (int idx = 1; idx < exp; idx++)
+        {
+            tmp *= 10;
+        }
+        return tmp;
+    }
+
+    /**
+     * Deallocates the reserved number of the shortened name (if any)
+     * and forgets about the given name so that it can be chosen again later.
+     *
+     * @param rscDfn
+     * @param rscSuffix
+     *
+     * @throws AccessDeniedException
+     */
+    public void remove(ResourceDefinition rscDfn, String rscSuffix) throws AccessDeniedException
+    {
+        Props rscDfnProps = rscDfn.getProps(accCtx);
+        String shortName = rscDfnProps.getProp(key);
+        if (shortName != null)
+        {
+            Matcher m = numberPattern.matcher(shortName);
+            if (m.find())
+            {
+                int digitCount = m.group(1).length();
+                int num = Integer.parseInt(m.group(1));
+
+                deallocate(shortName, digitCount, num);
+            }
+            existingNames.remove(shortName);
+        }
+    }
+}
---
+++ src/test/java/com/linbit/linstor/utils/NameShortenerTest.java
@@ -0,0 +1,154 @@
+package com.linbit.linstor.utils;
+
+import com.linbit.linstor.core.identifier.ResourceName;
+import com.linbit.linstor.core.objects.ResourceDefinition;
+import com.linbit.linstor.dbdrivers.SatellitePropDriver;
+import com.linbit.linstor.propscon.PropsContainerFactory;
+import com.linbit.linstor.security.TestAccessContextProvider;
+import com.linbit.linstor.transaction.manager.SatelliteTransactionMgr;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
+public class NameShortenerTest
+{
+    private final PropsContainerFactory propsContainerFactory;
+    private final Map<String, ResourceDefinition> rscDfnMap;
+
+    public NameShortenerTest()
+    {
+        propsContainerFactory = new PropsContainerFactory(
+            new SatellitePropDriver(),
+            () -> new SatelliteTransactionMgr()
+        );
+        rscDfnMap = new TreeMap<>();
+    }
+
+    @Test
+    public void simpleTest() throws Throwable
+    {
+        String key = "key";
+        NameShortener shorter = new NameShortener(key, 5, TestAccessContextProvider.SYS_CTX, "_", null);
+
+        addAndAssert(shorter, "rsc", "", key, "rsc"); // < limit
+        addAndAssert(shorter, "rscTooLong", "", key, "rsc_1");
+        addAndAssert(shorter, "rscTooLong42", "", key, "rsc_2");
+        addAndAssert(shorter, "rscTooLong43", "", key, "rsc_3");
+        addAndAssert(shorter, "rscTooLong44", "", key, "rsc_4");
+        addAndAssert(shorter, "rscTooLong45", "", key, "rsc_5");
+        addAndAssert(shorter, "rscTooLong46", "", key, "rsc_6");
+        addAndAssert(shorter, "rscTooLong47", "", key, "rsc_7");
+        addAndAssert(shorter, "rscTooLong48", "", key, "rsc_8");
+        addAndAssert(shorter, "rscTooLong49", "", key, "rsc_9");
+        addAndAssert(shorter, "rscTooLong50", "", key, "rs_10");
+        addAndAssert(shorter, "rscTooLong70", "", key, "rs_11");
+
+        addAndAssert(shorter, "otherRsc", "", key, "oth_1");
+
+        addAndAssert(shorter, "oth_1", "", key, "oth_2");
+
+        addAndAssert(shorter, "ot_10", "", key, "ot_10");
+
+        addAndAssert(shorter, "otherRsc02", "", key, "oth_3");
+        ResourceDefinition otherRsc03 = addAndAssert(shorter, "otherRsc03", "", key, "oth_4");
+        addAndAssert(shorter, "otherRsc04", "", key, "oth_5");
+        addAndAssert(shorter, "otherRsc05", "", key, "oth_6");
+        addAndAssert(shorter, "otherRsc06", "", key, "oth_7");
+        addAndAssert(shorter, "otherRsc32", "", key, "oth_8");
+        addAndAssert(shorter, "otherRsc42", "", key, "oth_9");
+        addAndAssert(shorter, "otherRsc90", "", key, "ot_11");
+        addAndAssert(shorter, "ot_11", "", key, "ot_12");
+
+        addAndAssert(shorter, "ot", "42", key, "ot42");
+        addAndAssert(shorter, "oth", "9001", key, "ot_13");
+
+        addAndAssert(shorter, "ab", "9001", key, "ab9_1");
+
+        shorter.remove(otherRsc03, "");
+        addAndAssert(shorter, "otherRsc03", "", key, "oth_4");
+        addAndAssert(shorter, "otherRsc03", "", key, "oth_4");
+    }
+
+    @Test
+    public void sharedBaseTest() throws Throwable
+    {
+        String key = "key";
+        NameShortener shorter = new NameShortener(key, 5, TestAccessContextProvider.SYS_CTX, "_", null);
+
+        addAndAssert(shorter, "abbxx1", "", key, "abb_1");
+        addAndAssert(shorter, "abbxx2", "", key, "abb_2");
+        addAndAssert(shorter, "abbxx3", "", key, "abb_3");
+
+        addAndAssert(shorter, "abcxx1", "", key, "abc_1");
+        addAndAssert(shorter, "abcxx2", "", key, "abc_2");
+        addAndAssert(shorter, "abcxx3", "", key, "abc_3");
+        addAndAssert(shorter, "abcxx4", "", key, "abc_4");
+        addAndAssert(shorter, "abcxx5", "", key, "abc_5");
+        addAndAssert(shorter, "abcxx6", "", key, "abc_6");
+        addAndAssert(shorter, "abcxx7", "", key, "abc_7");
+        addAndAssert(shorter, "abcxx8", "", key, "abc_8");
+        addAndAssert(shorter, "abcxx9", "", key, "abc_9");
+        addAndAssert(shorter, "abcx10", "", key, "ab_10");
+        addAndAssert(shorter, "abcx11", "", key, "ab_11");
+
+        addAndAssert(shorter, "abbxx4", "", key, "abb_4");
+        addAndAssert(shorter, "abbxx5", "", key, "abb_5");
+        addAndAssert(shorter, "abbxx6", "", key, "abb_6");
+        addAndAssert(shorter, "abbxx7", "", key, "abb_7");
+        addAndAssert(shorter, "abbxx8", "", key, "abb_8");
+        addAndAssert(shorter, "abbxx9", "", key, "abb_9");
+        addAndAssert(shorter, "abbx10", "", key, "ab_12");
+        addAndAssert(shorter, "abbx11", "", key, "ab_13");
+    }
+
+    @Test
+    public void invalidCharsTest() throws Throwable
+    {
+        String key = "key";
+        NameShortener shorter = new NameShortener(key, 5, TestAccessContextProvider.SYS_CTX, "_", "a-zA-Z");
+
+        addAndAssert(shorter, "abc123", "", key, "abc");
+        addAndAssert(shorter, "abc124", "", key, "abc_1");
+        addAndAssert(shorter, "abc125", "", key, "abc_2");
+    }
+
+    private ResourceDefinition addAndAssert(
+        NameShortener nameShortener,
+        String rscName,
+        String rscSuffix,
+        String propKey,
+        String expectedShortenedRscName
+    )
+        throws Throwable
+    {
+        ResourceDefinition rscDfn = rscDfnMap.get(rscName);
+        if (rscDfn == null)
+        {
+            rscDfn = mock(rscName);
+            rscDfnMap.put(rscName, rscDfn);
+        }
+
+        String shortenedName = nameShortener.shorten(rscDfn, rscSuffix);
+
+        assertEquals(expectedShortenedRscName, shortenedName);
+        assertEquals(
+            rscDfn.getProps(TestAccessContextProvider.SYS_CTX).getProp(propKey),
+            shortenedName
+        );
+
+        return rscDfn;
+    }
+
+    private ResourceDefinition mock(String rscNameRef) throws Throwable
+    {
+        ResourceDefinition mockedRscDfn = Mockito.mock(ResourceDefinition.class);
+        Mockito.when(mockedRscDfn.getName()).thenReturn(new ResourceName(rscNameRef));
+        Mockito.when(mockedRscDfn.getProps(Mockito.any())).thenReturn(propsContainerFactory.create(rscNameRef));
+        return mockedRscDfn;
+    }
+}
