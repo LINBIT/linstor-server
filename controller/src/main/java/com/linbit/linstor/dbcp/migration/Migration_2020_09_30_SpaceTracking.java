90c21c1956c31c4b8281f139a1339fedc5e10815
Robert Altnoeder <robert.altnoeder@linbit.com>
2020-09-30 15:08:54 +0200

    SpaceTracking: Add DB migration
    This DB migration creates the DB tables required for SpaceTracking.

---
+++ controller/src/main/java/com/linbit/linstor/dbcp/migration/Migration_2020_09_30_SpaceTracking.java
@@ -0,0 +1,23 @@
+package com.linbit.linstor.dbcp.migration;
+
+import com.linbit.linstor.DatabaseInfo.DbProduct;
+import com.linbit.linstor.dbdrivers.SQLUtils;
+import java.sql.Connection;
+
+/**
+ * Adds SpaceTracking capability by creating the required database tables
+ */
+@Migration(
+    version = "2020.09.30.16.15",
+    description = "Create tables required for SpaceTracking"
+)
+public class Migration_2020_09_30_SpaceTracking extends LinstorMigration
+{
+    @Override
+    protected void migrate(final Connection dbConn, final DbProduct product)
+        throws Exception
+    {
+        final String sqlScript = MigrationUtils.loadResource("2020_09_30_spacetracking.sql");
+        SQLUtils.runSql(dbConn, sqlScript);
+    }
+}
