cbbf84d98d6351bf67da2543cfb7b287baf49932
Robert Altnoeder <robert.altnoeder@linbit.com>
2021-03-17 15:49:58 +0100

    NameShortener: Simplified and fixed reimplementation
    - Fix: Avoid matching leading zeroes in name suffixes
    - Fix: Avoid matching out of range numbers
    - Fix: Remove empty number pools
    - Simplified: Only a single 1 million numbers pool per name.
      Those pools are sparse pools anyway, so it should not really
      be necessary to create pools dynamically depending on what the
      largest number is expected to be.
      This also avoids a number of other potential problems.

--- server/src/main/java/com/linbit/linstor/utils/NameShortener.java
+++ server/src/main/java/com/linbit/linstor/utils/NameShortener.java
@@ -2,6 +2,7 @@ package com.linbit.linstor.utils;
 
 import com.linbit.ExhaustedPoolException;
 import com.linbit.ImplementationError;
+import com.linbit.linstor.LinStorException;
 import com.linbit.linstor.core.objects.ResourceDefinition;
 import com.linbit.linstor.core.objects.VolumeDefinition;
 import com.linbit.linstor.dbdrivers.DatabaseException;
@@ -12,12 +13,12 @@ import com.linbit.linstor.propscon.InvalidValueException;
 import com.linbit.linstor.propscon.Props;
 import com.linbit.linstor.security.AccessContext;
 import com.linbit.linstor.security.AccessDeniedException;
-import com.linbit.utils.Pair;
 
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -59,14 +60,15 @@ public class NameShortener
     /**
      * Map of <BaseString, digitCount> to specific numberPools
      */
-    private final Map<Pair<String, Integer>, NumberPool> namePools;
+    private final Map<String, NumberPool> namePools;
     /**
      * Set will contain shortened as well as rscNames that did not have to
      * be shortened. Used to detect any conflicting resource names
      */
     private final Set<String> existingNames;
+
     /**
-     * delimiter + "(\\d+)$"
+     * delimiter + "([1-9]\\d{0,5})$"
      */
     private final Pattern numberPattern;
 
@@ -95,19 +97,19 @@ public class NameShortener
 
         existingNames = new TreeSet<>();
         namePools = new TreeMap<>();
-        numberPattern = Pattern.compile(delimiterRef + "(\\d+)$");
+        numberPattern = Pattern.compile(delimiterRef + "([1-9]\\d{0,5})$");
 
         invalidCharRegex = validCharacters == null ? null : "[^" + validCharacters + "]";
     }
 
     public String shorten(ResourceDefinition rscDfn, String rscSuffix)
-        throws AccessDeniedException, DatabaseException
+        throws AccessDeniedException, DatabaseException, LinStorException
     {
         return shorten(rscDfn.getProps(accCtx), "", rscDfn.getName().displayValue + rscSuffix);
     }
 
     public String shorten(VolumeDefinition vlmDfn, String keyPrefix, String rscSuffix)
-        throws AccessDeniedException, DatabaseException
+        throws AccessDeniedException, DatabaseException, LinStorException
     {
         return shorten(
             vlmDfn.getProps(accCtx),
@@ -118,7 +120,7 @@ public class NameShortener
     }
 
     public String shorten(Props props, String propKeyPrefix, String fullName)
-        throws AccessDeniedException, DatabaseException
+        throws AccessDeniedException, DatabaseException, LinStorException
     {
         String shortName = null;
         try
@@ -135,72 +137,32 @@ public class NameShortener
                     ret = ret.replaceAll(invalidCharRegex, "");
                 }
 
-                if (ret.length() > maxLen || existingNames.contains(ret))
+                final boolean isExistingName = existingNames.contains(ret);
+                if (ret.length() > maxLen || isExistingName)
                 {
-                    // either too long or conflicting with existing / known names
+                    // Name is too long or conflicts with an existing name
+                    final NumberPool nrPool = getNumberPool(ret, true);
+                    int resourceNr = nrPool.autoAllocate(0, 999_999);
+                    String baseName = getBaseName(ret);
+                    shortName = baseName + delimiter + resourceNr;
 
-                    int digitCount = 1;
-                    if (existingNames.contains(ret))
-                    {
-                        /*
-                         * if the name is already reserved, we need to check if that name
-                         * ends with our number pattern. This only helps to calculate a better
-                         * starting digitCount, rather than starting with 1
-                         */
-                        Matcher m = numberPattern.matcher(ret);
-                        if (m.find())
-                        {
-                            digitCount = m.group(1).length();
-                        }
-                    }
-                    boolean retry = true;
-                    while (retry)
-                    {
-                        NumberPool numberPool = getNumberPool(ret, digitCount);
-
-                        int poolMin = base10pow(digitCount);
-                        int poolMax = poolMin * 10;
-
-                        try
-                        {
-                            // try to allocate new number or throw exception when no numbers are available
-                            int num = numberPool.autoAllocate(poolMin, poolMax - 1);
-
-                            // if succeeded set property and reserve the name
-                            String baseName = getBaseName(ret, digitCount);
-                            shortName = baseName + delimiter + num;
-                            existingNames.add(shortName);
-                            props.setProp(localPropKey, shortName);
-
-                            // we are done, we could basically return here
-                            retry = false;
-                        }
-                        catch (ExhaustedPoolException exc)
-                        {
-                            // retry with higher digitCount
-                            digitCount++;
-                        }
-                    }
+                    // Register the shortened name
+                    existingNames.add(shortName);
+                    // Set shortName property
+                    props.setProp(localPropKey, shortName);
                 }
                 else
                 {
-                    // fullName is short enough and is not already reserved
+                    // Full resource name is short enough and does not conflict with existing names
 
-                    // reserve name
+                    // Register the name as it is
                     existingNames.add(ret);
-                    shortName = ret;
-                    // make sure property is set
+                    // Set shortName property
                     props.setProp(localPropKey, shortName);
 
-                    // allocate number in case it might conflict our numberPattern
-                    Matcher m = numberPattern.matcher(ret);
-                    if (m.find())
-                    {
-                        int digitCount = m.group(1).length();
-                        int num = Integer.parseInt(m.group(1));
+                    shortName = ret;
 
-                        allocate(ret, digitCount, num);
-                    }
+                    processNumberSuffix(shortName);
                 }
             }
             else
@@ -208,25 +170,58 @@ public class NameShortener
                 // property is already set. mostly noop here
                 shortName = ret;
 
-                // in case the property was set in a previous controller-session
+                // Register the name in case it was set in a previous controller session
                 existingNames.add(shortName);
 
-                // make sure the number is allocated
-                Matcher m = numberPattern.matcher(ret);
-                if (m.find())
-                {
-                    int digitCount = m.group(1).length();
-                    int num = Integer.parseInt(m.group(1));
-
-                    allocate(ret, digitCount, num);
-                }
+                processNumberSuffix(shortName);
             }
+        }
+        catch (ExhaustedPoolException exc)
+        {
+            throw new LinStorException(
+                "Short name generation for resource " + fullName + " failed: " +
+                "The number pool for this name space is exhausted"
+            );
         }
         catch (InvalidKeyException | InvalidValueException exc)
         {
             throw new ImplementationError(exc);
         }
+
         return shortName;
+    }
+
+    private int parseNumber(String nrText)
+    {
+        final int number;
+        try
+        {
+            number = Integer.parseInt(nrText);
+        }
+        catch (NumberFormatException exc)
+        {
+            throw new ImplementationError(
+                "The regular expression \"" + numberPattern.toString() + "\" matched an unparsable character sequence",
+                exc
+            );
+        }
+        return number;
+    }
+
+    private void processNumberSuffix(String name)
+    {
+        // If the name ends with a text that conflicts with the numbering pattern generated
+        // by name shortening, allocate the number parsed from that pattern to avoid generating
+        // a conflicting name (with the same number, resulting in the same text)
+        Matcher nrMatcher = numberPattern.matcher(name);
+        if (nrMatcher.find())
+        {
+            final String nrText = nrMatcher.group(1);
+            final int resourceNr = parseNumber(nrText);
+
+            final NumberPool nrPool = getNumberPool(name, true);
+            nrPool.allocate(resourceNr);
+        }
     }
 
     /**
@@ -239,32 +234,11 @@ public class NameShortener
      *
      * @return
      */
-    private String getBaseName(String fullName, int digitCount)
+    private String getBaseName(String fullName)
     {
-        return fullName.substring(
-            0,
-            Math.min(maxLen - digitCount - delimiterLen, fullName.length())
-        );
-    }
-
-    /**
-     * Allocates the given number in the numberPool returned by {@link #getNumberPool(String, int)}.
-     * This method silently ignores the case if the number was already allocated.
-     *
-     * @param fullName
-     * @param digitCount
-     * @param num
-     */
-    private void allocate(String fullName, int digitCount, int num)
-    {
-        NumberPool numberPool = getNumberPool(fullName, digitCount);
-        numberPool.allocate(num);
-    }
-
-    private void deallocate(String fullName, int digitCount, int num)
-    {
-        NumberPool numberPool = getNumberPool(fullName, digitCount);
-        numberPool.deallocate(num);
+        int splitIdx = fullName.lastIndexOf(delimiter);
+        splitIdx += delimiter.length();
+        return fullName.substring(splitIdx, fullName.length());
     }
 
     /**
@@ -276,27 +250,22 @@ public class NameShortener
      *
      * @return
      */
-    private NumberPool getNumberPool(String fullName, int digitCount)
+    private NumberPool getNumberPool(String fullName, boolean createFlag)
     {
-        String baseName = getBaseName(fullName, digitCount);
-        Pair<String, Integer> namePoolKey = new Pair<>(baseName, digitCount);
-        NumberPool numberPool = namePools.get(namePoolKey);
-        if (numberPool == null)
+        final String baseName = getBaseName(fullName);
+        NumberPool numberPool = namePools.get(baseName);
+        if (numberPool == null && createFlag)
         {
-            numberPool = new BitmapPool(base10pow(digitCount + 1));
-            namePools.put(namePoolKey, numberPool);
+            numberPool = new BitmapPool(1_000_000);
+            namePools.put(baseName, numberPool);
         }
         return numberPool;
     }
 
-    private int base10pow(int exp)
+    private void removeNumberPool(String fullName)
     {
-        int tmp = 1;
-        for (int idx = 1; idx < exp; idx++)
-        {
-            tmp *= 10;
-        }
-        return tmp;
+        String baseName = getBaseName(fullName);
+        namePools.remove(baseName);
     }
 
     /**
@@ -314,13 +283,19 @@ public class NameShortener
         String shortName = rscDfnProps.getProp(propKey);
         if (shortName != null)
         {
-            Matcher m = numberPattern.matcher(shortName);
-            if (m.find())
+            Matcher nrMatcher = numberPattern.matcher(shortName);
+            if (nrMatcher.find())
             {
-                int digitCount = m.group(1).length();
-                int num = Integer.parseInt(m.group(1));
-
-                deallocate(shortName, digitCount, num);
+                final int resourceNr = parseNumber(nrMatcher.group(1));
+                final NumberPool nrPool = getNumberPool(shortName, false);
+                if (nrPool != null)
+                {
+                    nrPool.deallocate(resourceNr);
+                    if (nrPool.isEmpty())
+                    {
+                        removeNumberPool(shortName);
+                    }
+                }
             }
             existingNames.remove(shortName);
         }
