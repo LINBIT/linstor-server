2edae896af55dd16eaafbd17f033ff5f6441d45f
Robert Altnoeder <robert.altnoeder@linbit.com>
2021-04-13 11:56:01 +0200

    NameShortenerTest: Adjust max length

--- src/test/java/com/linbit/linstor/utils/NameShortenerTest.java
+++ src/test/java/com/linbit/linstor/utils/NameShortenerTest.java
@@ -37,7 +37,7 @@ public class NameShortenerTest
     public void simpleTest() throws Throwable
     {
         String key = "key";
-        NameShortener shorter = new NameShortener("", key, 5, TestAccessContextProvider.SYS_CTX, "_", null);
+        NameShortener shorter = new NameShortener("", key, 10, TestAccessContextProvider.SYS_CTX, "_", null);
 
         addAndAssert(shorter, "rsc", "", key, "rsc"); // < limit
         addAndAssert(shorter, "rscTooLong", "", key, "rsc_1");
@@ -49,72 +49,66 @@ public class NameShortenerTest
         addAndAssert(shorter, "rscTooLong47", "", key, "rsc_7");
         addAndAssert(shorter, "rscTooLong48", "", key, "rsc_8");
         addAndAssert(shorter, "rscTooLong49", "", key, "rsc_9");
-        addAndAssert(shorter, "rscTooLong50", "", key, "rs_10");
-        addAndAssert(shorter, "rscTooLong70", "", key, "rs_11");
+        addAndAssert(shorter, "rscTooLong50", "", key, "rsc_10");
+        addAndAssert(shorter, "rscTooLong70", "", key, "rsc_11");
+
+        addAndAssert(shorter, "otherRsc", "", key, "otherRsc");
 
         addAndAssert(shorter, "otherRsc", "", key, "oth_1");
 
+        addAndAssert(shorter, "oth_10", "", key, "oth_10");
+
         addAndAssert(shorter, "oth_1", "", key, "oth_2");
 
-        addAndAssert(shorter, "ot_10", "", key, "ot_10");
-
         addAndAssert(shorter, "otherRsc02", "", key, "oth_3");
+
         ResourceDefinition otherRsc03 = addAndAssert(shorter, "otherRsc03", "", key, "oth_4");
         addAndAssert(shorter, "otherRsc04", "", key, "oth_5");
         addAndAssert(shorter, "otherRsc05", "", key, "oth_6");
         addAndAssert(shorter, "otherRsc06", "", key, "oth_7");
         addAndAssert(shorter, "otherRsc32", "", key, "oth_8");
         addAndAssert(shorter, "otherRsc42", "", key, "oth_9");
-        addAndAssert(shorter, "otherRsc90", "", key, "ot_11");
-        addAndAssert(shorter, "ot_11", "", key, "ot_12");
+        addAndAssert(shorter, "otherRsc90", "", key, "oth_11");
+        addAndAssert(shorter, "oth_11", "", key, "oth_12");
 
         addAndAssert(shorter, "ot", "42", key, "ot42");
-        addAndAssert(shorter, "oth", "9001", key, "ot_13");
+        addAndAssert(shorter, "oth", "9001", key, "oth9001");
 
-        addAndAssert(shorter, "ab", "9001", key, "ab9_1");
+        addAndAssert(shorter, "abcdefg", "9001", key, "abc_1");
 
         shorter.remove(otherRsc03, "");
-        addAndAssert(shorter, "otherRsc03", "", key, "oth_4");
-        addAndAssert(shorter, "otherRsc03", "", key, "oth_4");
+        addAndAssert(shorter, "otherRscTooLong", "", key, "oth_4");
+        addAndAssert(shorter, "otherRsc03", "", key, "oth_13");
     }
 
     @Test
     public void sharedBaseTest() throws Throwable
     {
         String key = "key";
-        NameShortener shorter = new NameShortener("", key, 5, TestAccessContextProvider.SYS_CTX, "_", null);
+        NameShortener shorter = new NameShortener("", key, 10, TestAccessContextProvider.SYS_CTX, "_", null);
 
-        addAndAssert(shorter, "abbxx1", "", key, "abb_1");
-        addAndAssert(shorter, "abbxx2", "", key, "abb_2");
-        addAndAssert(shorter, "abbxx3", "", key, "abb_3");
+        addAndAssert(shorter, "aaatest001", "", key, "aaa_1");
+        addAndAssert(shorter, "aaatest002", "", key, "aaa_2");
+        addAndAssert(shorter, "aaatest003", "", key, "aaa_3");
+        addAndAssert(shorter, "aaatest004", "", key, "aaa_4");
+        addAndAssert(shorter, "aaatest005", "", key, "aaa_5");
+        addAndAssert(shorter, "aaatest006", "", key, "aaa_6");
+        addAndAssert(shorter, "aaatest007", "", key, "aaa_7");
+        addAndAssert(shorter, "aaatest008", "", key, "aaa_8");
+        addAndAssert(shorter, "aaatest009", "", key, "aaa_9");
+        addAndAssert(shorter, "aaatest010", "", key, "aaa_10");
 
-        addAndAssert(shorter, "abcxx1", "", key, "abc_1");
-        addAndAssert(shorter, "abcxx2", "", key, "abc_2");
-        addAndAssert(shorter, "abcxx3", "", key, "abc_3");
-        addAndAssert(shorter, "abcxx4", "", key, "abc_4");
-        addAndAssert(shorter, "abcxx5", "", key, "abc_5");
-        addAndAssert(shorter, "abcxx6", "", key, "abc_6");
-        addAndAssert(shorter, "abcxx7", "", key, "abc_7");
-        addAndAssert(shorter, "abcxx8", "", key, "abc_8");
-        addAndAssert(shorter, "abcxx9", "", key, "abc_9");
-        addAndAssert(shorter, "abcx10", "", key, "ab_10");
-        addAndAssert(shorter, "abcx11", "", key, "ab_11");
+        addAndAssert(shorter, "aabtest001", "", key, "aab_1");
+        addAndAssert(shorter, "aabtest002", "", key, "aab_2");
 
-        addAndAssert(shorter, "abbxx4", "", key, "abb_4");
-        addAndAssert(shorter, "abbxx5", "", key, "abb_5");
-        addAndAssert(shorter, "abbxx6", "", key, "abb_6");
-        addAndAssert(shorter, "abbxx7", "", key, "abb_7");
-        addAndAssert(shorter, "abbxx8", "", key, "abb_8");
-        addAndAssert(shorter, "abbxx9", "", key, "abb_9");
-        addAndAssert(shorter, "abbx10", "", key, "ab_12");
-        addAndAssert(shorter, "abbx11", "", key, "ab_13");
+        addAndAssert(shorter, "aabdiff001", "", key, "aab_3");
     }
 
     @Test
     public void invalidCharsTest() throws Throwable
     {
         String key = "key";
-        NameShortener shorter = new NameShortener("", key, 5, TestAccessContextProvider.SYS_CTX, "_", "a-zA-Z");
+        NameShortener shorter = new NameShortener("", key, 10, TestAccessContextProvider.SYS_CTX, "_", "a-zA-Z");
 
         addAndAssert(shorter, "abc123", "", key, "abc");
         addAndAssert(shorter, "abc124", "", key, "abc_1");
@@ -125,16 +119,18 @@ public class NameShortenerTest
     public void notTooLongTest() throws Throwable
     {
         String key = "key";
-        NameShortener shorter = new NameShortener("", key, 5, TestAccessContextProvider.SYS_CTX, "_", "a-zA-Z");
+        NameShortener shorter = new NameShortener("", key, 10, TestAccessContextProvider.SYS_CTX, "_", "a-zA-Z");
         addAndAssert(shorter, "ab", "", key, "ab");
         addAndAssert(shorter, "abc", "", key, "abc");
+        addAndAssert(shorter, "abcdef", "", key, "abcdef");
+        addAndAssert(shorter, "abcdefghij", "", key, "abcdefghij");
     }
 
     @Test
     public void namespaceTest() throws Throwable
     {
         String key = "key";
-        NameShortener shorter = new NameShortener("", key, 5, TestAccessContextProvider.SYS_CTX, "_", "a-zA-Z");
+        NameShortener shorter = new NameShortener("", key, 10, TestAccessContextProvider.SYS_CTX, "_", "a-zA-Z");
         addAndAssert(shorter, "ab", 0, "", "ns1", key, "ab");
         addAndAssert(shorter, "ab", 0, "", "ns2", key, "ab_1");
     }
