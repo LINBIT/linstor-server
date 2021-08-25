335b75e7ed4d06eff8db20e2c481428344756316
Rene Peinthor <rene.peinthor@linbit.com>
2021-08-25 12:47:41 +0200

    rsc-dfn: fix npe while deleting

--- controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/CtrlRscDfnDeleteApiCallHandler.java
+++ controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/CtrlRscDfnDeleteApiCallHandler.java
@@ -295,7 +295,7 @@ public class CtrlRscDfnDeleteApiCallHandler implements CtrlSatelliteConnectionLi
                             rscToActivate = sharedRsc;
                         }
                     }
-                    if (activeRsc == null)
+                    if (activeRsc == null && rscToActivate != null)
                     {
                         unsetFlagPrivleged(rscToActivate, Resource.Flags.INACTIVE);
                     }
