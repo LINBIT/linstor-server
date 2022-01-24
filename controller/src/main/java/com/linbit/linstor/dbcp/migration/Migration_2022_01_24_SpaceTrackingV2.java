57e61bc3b82679d5ffde6717b3974ee264153680
Robert Altnoeder <robert.altnoeder@linbit.com>
2022-01-24 17:20:23 +0100

    Add SQL database migrations

---
+++ controller/src/main/java/com/linbit/linstor/dbcp/migration/Migration_2022_01_24_SpaceTrackingV2.java
@@ -0,0 +1,18 @@
+package com.linbit.linstor.dbcp.migration;
+
+import com.linbit.linstor.DatabaseInfo;
+import com.linbit.linstor.dbdrivers.SQLUtils;
+import java.sql.Connection;
+
+@Migration(
+    version = "2022.01.16.12.00",
+    description = "Migrate to SpaceTracking V2"
+)
+public class Migration_2022_01_24_SpaceTrackingV2 extends LinstorMigration
+{
+    @Override
+    protected void migrate(Connection dbCon, DatabaseInfo.DbProduct dbProductRef) throws Exception
+    {
+        SQLUtils.runSql(dbCon, "2022_01_16_SpaceTrackingV2.sql");
+    }
+}
