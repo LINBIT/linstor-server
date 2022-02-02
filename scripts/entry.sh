99df19a17747f0cda579234f660e6ace531369bd
Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>
2022-02-02 09:43:04 +0100

    docker: try to convert PEM keys/certs on startup
    This makes it possible to re-use the standard k8s TLS secrets with LINSTOR.
    These secrets store as pem-encoded x509 certs, which can't be consumed by
    LINSTOR directly. Instead, they are converted on start-up.
    
    Inspired by https://github.com/piraeusdatastore/piraeus-operator/pull/262

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+try_import_key() {
+  indir=$1
+  [ -d "$indir" ] || return 0
+  destkeystore=$2
+  destcrtstore=$3
+  tmpfile=$(mktemp)
+
+  openssl pkcs12 -export -in "${indir}/tls.crt" -inkey "${indir}/tls.key" -out "$tmpfile" -name linstor -passin 'pass:linstor' -passout 'pass:linstor'
+  keytool -importkeystore -srcstorepass linstor -deststorepass linstor -keypass linstor -srckeystore "$tmpfile" -destkeystore "$destkeystore"
+  keytool -importcert -noprompt -deststorepass linstor -keypass linstor -file "${indir}/ca.crt" -alias ca -destkeystore "$destcrtstore"
+  rm -f "$tmpfile"
+}
+
+try_import_key /etc/linstor/ssl-pem /etc/linstor/ssl/keystore.jks /etc/linstor/ssl/certificates.jks
+try_import_key /etc/linstor/https-pem /etc/linstor/https/keystore.jks /etc/linstor/https/truststore.jks
+
 if mountpoint -q /host/proc; then
 	lvmpath=$(command -v lvm)
 	mv "$lvmpath" "${lvmpath}.distro"
