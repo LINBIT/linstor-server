d5f603e475c02815961a32af9c3d84707a75e44d
Gabor Hernadi <gabor.hernadi@linbit.com>
2022-02-22 14:07:01 +0100

    SpaceTracking: Foundations for allocated/usable tracking
    - Added allocated/usable fields

--- controller/src/main/java/com/linbit/linstor/spacetracking/AggregateCapacityInfo.java
+++ controller/src/main/java/com/linbit/linstor/spacetracking/AggregateCapacityInfo.java
@@ -5,5 +5,7 @@ import java.math.BigInteger;
 public class AggregateCapacityInfo
 {
     public BigInteger capacity = BigInteger.ZERO;
+    public BigInteger allocated = BigInteger.ZERO;
+    public BigInteger usable = BigInteger.ZERO;
     public boolean storPoolExcFlag;
 }
