21b6332eda0c3fa737dbd1b6c0d8a654ea92e387
Robert Altnoeder <robert.altnoeder@linbit.com>
2022-03-02 18:02:28 +0100

    Transform SecretGenerator into an interface

--- controller/src/main/java/com/linbit/linstor/core/SecretGenerator.java
+++ controller/src/main/java/com/linbit/linstor/core/SecretGenerator.java
@@ -1,28 +1,11 @@
 package com.linbit.linstor.core;
 
-import com.linbit.utils.Base64;
-
-import java.security.SecureRandom;
-
-public class SecretGenerator
+public interface SecretGenerator
 {
-    // SecureRandom is multithreading-safe
-    public static final SecureRandom RND = new SecureRandom();
-
-    // Random data size for automatic DRBD shared secret generation
-    // The random data will be Base64 encoded, so the length of the
-    // shared secret string will be (SECRET_LEN + 2) / 3 * 4
-    public static final int DRBD_SHARED_SECRET_SIZE = 15;
-
     /**
      * Generates a random value for a DRBD resource's shared secret
-     *
-     * @return a 20 character long random String
      */
-    public static String generateSharedSecret()
-    {
-        return generateSecretString(DRBD_SHARED_SECRET_SIZE);
-    }
+    String generateDrbdSharedSecret();
 
     /**
      * Generates a random String.<br />
@@ -33,27 +16,16 @@ public class SecretGenerator
      *       to side effects of encoding a byte array into a String that is
      *       composed of unicode characters.
      *
-     * @param size
+     * @param size The number of bytes of random data to translate into a String (not the length of the String)
      * @return A Base64 encoded String of <code>size</code> random bytes.
      */
-    public static String generateSecretString(int size)
-    {
-        String secret = Base64.encode(generateSecret(size));
-        return secret;
-    }
+    String generateSecretString(int size);
 
     /**
-     * @param size
+     * Generates a byte array of the specified size, filled with random data
+     *
+     * @param size The size of the byte array to generate
      * @return An array of random bytes with the length of the <code>size</code> parameter
      */
-    public static byte[] generateSecret(int size)
-    {
-        byte[] randomBytes = new byte[size];
-        RND.nextBytes(randomBytes);
-        return randomBytes;
-    }
-
-    private SecretGenerator()
-    {
-    }
+    byte[] generateSecret(int size);
 }
