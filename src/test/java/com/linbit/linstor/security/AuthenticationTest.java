3a304c700f23a70f794ca36e8cc03778a10a4dff
Robert Altnoeder <robert.altnoeder@linbit.com>
2022-03-17 15:57:59 +0100

    AuthenticationTest: Adjust test to the modular cryptography architecture

--- src/test/java/com/linbit/linstor/security/AuthenticationTest.java
+++ src/test/java/com/linbit/linstor/security/AuthenticationTest.java
@@ -1,6 +1,10 @@
 package com.linbit.linstor.security;
 
-import java.security.MessageDigest;
+import com.linbit.linstor.security.Authentication;
+import com.linbit.linstor.modularcrypto.CryptoModule;
+import com.linbit.linstor.modularcrypto.ModularCryptoProvider;
+import com.linbit.crypto.KeyDerivation;
+
 import javax.crypto.SecretKeyFactory;
 import org.junit.After;
 import org.junit.Before;
@@ -30,74 +34,80 @@ public class AuthenticationTest
     @SuppressWarnings("checkstyle:magicnumber")
     public void testPasswordsMatch() throws Exception
     {
-        final SecretKeyFactory keyFact = SecretKeyFactory.getInstance("PBKDF2WITHHMACSHA512");
+        final ModularCryptoProvider cryptoProvider = CryptoModule.getCryptoProvider();
+        final KeyDerivation keyDrv = cryptoProvider.createKeyDerivation();
+
         {
             byte[] password = "AllW0rk&NoPlayMakesJ0nnyADullB0y".getBytes();
+
             byte[] salt =
             {
-                (byte) 0x21, (byte) 0x39, (byte) 0xC9, (byte) 0x75,
-                (byte) 0x9B, (byte) 0xBB, (byte) 0x93, (byte) 0x5F,
-                (byte) 0x9D, (byte) 0xAB, (byte) 0x0D, (byte) 0x08,
-                (byte) 0x7A, (byte) 0x4F, (byte) 0xA3, (byte) 0x9F
+                (byte) 0x9D, (byte) 0x61, (byte) 0x9D, (byte) 0x37,
+                (byte) 0xC6, (byte) 0x46, (byte) 0x1A, (byte) 0x94,
+                (byte) 0xB0, (byte) 0x44, (byte) 0xB4, (byte) 0xD1,
+                (byte) 0xF3, (byte) 0x8D, (byte) 0x80, (byte) 0x15
             };
 
             byte[] hash =
             {
-                (byte) 0xD3, (byte) 0xFA, (byte) 0xF8, (byte) 0x82,
-                (byte) 0x7E, (byte) 0x5D, (byte) 0x6D, (byte) 0xD1,
-                (byte) 0xEB, (byte) 0x6C, (byte) 0xE5, (byte) 0xF7,
-                (byte) 0x6E, (byte) 0x81, (byte) 0xC5, (byte) 0xF9,
-                (byte) 0x25, (byte) 0xDF, (byte) 0x71, (byte) 0x07,
-                (byte) 0x88, (byte) 0x19, (byte) 0x31, (byte) 0x52,
-                (byte) 0xA0, (byte) 0xCF, (byte) 0x28, (byte) 0x8F,
-                (byte) 0xB4, (byte) 0x68, (byte) 0x6F, (byte) 0x44,
-                (byte) 0x70, (byte) 0x4E, (byte) 0x19, (byte) 0xAE,
-                (byte) 0x3C, (byte) 0xF5, (byte) 0x60, (byte) 0x81,
-                (byte) 0x3A, (byte) 0xBA, (byte) 0x5F, (byte) 0xC9,
-                (byte) 0x31, (byte) 0x11, (byte) 0x5B, (byte) 0xC9,
-                (byte) 0xCE, (byte) 0x01, (byte) 0x4F, (byte) 0xD7,
-                (byte) 0x3D, (byte) 0x19, (byte) 0x92, (byte) 0x0E,
-                (byte) 0xA3, (byte) 0xE7, (byte) 0xBA, (byte) 0x7A,
-                (byte) 0x50, (byte) 0x5A, (byte) 0xBA, (byte) 0x72
+                (byte) 0xF9, (byte) 0x07, (byte) 0x77, (byte) 0x5C,
+                (byte) 0x7E, (byte) 0xA2, (byte) 0x59, (byte) 0x40,
+                (byte) 0x83, (byte) 0x72, (byte) 0x9F, (byte) 0x85,
+                (byte) 0x2D, (byte) 0x82, (byte) 0x35, (byte) 0xB4,
+                (byte) 0x5B, (byte) 0xDF, (byte) 0x3F, (byte) 0x35,
+                (byte) 0xD8, (byte) 0x3B, (byte) 0x0F, (byte) 0x69,
+                (byte) 0xB6, (byte) 0x40, (byte) 0x51, (byte) 0xA8,
+                (byte) 0x11, (byte) 0xF6, (byte) 0xBE, (byte) 0xA2,
+                (byte) 0x99, (byte) 0x74, (byte) 0xCD, (byte) 0x13,
+                (byte) 0xF0, (byte) 0x2C, (byte) 0x3C, (byte) 0xD0,
+                (byte) 0xEE, (byte) 0xDF, (byte) 0x0A, (byte) 0x75,
+                (byte) 0xC1, (byte) 0x0F, (byte) 0x07, (byte) 0xBF,
+                (byte) 0xB8, (byte) 0x92, (byte) 0x60, (byte) 0x84,
+                (byte) 0x61, (byte) 0x60, (byte) 0x2D, (byte) 0xFD,
+                (byte) 0x9F, (byte) 0x12, (byte) 0xCE, (byte) 0x03,
+                (byte) 0x00, (byte) 0xEB, (byte) 0x1E, (byte) 0xA4
             };
+
             assertTrue(
                 "Password check failed for a correct password",
-                Authentication.passwordMatches(keyFact, password, salt, hash)
+                Authentication.passwordMatches(keyDrv, password, salt, hash)
             );
         }
 
         {
-            byte[] password = "AllPlay&NoW0rkMakesJ0nnyAD0llBuoy".getBytes();
+            byte[] password = "NoPlay&AllW0rkMakesJ0nnyAD0llBuoy".getBytes();
+
             byte[] salt =
             {
-                (byte) 0x21, (byte) 0x39, (byte) 0xC9, (byte) 0x75,
-                (byte) 0x9B, (byte) 0xBB, (byte) 0x93, (byte) 0x5F,
-                (byte) 0x9D, (byte) 0xAB, (byte) 0x0D, (byte) 0x08,
-                (byte) 0x7A, (byte) 0x4F, (byte) 0xA3, (byte) 0x9F
+                (byte) 0x9D, (byte) 0x61, (byte) 0x9D, (byte) 0x37,
+                (byte) 0xC6, (byte) 0x46, (byte) 0x1A, (byte) 0x94,
+                (byte) 0xB0, (byte) 0x44, (byte) 0xB4, (byte) 0xD1,
+                (byte) 0xF3, (byte) 0x8D, (byte) 0x80, (byte) 0x15
             };
 
             byte[] hash =
             {
-                (byte) 0xD3, (byte) 0xFA, (byte) 0xF8, (byte) 0x82,
-                (byte) 0x7E, (byte) 0x5D, (byte) 0x6D, (byte) 0xD1,
-                (byte) 0xEB, (byte) 0x6C, (byte) 0xE5, (byte) 0xF7,
-                (byte) 0x6E, (byte) 0x81, (byte) 0xC5, (byte) 0xF9,
-                (byte) 0x25, (byte) 0xDF, (byte) 0x71, (byte) 0x07,
-                (byte) 0x88, (byte) 0x19, (byte) 0x31, (byte) 0x52,
-                (byte) 0xA0, (byte) 0xCF, (byte) 0x28, (byte) 0x8F,
-                (byte) 0xB4, (byte) 0x68, (byte) 0x6F, (byte) 0x44,
-                (byte) 0x70, (byte) 0x4E, (byte) 0x19, (byte) 0xAE,
-                (byte) 0x3C, (byte) 0xF5, (byte) 0x60, (byte) 0x81,
-                (byte) 0x3A, (byte) 0xBA, (byte) 0x5F, (byte) 0xC9,
-                (byte) 0x31, (byte) 0x11, (byte) 0x5B, (byte) 0xC9,
-                (byte) 0xCE, (byte) 0x01, (byte) 0x4F, (byte) 0xD7,
-                (byte) 0x3D, (byte) 0x19, (byte) 0x92, (byte) 0x0E,
-                (byte) 0xA3, (byte) 0xE7, (byte) 0xBA, (byte) 0x7A,
-                (byte) 0x50, (byte) 0x5A, (byte) 0xBA, (byte) 0x72
+                (byte) 0xF9, (byte) 0x07, (byte) 0x77, (byte) 0x5C,
+                (byte) 0x7E, (byte) 0xA2, (byte) 0x59, (byte) 0x40,
+                (byte) 0x83, (byte) 0x72, (byte) 0x9F, (byte) 0x85,
+                (byte) 0x2D, (byte) 0x82, (byte) 0x35, (byte) 0xB4,
+                (byte) 0x5B, (byte) 0xDF, (byte) 0x3F, (byte) 0x35,
+                (byte) 0xD8, (byte) 0x3B, (byte) 0x0F, (byte) 0x69,
+                (byte) 0xB6, (byte) 0x40, (byte) 0x51, (byte) 0xA8,
+                (byte) 0x11, (byte) 0xF6, (byte) 0xBE, (byte) 0xA2,
+                (byte) 0x99, (byte) 0x74, (byte) 0xCD, (byte) 0x13,
+                (byte) 0xF0, (byte) 0x2C, (byte) 0x3C, (byte) 0xD0,
+                (byte) 0xEE, (byte) 0xDF, (byte) 0x0A, (byte) 0x75,
+                (byte) 0xC1, (byte) 0x0F, (byte) 0x07, (byte) 0xBF,
+                (byte) 0xB8, (byte) 0x92, (byte) 0x60, (byte) 0x84,
+                (byte) 0x61, (byte) 0x60, (byte) 0x2D, (byte) 0xFD,
+                (byte) 0x9F, (byte) 0x12, (byte) 0xCE, (byte) 0x03,
+                (byte) 0x00, (byte) 0xEB, (byte) 0x1E, (byte) 0xA4
             };
+
             assertFalse(
                 "Password check succeeded for an incorrect password",
-                Authentication.passwordMatches(keyFact, password, salt, hash)
+                Authentication.passwordMatches(keyDrv, password, salt, hash)
             );
         }
     }
