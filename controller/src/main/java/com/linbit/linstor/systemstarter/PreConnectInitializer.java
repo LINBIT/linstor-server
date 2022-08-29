a742d7f7e0a93d49236731d947feb4a0b65f0946
Johanna Kucera <johanna.kucera@linbit.com>
2022-08-29 14:48:46 +0200

    BackupShipping: abort failed shippings after ctrl-restart
    Make sure to remove the SHIPPING flag and special props related
    to non-persistent data (specifically to stlt-remotes) from the snaps
    after the ctrl is restarted.
    Also sets the SHIPPING_ABORT flag to mark the failure more clearly.
    This is a cleanup that only the ctrl needs to do since the stlt(s)
    already abort any shippings as soon as they lose connection with
    the ctrl.

--- controller/src/main/java/com/linbit/linstor/systemstarter/PreConnectInitializer.java
+++ controller/src/main/java/com/linbit/linstor/systemstarter/PreConnectInitializer.java
@@ -2,7 +2,9 @@ package com.linbit.linstor.systemstarter;
 
 import com.linbit.SystemServiceStartException;
 import com.linbit.linstor.InitializationException;
+import com.linbit.linstor.InternalApiConsts;
 import com.linbit.linstor.annotation.SystemContext;
+import com.linbit.linstor.api.ApiConsts;
 import com.linbit.linstor.api.LinStorScope;
 import com.linbit.linstor.core.apicallhandler.controller.CtrlSnapshotDeleteApiCallHandler;
 import com.linbit.linstor.core.apicallhandler.controller.CtrlTransactionHelper;
@@ -60,7 +62,6 @@ public class PreConnectInitializer implements StartupInitializer
     public void initialize()
         throws InitializationException, AccessDeniedException, DatabaseException, SystemServiceStartException
     {
-
         apiCallScope.enter();
         try
         {
@@ -75,32 +76,63 @@ public class PreConnectInitializer implements StartupInitializer
                             sysCtx,
                             SnapshotDefinition.Flags.SHIPPING,
                             SnapshotDefinition.Flags.SHIPPING_CLEANUP
-                        ) &&
-                        !snapDfn.getFlags().isSet(sysCtx, SnapshotDefinition.Flags.BACKUP)
+                        )
                     )
                     {
-                        snapDfn.getFlags().enableFlags(sysCtx, SnapshotDefinition.Flags.DELETE);
-                        for (Snapshot snap : snapDfn.getAllSnapshots(sysCtx))
+                        if (!snapDfn.getFlags().isSet(sysCtx, SnapshotDefinition.Flags.BACKUP))
                         {
-                            snap.getFlags().enableFlags(sysCtx, Snapshot.Flags.DELETE);
-
-                            Resource rsc = rscDfn.getResource(sysCtx, snap.getNodeName());
-                            if (rsc != null)
+                            snapDfn.getFlags().enableFlags(sysCtx, SnapshotDefinition.Flags.DELETE);
+                            for (Snapshot snap : snapDfn.getAllSnapshots(sysCtx))
                             {
-                                retryResourceTask.add(
-                                    rsc,
-                                    snapDelApiCallHandler.deleteSnapshotsOnNodes(rscDfn.getName(), snapDfn.getName())
-                                );
+                                snap.getFlags().enableFlags(sysCtx, Snapshot.Flags.DELETE);
+
+                                Resource rsc = rscDfn.getResource(sysCtx, snap.getNodeName());
+                                if (rsc != null)
+                                {
+                                    retryResourceTask.add(
+                                        rsc,
+                                        snapDelApiCallHandler
+                                            .deleteSnapshotsOnNodes(rscDfn.getName(), snapDfn.getName())
+                                    );
+                                }
                             }
+                        }
+                        else
+                        {
+                            snapDfn.getFlags().disableFlags(sysCtx, SnapshotDefinition.Flags.SHIPPING);
+                            snapDfn.getFlags().enableFlags(sysCtx, SnapshotDefinition.Flags.SHIPPING_ABORT);
+                            for (Snapshot snap : snapDfn.getAllSnapshots(sysCtx))
+                            {
+                                String key = null;
+                                if (snap.getFlags().isSet(sysCtx, Snapshot.Flags.BACKUP_SOURCE))
+                                {
+                                    // if the snapshot is the source, the remote is the target
+                                    key = InternalApiConsts.KEY_BACKUP_TARGET_REMOTE;
+                                }
+                                else if (snap.getFlags().isSet(sysCtx, Snapshot.Flags.BACKUP_TARGET))
+                                {
+                                    key = InternalApiConsts.KEY_BACKUP_SRC_REMOTE;
+                                }
+
+                                if (key != null)
+                                {
+                                    snap.getProps(sysCtx).removeProp(
+                                        key,
+                                        ApiConsts.NAMESPC_BACKUP_SHIPPING
+                                    );
+                                }
+                            }
+                            // no need to do something similar to the retryTask-stuff above, since full-sync will happen
+                            // next anyways
                         }
                     }
                 }
             }
             ctrlTransactionHelper.commit();
         }
-        catch (Throwable exc)
+        catch (Exception exc)
         {
-            throw new SystemServiceStartException("Automatic injection of passphrase failed", exc, true);
+            throw new SystemServiceStartException("Automatic cleanup after restart failed", exc, true);
         }
         finally
         {
