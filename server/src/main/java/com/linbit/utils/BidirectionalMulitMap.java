dead48e9d277f457b3b478848959f13be81bff20
Johanna Kucera <johanna.kucera@linbit.com>
2023-01-12 15:34:34 +0100

    Utils: add BidirectionalMultiMap

---
+++ server/src/main/java/com/linbit/utils/BidirectionalMulitMap.java
@@ -0,0 +1,229 @@
+package com.linbit.utils;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Supplier;
+
+public class BidirectionalMulitMap<K, V>
+{
+    /*
+     * Should these maps ever need a Collection instead of a Set, the remove* methods (and maybe others) need some
+     * changes to make sure Lists (duplicate elements) can work as expected. This also means new tests will be needed.
+     */
+    private final Map<K, Set<V>> main;
+    private final Map<V, Set<K>> inverted;
+    private final Supplier<Set<K>> supplierK;
+    private final Supplier<Set<V>> supplierV;
+
+    public BidirectionalMulitMap()
+    {
+        this(new HashMap<>(), new HashMap<>(), () -> new TreeSet<>(), () -> new TreeSet<>());
+    }
+
+    @SuppressWarnings("unchecked")
+    public <CK extends Set<K>, CV extends Set<V>> BidirectionalMulitMap(
+        Map<K, CV> mainRef,
+        Map<V, CK> invertedRef,
+        Supplier<CK> supplierKRef,
+        Supplier<CV> supplierVRef
+    )
+    {
+        main = (Map<K, Set<V>>) mainRef;
+        inverted = (Map<V, Set<K>>) invertedRef;
+        supplierK = (Supplier<Set<K>>) supplierKRef;
+        supplierV = (Supplier<Set<V>>) supplierVRef;
+    }
+
+    public int keyCount()
+    {
+        return main.size();
+    }
+
+    public int valueCount()
+    {
+        return inverted.size();
+    }
+
+    public boolean isEmpty()
+    {
+        return main.isEmpty();
+    }
+
+    public boolean containsKey(K key)
+    {
+        return main.containsKey(key);
+    }
+
+    public boolean containsValue(V value)
+    {
+        return inverted.containsKey(value);
+    }
+
+    public Set<V> getByKey(K key)
+    {
+        return getByKeyOrDefault(key, null);
+    }
+
+    public Set<K> getByValue(V value)
+    {
+        return getByValueOrDefault(value, null);
+    }
+
+    public Set<V> getByKeyOrEmpty(K key)
+    {
+        return getByKeyOrDefault(key, Collections.emptySet());
+    }
+
+    public Set<K> getByValueOrEmpty(V value)
+    {
+        return getByValueOrDefault(value, Collections.emptySet());
+    }
+
+    public Set<V> getByKeyOrDefault(K key, Set<V> dflt)
+    {
+        Set<V> ret = dflt;
+        if (main.containsKey(key))
+        {
+            ret = Collections.unmodifiableSet(main.get(key));
+        }
+        return ret;
+    }
+
+    public Set<K> getByValueOrDefault(V value, Set<K> dflt)
+    {
+        Set<K> ret = dflt;
+        if (inverted.containsKey(value))
+        {
+            ret = Collections.unmodifiableSet(inverted.get(value));
+        }
+        return ret;
+    }
+
+    public boolean put(K key, V value)
+    {
+        boolean ret = true;
+        ret &= main.computeIfAbsent(key, k -> supplierV.get()).add(value);
+        ret &= inverted.computeIfAbsent(value, v -> supplierK.get()).add(key);
+        return ret;
+    }
+
+    public Set<V> removeKey(K key)
+    {
+        Set<V> ret = main.remove(key);
+        if (ret != null)
+        {
+            for (V val : ret)
+            {
+                Set<K> keys = inverted.get(val);
+                if (keys != null)
+                {
+                    // this needs to remove all occurrences of key, make sure to change if Lists get allowed
+                    keys.remove(key);
+                    if (keys.isEmpty())
+                    {
+                        inverted.remove(val);
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    public Set<K> removeValue(V value)
+    {
+        Set<K> ret = inverted.remove(value);
+        if (ret != null)
+        {
+            for (K key : ret)
+            {
+                Set<V> vals = main.get(key);
+                if (vals != null)
+                {
+                    // this needs to remove all occurrences of value, make sure to change if Lists get allowed
+                    vals.remove(value);
+                    if (vals.isEmpty())
+                    {
+                        main.remove(key);
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    public boolean remove(K key, V value)
+    {
+        Set<V> vals = main.get(key);
+        boolean ret = false;
+        if (vals != null)
+        {
+            ret = vals.remove(value);
+            if (vals.isEmpty())
+            {
+                main.remove(key);
+            }
+        }
+        Set<K> keys = inverted.get(value);
+        if (keys != null)
+        {
+            keys.remove(key);
+            if (keys.isEmpty())
+            {
+                inverted.remove(value);
+            }
+        }
+        return ret;
+    }
+
+    public void clear()
+    {
+        main.clear();
+        inverted.clear();
+    }
+
+    public Set<K> keySet()
+    {
+        return Collections.unmodifiableSet(main.keySet());
+    }
+
+    public Set<V> valueSet()
+    {
+        return Collections.unmodifiableSet(inverted.keySet());
+    }
+
+    public Set<Entry<K, Set<V>>> entrySet()
+    {
+        return Collections.unmodifiableSet(main.entrySet());
+    }
+
+    public Set<Entry<V, Set<K>>> entrySetInverted()
+    {
+        return Collections.unmodifiableSet(inverted.entrySet());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(inverted, main);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (!(obj instanceof BidirectionalMulitMap))
+        {
+            return false;
+        }
+        BidirectionalMulitMap<?, ?> other = (BidirectionalMulitMap<?, ?>) obj;
+        return Objects.equals(inverted, other.inverted) && Objects.equals(main, other.main);
+    }
+}
---
+++ src/test/java/com/linbit/utils/BidirectionalMultiMapTest.java
@@ -0,0 +1,249 @@
+package com.linbit.utils;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("checkstyle:magicnumber")
+public class BidirectionalMultiMapTest
+{
+    private Map<String, TreeSet<Integer>> backingMain;
+    private Map<Integer, TreeSet<String>> backingInverted;
+    private BidirectionalMulitMap<String, Integer> map;
+
+    @Before
+    public void setup()
+    {
+        backingMain = new HashMap<>();
+        backingInverted = new HashMap<>();
+        map = new BidirectionalMulitMap<>(
+            backingMain,
+            backingInverted,
+            () -> new TreeSet<>(),
+            () -> new TreeSet<>()
+        );
+        assertTrue(backingMain.isEmpty());
+        assertTrue(backingInverted.isEmpty());
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void testPut()
+    {
+        map.put("A", 1);
+        assertTrue(backingMain.containsKey("A"));
+        assertTrue(backingMain.get("A").contains(1));
+        assertTrue(backingInverted.containsKey(1));
+        assertTrue(backingInverted.get(1).contains("A"));
+    }
+
+    @Test
+    public void testContains()
+    {
+        map.put("A", 1);
+        assertTrue(backingMain.containsKey("A"));
+        assertTrue(map.containsKey("A"));
+        assertTrue(backingInverted.containsKey(1));
+        assertTrue(map.containsValue(1));
+    }
+
+    @Test
+    public void testCount()
+    {
+        map.put("A", 1);
+        map.put("A", 2);
+        map.put("B", 2);
+        map.put("B", 1);
+        map.put("C", 3);
+        map.put("D", 4);
+        map.put("D", 5);
+        assertEquals(4, backingMain.size());
+        assertEquals(backingMain.size(), map.keyCount());
+        assertEquals(5, backingInverted.size());
+        assertEquals(backingInverted.size(), map.valueCount());
+    }
+
+    @Test
+    public void testIsEmpty()
+    {
+        map.put("A", 1);
+        assertFalse(backingMain.isEmpty());
+        assertFalse(backingInverted.isEmpty());
+        assertFalse(map.isEmpty());
+        map.removeValue(1);
+        assertTrue(backingMain.isEmpty());
+        assertTrue(backingInverted.isEmpty());
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void testRemoveKey()
+    {
+        map.put("A", 1);
+        map.put("B", 2);
+        map.removeKey("B");
+        assertTrue(map.containsKey("A"));
+        assertTrue(map.containsValue(1));
+        assertFalse(map.containsKey("B"));
+        assertFalse(map.containsValue(2));
+    }
+
+    @Test
+    public void testRemoveValue()
+    {
+        map.put("A", 1);
+        map.put("B", 2);
+        map.removeValue(2);
+        assertTrue(map.containsKey("A"));
+        assertTrue(map.containsValue(1));
+        assertFalse(map.containsKey("B"));
+        assertFalse(map.containsValue(2));
+    }
+
+    @Test
+    public void testRemove()
+    {
+        map.put("A", 1);
+        map.put("A", 2);
+        map.put("A", 3);
+        map.put("A", 4);
+        map.put("A", 5);
+        map.put("B", 1);
+        assertTrue(map.containsKey("A"));
+        assertTrue(map.containsValue(1));
+        assertTrue(map.getByKey("A").contains(1));
+        assertTrue(map.getByValue(1).contains("A"));
+        map.remove("A", 1);
+        assertTrue(map.containsKey("A"));
+        assertTrue(map.containsValue(1));
+        assertFalse(map.getByKey("A").contains(1));
+        assertFalse(map.getByValue(1).contains("A"));
+    }
+
+    @Test
+    public void testClear()
+    {
+        map.put("A", 1);
+        map.put("A", 2);
+        map.put("B", 2);
+        map.put("B", 1);
+        map.put("C", 3);
+        map.put("D", 4);
+        map.put("D", 5);
+        assertFalse(map.isEmpty());
+        map.clear();
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void testSets()
+    {
+        Map<String, Set<Integer>> mainForComp = new HashMap<>();
+        mainForComp.put("A", new TreeSet<>());
+        mainForComp.get("A").add(1);
+        mainForComp.get("A").add(2);
+        mainForComp.put("B", new TreeSet<>());
+        mainForComp.get("B").add(2);
+        mainForComp.put("C", new TreeSet<>());
+        mainForComp.get("C").add(1);
+        mainForComp.put("D", new TreeSet<>());
+        mainForComp.get("D").add(3);
+        Map<Integer, Set<String>> invertedForComp = new HashMap<>();
+        invertedForComp.put(1, new TreeSet<>());
+        invertedForComp.get(1).add("A");
+        invertedForComp.get(1).add("C");
+        invertedForComp.put(2, new TreeSet<>());
+        invertedForComp.get(2).add("A");
+        invertedForComp.get(2).add("B");
+        invertedForComp.put(3, new TreeSet<>());
+        invertedForComp.get(3).add("D");
+        map.put("A", 1);
+        map.put("A", 2);
+        map.put("B", 2);
+        map.put("C", 1);
+        map.put("D", 3);
+        Set<String> keySet = map.keySet();
+        assertEquals(4, keySet.size());
+        assertTrue(keySet.contains("A"));
+        assertTrue(keySet.contains("B"));
+        assertTrue(keySet.contains("C"));
+        assertTrue(keySet.contains("D"));
+        Set<Integer> valueSet = map.valueSet();
+        assertEquals(3, valueSet.size());
+        assertTrue(valueSet.contains(1));
+        assertTrue(valueSet.contains(2));
+        assertTrue(valueSet.contains(3));
+        Set<Entry<String, Set<Integer>>> entrySet = map.entrySet();
+        assertEquals(4, entrySet.size());
+        assertEquals(mainForComp.entrySet(), entrySet);
+        Set<Entry<Integer, Set<String>>> entrySetInverted = map.entrySetInverted();
+        assertEquals(3, entrySetInverted.size());
+        assertEquals(invertedForComp.entrySet(), entrySetInverted);
+    }
+
+    @Test
+    public void testGets() {
+        map.put("A", 1);
+        map.put("A", 2);
+        map.put("B", 1);
+        map.put("C", 1);
+        Set<Integer> byKey = map.getByKey("A");
+        Set<Integer> byKeyComp = new TreeSet<>(Arrays.asList(1, 2));
+        assertEquals(byKeyComp, byKey);
+        Set<String> byValue = map.getByValue(1);
+        Set<String> byValueComp = new TreeSet<>(Arrays.asList("A", "B", "C"));
+        assertEquals(byValueComp, byValue);
+        Set<Integer> byKeyNotEmpty = map.getByKeyOrEmpty("C");
+        Set<Integer> byKeyNotEmptyComp = Collections.singleton(1);
+        assertEquals(byKeyNotEmptyComp, byKeyNotEmpty);
+        Set<Integer> byKeyIsEmpty = map.getByKeyOrEmpty("D");
+        Set<Integer> byKeyIsEmptyComp = Collections.emptySet();
+        assertEquals(byKeyIsEmptyComp, byKeyIsEmpty);
+        Set<String> byValueNotEmpty = map.getByValueOrEmpty(2);
+        Set<String> byValueNotEmptyComp = Collections.singleton("A");
+        assertEquals(byValueNotEmptyComp, byValueNotEmpty);
+        Set<String> byValueIsEmpty = map.getByValueOrEmpty(3);
+        Set<String> byValueIsEmptyComp = Collections.emptySet();
+        assertEquals(byValueIsEmptyComp, byValueIsEmpty);
+        Set<Integer> byKeyNotDefault = map.getByKeyOrDefault("B", Collections.singleton(-1));
+        Set<Integer> byKeyNotDefaultComp = Collections.singleton(1);
+        assertEquals(byKeyNotDefaultComp, byKeyNotDefault);
+        Set<Integer> byKeyIsDefault = map.getByKeyOrDefault("D", Collections.singleton(-1));
+        Set<Integer> byKeyIsDefaultComp = Collections.singleton(-1);
+        assertEquals(byKeyIsDefaultComp, byKeyIsDefault);
+        Set<String> byValueNotDefault = map.getByValueOrDefault(2, Collections.singleton("ups"));
+        Set<String> byValueNotDefaultComp = Collections.singleton("A");
+        assertEquals(byValueNotDefaultComp, byValueNotDefault);
+        Set<String> byValueIsDefault = map.getByValueOrDefault(3, Collections.singleton("ups"));
+        Set<String> byValueIsDefaultComp = Collections.singleton("ups");
+        assertEquals(byValueIsDefaultComp, byValueIsDefault);
+    }
+
+    @Test
+    public void testEqualsAndHashCode()
+    {
+        BidirectionalMulitMap<String, Integer> otherMap = new BidirectionalMulitMap<>();
+        assertEquals(map.hashCode(), otherMap.hashCode());
+        assertEquals(map, otherMap);
+        map.put("A", 1);
+        map.put("B", 2);
+        assertNotEquals(map.hashCode(), otherMap.hashCode());
+        assertNotEquals(map, otherMap);
+        otherMap.put("A", 1);
+        otherMap.put("B", 2);
+        assertEquals(map.hashCode(), otherMap.hashCode());
+        assertEquals(map, otherMap);
+    }
+}
