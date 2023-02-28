LINBIT/linstor-server

commit 0ef01d29ec4622d2990b5b743afbc81a619a304b

Layer: Add IgnoreReason
In some cases a layer should be entirely skipped on the satellite
while processing. For example even if layer-stack states there should
be a DRBD above NVME, the satellite that has the NVME target resource
deployed does not necessarily need to have DRBD installed at all.
However the DrbdLayer class starts its processing with trying to
sync Linstor's DRBD state with the actual DRBD state (which fails
if DRBD is not installed).

This patch adds an "ignoreReason" (null / empty means no reason,
i.e. the layer should not be skipped), where the DrbdRscData gets
marked by the NVMe layer (on controller side, during rscData-creation)

Setting a resource INACTIVE requires now an additional step where
the INACTIVE flag as well as the INACTIVATING flags are set.
That is needed so that the layers are *not* ignored while INACTIVATING
so that they can deconstruct themself before getting skipped

controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/CtrlRscCrtApiHelper.java
@@ -116,6 +116,8 @@ public class CtrlRscCrtApiHelper
     private final SharedResourceManager sharedRscMgr;
     private final BackupInfoManager backupInfoMgr;
     private final ScheduleBackupService scheduleBackupService;
+    private final CtrlRscLayerDataFactory ctrlRscLayerDataFactory;
+    private final CtrlRscActivateApiCallHandler ctrlRscActivateApiCallHandler;
 
     @Inject
     CtrlRscCrtApiHelper(
@@ -136,7 +138,9 @@ public class CtrlRscCrtApiHelper
         Provider<CtrlRscToggleDiskApiCallHandler> toggleDiskHelperRef,
         SharedResourceManager sharedRscMgrRef,
         BackupInfoManager backupInfoMgrRef,
-        ScheduleBackupService scheduleBackupServiceRef
+        ScheduleBackupService scheduleBackupServiceRef,
+        CtrlRscLayerDataFactory ctrlRscLayerDataFactoryRef,
+        CtrlRscActivateApiCallHandler ctrlRscActivateApiCallHandlerRef
     )
     {
         apiCtx = apiCtxRef;
@@ -157,6 +161,8 @@ public class CtrlRscCrtApiHelper
         sharedRscMgr = sharedRscMgrRef;
         backupInfoMgr = backupInfoMgrRef;
         scheduleBackupService = scheduleBackupServiceRef;
+        ctrlRscLayerDataFactory = ctrlRscLayerDataFactoryRef;
+        ctrlRscActivateApiCallHandler = ctrlRscActivateApiCallHandlerRef;
     }
 
     /**
@@ -232,7 +238,7 @@ public class CtrlRscCrtApiHelper
 
             if (FlagsHelper.isFlagEnabled(adjustedFlags, Resource.Flags.INACTIVE))
             {
-                setResourceFlags(rsc, Resource.Flags.INACTIVE);
+                autoFlux.add(ctrlRscActivateApiCallHandler.deactivateRsc(nodeNameStr, rscNameStr));
             }
 
             if (!isDiskless)
@@ -454,7 +460,7 @@ public class CtrlRscCrtApiHelper
 
         if (!isFlagSet(rsc, Resource.Flags.INACTIVE) && !sharedRscMgr.isActivationAllowed(rsc))
         {
-            setResourceFlags(rsc, Resource.Flags.INACTIVE);
+            autoFlux.add(ctrlRscActivateApiCallHandler.activateRsc(nodeNameStr, rscNameStr));
         }
 
         return new Pair<>(autoFlux, new ApiCallRcWith<>(responses, rsc));

commit 3b583b8b9028a1f4d84874f3b4a649eb5cc29e06

Rsc,Crt,Shared: Automatically deactivate rsc in shared SP
Commit 0ef01d29ec4622d2990b5b743afbc81a619a304b accidentally introduced
this bug:

-            setResourceFlags(rsc, Resource.Flags.INACTIVE);
+            autoFlux.add(ctrlRscActivateApiCallHandler.activateRsc(nodeNameStr, rscNameStr));

Instead of setting the INACTIVE flag the resource gets activated.
This patch fixes this issue.

controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/CtrlRscCrtApiHelper.java
@@ -482,7 +482,7 @@ public class CtrlRscCrtApiHelper
 
         if (!isFlagSet(rsc, Resource.Flags.INACTIVE) && !sharedRscMgr.isActivationAllowed(rsc))
         {
-            autoFlux.add(ctrlRscActivateApiCallHandler.activateRsc(nodeNameStr, rscNameStr));
+            autoFlux.add(ctrlRscActivateApiCallHandler.deactivateRsc(nodeNameStr, rscNameStr));
         }
 
         return new Pair<>(autoFlux, new ApiCallRcWith<>(responses, rsc));