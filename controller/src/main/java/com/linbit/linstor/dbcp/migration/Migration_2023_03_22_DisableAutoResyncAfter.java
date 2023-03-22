12d43de1401097888d21ad825911bb31ed8ab338
Rene Peinthor <rene.peinthor@linbit.com>
2023-03-22 11:07:32 +0100

    disable auto-resync-after if prop not already set

---
+++ controller/src/main/java/com/linbit/linstor/dbcp/migration/Migration_2023_03_22_DisableAutoResyncAfter.java
@@ -0,0 +1,39 @@
+package com.linbit.linstor.dbcp.migration;
+
+import com.linbit.linstor.DatabaseInfo.DbProduct;
+import com.linbit.linstor.api.ApiConsts;
+import com.linbit.linstor.dbdrivers.SQLUtils;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+@SuppressWarnings("checkstyle:typename")
+@Migration(
+    version = "2023.03.22.10.00",
+    description = "Disable the automatic resync-after feature"
+)
+public class Migration_2023_03_22_DisableAutoResyncAfter extends LinstorMigration
+{
+    private static final String TBL_PROPS_CONTAINERS = "PROPS_CONTAINERS";
+    private final static String KEY_AUTO_RESYNC_AFTER = ApiConsts.NAMESPC_DRBD_OPTIONS + "/auto-resync-after-disable";
+
+    @Override
+    protected void migrate(Connection connection, DbProduct dbProduct) throws Exception
+    {
+        ResultSet rs = connection.createStatement().executeQuery(
+            "SELECT 1 FROM " + TBL_PROPS_CONTAINERS + " WHERE PROPS_INSTANCE='/CTRLCFG' AND PROP_KEY='" +
+                KEY_AUTO_RESYNC_AFTER + "'"
+        );
+
+        if (!rs.next())
+        {
+            SQLUtils.runSql(
+                connection,
+                "INSERT INTO " + TBL_PROPS_CONTAINERS + " (PROPS_INSTANCE, PROP_KEY, PROP_VALUE) " +
+                    "VALUES ('/CTRLCFG', '" + KEY_AUTO_RESYNC_AFTER + "', 'true')"
+            );
+        }
+
+        rs.close();
+    }
+}
---
+++ controller/src/main/java/com/linbit/linstor/dbcp/migration/k8s/crd/Migration_11_v1_19_1_DisableResyncAfter.java
@@ -0,0 +1,50 @@
+package com.linbit.linstor.dbcp.migration.k8s.crd;
+
+import com.linbit.linstor.ControllerK8sCrdDatabase;
+import com.linbit.linstor.dbdrivers.GeneratedDatabaseTables;
+import com.linbit.linstor.dbdrivers.k8s.crd.GenCrdV1_19_1;
+import com.linbit.linstor.dbdrivers.k8s.crd.GenCrdV1_19_1.PropsContainers;
+import com.linbit.linstor.dbdrivers.k8s.crd.GenCrdV1_19_1.PropsContainersSpec;
+
+import java.util.Collection;
+
+@K8sCrdMigration(
+    description = "Disable AutoResyncAfter",
+    version = 11
+)
+public class Migration_11_v1_19_1_DisableResyncAfter extends BaseK8sCrdMigration
+{
+    private final static String KEY_AUTO_RESYNC_AFTER = "DrbdOptions/auto-resync-after-disable";
+
+    public Migration_11_v1_19_1_DisableResyncAfter()
+    {
+        super(
+            GenCrdV1_19_1.createTxMgrContext(),
+            GenCrdV1_19_1.createTxMgrContext(),
+            GenCrdV1_19_1.createSchemaUpdateContext()
+        );
+    }
+
+    @Override
+    public MigrationResult migrateImpl(ControllerK8sCrdDatabase k8sDbRef) throws Exception
+    {
+        Collection<PropsContainers> propsContainers = txFrom.<PropsContainers, PropsContainersSpec>getCrd(
+            GeneratedDatabaseTables.PROPS_CONTAINERS,
+            propsCont -> propsCont.getSpec().propsInstance.startsWith("/CTRLCFG") &&
+                propsCont.getSpec().propKey.equals(KEY_AUTO_RESYNC_AFTER)
+        ).values();
+
+        if (propsContainers.isEmpty())
+        {
+            txTo.create(
+                GeneratedDatabaseTables.PROPS_CONTAINERS,
+                GenCrdV1_19_1.createPropsContainers(
+                    "/CTRLCFG",
+                    KEY_AUTO_RESYNC_AFTER,
+                    "True"
+                )
+            );
+        }
+        return null;
+    }
+}
