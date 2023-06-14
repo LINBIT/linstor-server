6e7f957f372a3a66a7574078a5d26e060d8edf46
Gabor Hernadi <gabor.hernadi@linbit.com>
2023-06-14 09:42:58 +0200

    ETCD,K8s,PropInstance: Upper cased instance name
    Since the old SQL driver already upper cased all instance names it
    is not possible to use a mixed case for SQL. Therefore K8s and ETCD
    have to be migrated to store the PropsInstance name as upper case.

---
+++ controller/src/main/java/com/linbit/linstor/dbcp/migration/k8s/crd/Migration_13_v1_19_1_UpperCasePropsInstance.java
@@ -0,0 +1,49 @@
+package com.linbit.linstor.dbcp.migration.k8s.crd;
+
+import com.linbit.linstor.ControllerK8sCrdDatabase;
+import com.linbit.linstor.dbdrivers.GeneratedDatabaseTables;
+import com.linbit.linstor.dbdrivers.k8s.crd.GenCrdV1_19_1;
+import com.linbit.linstor.dbdrivers.k8s.crd.GenCrdV1_19_1.PropsContainers;
+import com.linbit.linstor.dbdrivers.k8s.crd.GenCrdV1_19_1.PropsContainersSpec;
+
+import java.util.Collection;
+
+@K8sCrdMigration(
+    description = "Upper case props instance",
+    version = 13
+)
+public class Migration_13_v1_19_1_UpperCasePropsInstance extends BaseK8sCrdMigration
+{
+    public Migration_13_v1_19_1_UpperCasePropsInstance()
+    {
+        super(
+            GenCrdV1_19_1.createTxMgrContext(),
+            GenCrdV1_19_1.createTxMgrContext(),
+            GenCrdV1_19_1.createSchemaUpdateContext()
+        );
+    }
+
+    @Override
+    public MigrationResult migrateImpl(ControllerK8sCrdDatabase k8sDbRef) throws Exception
+    {
+        Collection<PropsContainers> propsContainers = txFrom.<PropsContainers, PropsContainersSpec>getCrd(
+            GeneratedDatabaseTables.PROPS_CONTAINERS
+        ).values();
+
+        for (PropsContainers propsCon : propsContainers)
+        {
+            PropsContainersSpec spec = propsCon.getSpec();
+            txTo.delete(GeneratedDatabaseTables.PROPS_CONTAINERS, propsCon);
+            txTo.create(
+                GeneratedDatabaseTables.PROPS_CONTAINERS,
+                GenCrdV1_19_1.createPropsContainers(
+                    // will also recalculate (changed) new k8s name
+                    spec.propsInstance.toUpperCase(),
+                    spec.propKey,
+                    spec.propValue
+                )
+            );
+        }
+        return null;
+    }
+}
