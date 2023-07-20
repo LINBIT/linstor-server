8b40af95262f8ea20dfacec2b807b654c157fec3
Gabor Hernadi <gabor.hernadi@linbit.com>
2023-07-20 13:24:33 +0200

    SpaceTracking: Make AggregateCapacityInfo comparable

--- controller/src/main/java/com/linbit/linstor/spacetracking/AggregateCapacityInfo.java
+++ controller/src/main/java/com/linbit/linstor/spacetracking/AggregateCapacityInfo.java
@@ -3,8 +3,9 @@ package com.linbit.linstor.spacetracking;
 import com.linbit.linstor.core.identifier.NodeName;
 
 import java.math.BigInteger;
+import java.util.Objects;
 
-public class AggregateCapacityInfo
+public class AggregateCapacityInfo implements Comparable<AggregateCapacityInfo>
 {
     public final NodeName nodeName;
 
@@ -16,5 +17,55 @@ public class AggregateCapacityInfo
     public AggregateCapacityInfo(NodeName nodeNameRef)
     {
         nodeName = nodeNameRef;
+    }
+
+    @Override
+    public int compareTo(AggregateCapacityInfo oRef)
+    {
+        int cmp;
+        if (nodeName == null)
+        {
+            if (oRef.nodeName == null)
+            {
+                cmp = 0;
+            }
+            else
+            {
+                cmp = -1;
+            }
+        }
+        else
+        {
+            if (oRef.nodeName == null)
+            {
+                cmp = 1;
+            }
+            else
+            {
+                cmp = nodeName.compareTo(oRef.nodeName);
+            }
+        }
+        return cmp;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(nodeName);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (!(obj instanceof AggregateCapacityInfo))
+        {
+            return false;
+        }
+        AggregateCapacityInfo other = (AggregateCapacityInfo) obj;
+        return Objects.equals(nodeName, other.nodeName);
     }
 }
