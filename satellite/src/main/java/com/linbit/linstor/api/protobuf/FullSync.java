LINBIT/linstor-server

commit cb538c4647887e19f484b86234fa76c1274c7188

FetchThinSpaces: Do not send request to diskless nodes

satellite/src/main/java/com/linbit/linstor/core/devmgr/DeviceManagerImpl.java
@@ -2,6 +2,7 @@ package com.linbit.linstor.core.devmgr;
 
 import com.linbit.ImplementationError;
 import com.linbit.InvalidNameException;
+import com.linbit.PlatformStlt;
 import com.linbit.ServiceName;
 import com.linbit.SystemService;
 import com.linbit.SystemServiceStartException;
@@ -71,7 +72,6 @@ import com.linbit.linstor.transaction.manager.TransactionMgrUtil;
 import com.linbit.linstor.utils.layer.LayerVlmUtils;
 import com.linbit.locks.AtomicSyncPoint;
 import com.linbit.locks.SyncPoint;
-import com.linbit.PlatformStlt;
 import com.linbit.utils.Either;
 
 import static com.linbit.linstor.api.ApiConsts.MODIFIED;

controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/FreeCapacityFetcherProto.java
@@ -128,7 +128,7 @@ public class FreeCapacityFetcherProto implements FreeCapacityFetcher
     {
         return streamStorPools(node)
             .map(StorPool::getDeviceProviderKind)
-            .anyMatch(DeviceProviderKind::usesThinProvisioning);
+            .anyMatch(kind -> kind.usesThinProvisioning() && !DeviceProviderKind.DISKLESS.equals(kind));
     }
 
     private Flux<ByteArrayInputStream> prepareFreeSpaceApiCall(Node node)

commit 9d5595e2ab58def4f34164afa57c668e89626b3f

sp,l: Fix offline DfltDisklessStorPool
Commit cb538c4647887e19f484b86234fa76c1274c7188 introduced a useful
optimization, but unfortunately introduced a caching problem.

The controller stores possible warnings / issues within a StorPool object
so that every "sp l" can show properly a warning/error if necessary.
A warning as "the node is offline".

The problematic scenario can be reproduced as follows:

* Have a satellite up and running
* Shutdown the satellite
* IMPORTANT: query "sp l"
  This places the "warning"-report within the StorPool object that
  is returned to the client if asked
* Start the satellite and wait until it is online again
* Query "sp l" again

Before this patch, nothing cleared / updated the DfltDisklessStorPool's
reports cache, resulting in the false positive warning that the node
is offline.

This patch fixes this issue by simply including the "fetched free spaces"
from the DfltDisklessStorPool as well (instead of just the thick-SPs as
previously). This goes a bit against the commit mentioned in the beginning,
this this fetching is only done during the FullSync, not during every
"FetchThinFreeSpaces" call.
The trick is, that if the satellite reports the "free space" of the
DfltDisklessStorPool, processing the report also clears the cached
report so that they can be replaced with the new reports (which do not
exist / are null, since the satellite is up and running and online
and everything is fine).

satellite/src/main/java/com/linbit/linstor/api/protobuf/FullSync.java
@@ -137,7 +137,7 @@ public class FullSync implements ApiCall
             }
 
             Map<StorPool, Either<SpaceInfo, ApiRcException>> spaceInfoQueryMap =
-                apiCallHandlerUtils.getAllSpaceInfo(false);
+                apiCallHandlerUtils.getAllSpaceInfo();
 
             for (Entry<StorPool, Either<SpaceInfo, ApiRcException>> entry : spaceInfoQueryMap.entrySet())
             {

satellite/src/main/java/com/linbit/linstor/api/protobuf/ReqFreeSpace.java
@@ -65,7 +65,7 @@ public class ReqFreeSpace implements ApiCallReactive
     private Flux<byte[]> executeInScope()
         throws IOException
     {
-        Map<StorPool, Either<SpaceInfo, ApiRcException>> freeSpaceMap = apiCallHandlerUtils.getAllSpaceInfo(true);
+        Map<StorPool, Either<SpaceInfo, ApiRcException>> freeSpaceMap = apiCallHandlerUtils.getSpaceInfo(true);
 
         MsgIntFreeSpace.Builder builder = MsgIntFreeSpace.newBuilder();
         for (Map.Entry<StorPool, Either<SpaceInfo, ApiRcException>> entry : freeSpaceMap.entrySet())

satellite/src/main/java/com/linbit/linstor/core/apicallhandler/StltApiCallHandlerUtils.java
@@ -52,6 +52,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.function.Predicate;
 
 import static java.util.stream.Collectors.toList;
 
@@ -267,7 +268,17 @@ public class StltApiCallHandlerUtils
         return allocatedMap;
     }
 
-    public Map<StorPool, Either<SpaceInfo, ApiRcException>> getAllSpaceInfo(boolean thin)
+    public Map<StorPool, Either<SpaceInfo, ApiRcException>> getAllSpaceInfo()
+    {
+        return getSpaceInfo(ignored -> true);
+    }
+
+    public Map<StorPool, Either<SpaceInfo, ApiRcException>> getSpaceInfo(boolean thin)
+    {
+        return getSpaceInfo(storPool -> storPool.getDeviceProviderKind().usesThinProvisioning() == thin);
+    }
+
+    public Map<StorPool, Either<SpaceInfo, ApiRcException>> getSpaceInfo(Predicate<StorPool> shouldIncludeSpTestRef)
     {
         Map<StorPool, Either<SpaceInfo, ApiRcException>> spaceMap = new HashMap<>();
 
@@ -281,7 +292,7 @@ public class StltApiCallHandlerUtils
 
             for (StorPool storPool : controllerPeerConnector.getLocalNode().streamStorPools(apiCtx).collect(toList()))
             {
-                if (storPool.getDeviceProviderKind().usesThinProvisioning() == thin)
+                if (shouldIncludeSpTestRef.test(storPool))
                 {
                     spaceMap.put(storPool, getStoragePoolSpaceInfoOrError(storPool));
                 }

satellite/src/main/java/com/linbit/linstor/core/devmgr/DeviceManagerImpl.java
@@ -1941,7 +1941,7 @@ class DeviceManagerImpl implements Runnable, SystemService, DeviceManager, Devic
         if (ctrlPeer != null)
         {
             Map<StorPool, Either<SpaceInfo, ApiRcException>> spaceInfoQueryMap =
-                apiCallHandlerUtils.getAllSpaceInfo(false);
+                apiCallHandlerUtils.getSpaceInfo(false);
 
             Map<StorPool, SpaceInfo> spaceInfoMap = new TreeMap<>();
 