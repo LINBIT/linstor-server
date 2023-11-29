4fb77a14cec1d8d3a86b6151a544137348ac4407
Gabor Hernadi <gabor.hernadi@linbit.com>
2023-11-29 15:53:18 +0100

    OpenFlex: delete leftover interfaces

--- server/src/main/java/com/linbit/linstor/storage/interfaces/layers/nvme/OpenflexRscObject.java
+++
@@ -1,9 +0,0 @@
-package com.linbit.linstor.storage.interfaces.layers.nvme;
-
-import com.linbit.linstor.core.objects.AbsResource;
-import com.linbit.linstor.storage.interfaces.categories.resource.AbsRscLayerObject;
-
-public interface OpenflexRscObject<RSC extends AbsResource<RSC>>
-    extends AbsRscLayerObject<RSC>
-{
-}
