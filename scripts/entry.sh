f5bcaed6a74ac150f4beb59ad46e5e89e784aa74
Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>
2023-12-07 16:08:23 +0100

    docker: exec LINSTOR commands
    Otherwise, the outer shell script may prevent swift stopping of LINSTOR
    by eating a SIGINT or similar.

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -57,11 +57,11 @@ fi
 case $1 in
 	startSatellite)
 		shift
-		/usr/share/linstor-server/bin/Satellite --logs=/var/log/linstor-satellite --config-directory=/etc/linstor --skip-hostname-check "$@"
+		exec /usr/share/linstor-server/bin/Satellite --logs=/var/log/linstor-satellite --config-directory=/etc/linstor --skip-hostname-check "$@"
 		;;
 	startController)
 		shift
-		/usr/share/linstor-server/bin/Controller --logs=/var/log/linstor-controller --config-directory=/etc/linstor "$@"
+		exec /usr/share/linstor-server/bin/Controller --logs=/var/log/linstor-controller --config-directory=/etc/linstor "$@"
 		;;
 	runMigration)
 		shift
