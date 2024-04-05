7c3ba5128d36e5d7315b7dbc5c53ed93518ee735
Rene Peinthor <rene.peinthor@linbit.com>
2024-04-05 12:33:03 +0200

    debian: include #DEBHELPER# token in postinst script
    this token gets replaced by debian generated code

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,7 +6,6 @@ DEF_LOCATION="/usr/share/linstor-server"
 DEF_VARLIB_LINSTOR="/var/lib/linstor"
 DEF_DB="${DEF_VARLIB_LINSTOR}/linstordb"
 DEF_LINSTOR_CFG_DIR="/etc/linstor"
-DEF_DB_CFG="${DEF_LINSTOR_CFG_DIR}/database.cfg"
 DEF_LINSTOR_CFG="${DEF_LINSTOR_CFG_DIR}/linstor.toml"
 DEF_DB_TYPE="h2"
 
@@ -17,12 +16,9 @@ DEF_DB_TYPE="h2"
 CURRENT_DB=${DEF_DB}.mv.db
 [ -f "$CURRENT_DB" ] && cp "$CURRENT_DB" "${CURRENT_DB}-$(date --iso-8601=minutes).bak"
 
-if [ -f ${DEF_DB_CFG} ]; then
-    ${DEF_LOCATION}/bin/linstor-config migrate-database-config ${DEF_DB_CFG} ${DEF_LINSTOR_CFG}
-    mv ${DEF_DB_CFG} "${DEF_DB_CFG}.bak"
-    exit 0;
-fi
+[ ! -f ${DEF_LINSTOR_CFG} ] && ${DEF_LOCATION}/bin/linstor-config create-db-file --dbtype=${DEF_DB_TYPE} ${DEF_DB} > ${DEF_LINSTOR_CFG}
 
-[ -f ${DEF_LINSTOR_CFG} ] && { echo "Linstor config toml file already exists, exiting"; exit 0; }
+# DEBHELPER will be replaced by debian build system, adding systemd helper scripts and so on...
+#DEBHELPER#
 
-${DEF_LOCATION}/bin/linstor-config create-db-file --dbtype=${DEF_DB_TYPE} ${DEF_DB} > ${DEF_LINSTOR_CFG}
+exit 0
