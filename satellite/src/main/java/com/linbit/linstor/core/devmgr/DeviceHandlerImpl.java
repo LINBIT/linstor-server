8943db7af3a72e71d68b973beb360bc199c420d6
Johanna Kucera <johanna.kucera@linbit.com>
2025-06-27 10:26:05 +0200

    LayerIgnoreReason: fix problems with ignore-reason and skip-disk
    Removed skipping layers if the ignore-reason didn't change, since
    it doesn't really bring much benefit but makes some special cases
    (such as re-creating part of the layer-tree during toggle disk)
    more complicated.
    
    Set the exists-boolean of the vlmData to false after trying and
    failing to remove a vlm because skipDisk is set.

--- satellite/src/main/java/com/linbit/linstor/core/devmgr/DeviceHandlerImpl.java
+++ satellite/src/main/java/com/linbit/linstor/core/devmgr/DeviceHandlerImpl.java
@@ -702,7 +702,7 @@ public class DeviceHandlerImpl implements DeviceHandler
         throws ImplementationError
     {
         VlmProviderObject<Resource> vlmData = rscLayerObjectRef.getVlmProviderObject(volumeNumberRef);
-        if (vlmData.exists())
+        if (!vlmData.getRscLayerObject().hasAnyPreventExecutionWhenDeletingReason() && vlmData.exists())
         {
             throw new ImplementationError("Layer '" + rscLayerObjectRef.getLayerKind() + " did not delete the volume " +
                 volumeNumberRef + " of resource " + rscLayerObjectRef.getSuffixedResourceName() + " properly");
@@ -1429,6 +1429,10 @@ public class DeviceHandlerImpl implements DeviceHandler
                     "Suppressing exception due to ignore reasons. Exception message: %s",
                     exc.getMessage()
                 );
+                for (VlmProviderObject<RSC> vlmData : rscLayerData.getVlmLayerObjects().values())
+                {
+                    vlmData.setExists(false);
+                }
             }
         }
         /*
