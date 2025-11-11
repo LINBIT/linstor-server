00d29a15803845f56209e0686afa34693be88147
Rene Peinthor <rene.peinthor@linbit.com>
2025-11-11 07:48:40 +0100

    bump rest api version 1.27.0 for 1.33.0-rc.1

--- controller/src/main/java/com/linbit/linstor/api/rest/v1/serializer/JsonGenTypes.java
+++ controller/src/main/java/com/linbit/linstor/api/rest/v1/serializer/JsonGenTypes.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 public class JsonGenTypes
 {
-    public static final String REST_API_VERSION = "1.26.1";
+    public static final String REST_API_VERSION = "1.27.0";
 
     /**
      * Common api reply structure
