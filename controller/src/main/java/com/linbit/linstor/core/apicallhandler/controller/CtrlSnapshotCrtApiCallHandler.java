LINBIT/linstor-server

commit 04aa2656e0905a0ae140c46f6eb7ee68b11dfcaa

Remove snapshot shipping.

controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/CtrlSnapshotCrtApiCallHandler.java
@@ -329,8 +329,7 @@ public class CtrlSnapshotCrtApiCallHandler
                 )
             );
             flux = Flux.<ApiCallRc>just(responses)
-                .concatWith(postCreateSnapshot(createSimpleSnapshotRequestPrivileged(snapDfn), true))
-                .concatWith(ctrlSnapshotDeleteApiCallHandler.cleanupOldAutoSnapshots(rscDfn));
+                .concatWith(postCreateSnapshot(createSimpleSnapshotRequestPrivileged(snapDfn), true));
         }
         return flux;
     }
@@ -590,7 +589,7 @@ public class CtrlSnapshotCrtApiCallHandler
         Flux<ApiCallRc> retFlux = Flux.empty();
 
         SnapshotDefinition snapshotDfn = ctrlApiDataLoader.loadSnapshotDfn(rscName, snapshotName, false);
-        // might be null when a snapshot(-shipping) is aborted multiple times
+        // might be null when a snapshot (or backup-shipping) is aborted multiple times
         // which can happen as both, sender and receiver try to abort a failed shipment
         if (snapshotDfn != null)
         {

controller/src/main/java/com/linbit/linstor/api/protobuf/internal/NotifySnapshotShippingReceived.java
@@ -1,45 +1,0 @@
-package com.linbit.linstor.api.protobuf.internal;
-
-import com.linbit.linstor.InternalApiConsts;
-import com.linbit.linstor.api.ApiCallReactive;
-import com.linbit.linstor.api.protobuf.ProtobufApiCall;
-import com.linbit.linstor.core.apicallhandler.controller.internal.SnapshotShippingInternalApiCallHandler;
-import com.linbit.linstor.proto.javainternal.s2c.MsgIntSnapshotShippedOuterClass.MsgIntSnapshotShipped;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import reactor.core.publisher.Flux;
-
-@Deprecated(forRemoval = true)
-@ProtobufApiCall(
-    name = InternalApiConsts.API_NOTIFY_SNAPSHOT_SHIPPING_RECEIVED,
-    description = "Called by the satellite to notify the controller that a snapshot has been received",
-    transactional = true
-)
-@Singleton
-public class NotifySnapshotShippingReceived implements ApiCallReactive
-{
-    private final SnapshotShippingInternalApiCallHandler snapShipIntCallHandler;
-
-    @Inject
-    public NotifySnapshotShippingReceived(SnapshotShippingInternalApiCallHandler snapShipIntCallHandlerRef)
-    {
-        snapShipIntCallHandler = snapShipIntCallHandlerRef;
-    }
-
-    @Override
-    public Flux<byte[]> executeReactive(InputStream msgDataInRef) throws IOException
-    {
-        MsgIntSnapshotShipped ship = MsgIntSnapshotShipped.parseDelimitedFrom(msgDataInRef);
-        return snapShipIntCallHandler.shippingReceived(
-            ship.getRscName(),
-            ship.getSnapName(),
-            ship.getSuccess(),
-            ship.getVlmNrsWithBlockedPortList()
-        ).thenMany(Flux.<byte[]>empty());
-    }
-}

controller/src/main/java/com/linbit/linstor/api/rest/v1/SnapshotShipping.java
@@ -1,81 +1,0 @@
-package com.linbit.linstor.api.rest.v1;
-
-import com.linbit.linstor.api.ApiCallRc;
-import com.linbit.linstor.api.ApiConsts;
-import com.linbit.linstor.api.rest.v1.serializer.JsonGenTypes;
-import com.linbit.linstor.api.rest.v1.utils.ApiCallRcRestUtils;
-import com.linbit.linstor.core.apicallhandler.controller.CtrlSnapshotShippingApiCallHandler;
-import com.linbit.linstor.logging.ErrorReporter;
-
-import javax.inject.Inject;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.container.AsyncResponse;
-import javax.ws.rs.container.Suspended;
-import javax.ws.rs.core.Context;
-
-import java.io.IOException;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.glassfish.grizzly.http.server.Request;
-import org.slf4j.MDC;
-import reactor.core.publisher.Flux;
-
-@Deprecated(forRemoval = true)
-@Path("v1/resource-definitions/{rscName}/snapshot-shipping")
-public class SnapshotShipping
-{
-    private final RequestHelper requestHelper;
-    private final CtrlSnapshotShippingApiCallHandler ctrlSnapshotShippingApiCallHandler;
-
-    private final ObjectMapper objectMapper;
-
-    @Inject
-    public SnapshotShipping(
-        RequestHelper requestHelperRef,
-        CtrlSnapshotShippingApiCallHandler ctrlSnapshotShippingApiCallHandlerRef
-    )
-    {
-        requestHelper = requestHelperRef;
-        ctrlSnapshotShippingApiCallHandler = ctrlSnapshotShippingApiCallHandlerRef;
-
-        objectMapper = new ObjectMapper();
-    }
-
-    @POST
-    public void shipSnapshot(
-        @Context Request request,
-        @Suspended final AsyncResponse asyncResponse,
-        @PathParam("rscName") String rscName,
-        String jsonData
-    )
-    {
-        try (var ignore = MDC.putCloseable(ErrorReporter.LOGID, ErrorReporter.getNewLogId()))
-        {
-            JsonGenTypes.SnapshotShipping snapShipData = objectMapper.readValue(
-                jsonData,
-                JsonGenTypes.SnapshotShipping.class
-            );
-            Flux<ApiCallRc> flux = ctrlSnapshotShippingApiCallHandler.shipSnapshot(
-                rscName,
-                snapShipData.from_node,
-                snapShipData.from_nic,
-                snapShipData.to_node,
-                snapShipData.to_nic,
-                false
-            );
-
-            requestHelper.doFlux(
-                ApiConsts.API_SHIP_SNAPSHOT,
-                request,
-                asyncResponse,
-                ApiCallRcRestUtils.mapToMonoResponse(flux)
-            );
-        }
-        catch (IOException ioExc)
-        {
-            ApiCallRcRestUtils.handleJsonParseException(ioExc, asyncResponse);
-        }
-    }
-}

controller/src/main/java/com/linbit/linstor/api/rest/v1/View.java
@@ -16,7 +16,6 @@ import com.linbit.linstor.core.apicallhandler.controller.helpers.ResourceList;
 import com.linbit.linstor.core.apicallhandler.controller.internal.CtrlBackupQueueInternalCallHandler;
 import com.linbit.linstor.core.apis.ResourceApi;
 import com.linbit.linstor.core.apis.SnapshotDefinitionListItemApi;
-import com.linbit.linstor.core.apis.SnapshotShippingListItemApi;
 import com.linbit.linstor.core.apis.StorPoolApi;
 import com.linbit.linstor.logging.ErrorReporter;
 
@@ -248,53 +247,6 @@ public class View
 
             response = RequestHelper.queryRequestResponse(
                 objectMapper, ApiConsts.FAIL_NOT_FOUND_SNAPSHOT, "Snapshot", null, snapshot
-            );
-
-            return response;
-        }, false);
-    }
-
-    @Deprecated(forRemoval = true)
-    @GET
-    @Path("snapshot-shippings")
-    public Response listSnapshotShippings(
-        @Context Request request,
-        @QueryParam("nodes") List<String> nodes,
-        @QueryParam("resources") List<String> resources,
-        @QueryParam("snapshots") List<String> snapshots,
-        @QueryParam("status") List<String> status,
-        @DefaultValue("0") @QueryParam("limit") int limit,
-        @DefaultValue("0") @QueryParam("offset") int offset
-    )
-    {
-        return requestHelper.doInScope(ApiConsts.API_LST_SNAPSHOT_SHIPPINGS, request, () ->
-        {
-            List<String> nodesFilter = nodes != null ? nodes : Collections.emptyList();
-            List<String> resourcesFilter = resources != null ?
-                resources.parallelStream().map(String::toLowerCase).collect(Collectors.toList()) :
-                Collections.emptyList();
-            List<String> statusFilter = status != null ? status : Collections.emptyList();
-
-            Response response;
-
-            Stream<SnapshotShippingListItemApi> snapsStream = ctrlApiCallHandler
-                .listSnapshotShippings(nodesFilter, resourcesFilter, snapshots, statusFilter).stream();
-
-            if (limit > 0)
-            {
-                snapsStream = snapsStream.skip(offset).limit(limit);
-            }
-
-            List<JsonGenTypes.SnapshotShippingStatus> snapshot = snapsStream
-                .map(Json::apiToSnapshotShipping)
-                .collect(Collectors.toList());
-
-            response = RequestHelper.queryRequestResponse(
-                objectMapper,
-                ApiConsts.FAIL_NOT_FOUND_SNAPSHOT,
-                "Snapshot shippments",
-                null,
-                snapshot
             );
 
             return response;

controller/src/main/java/com/linbit/linstor/api/rest/v1/serializer/Json.java
@@ -61,7 +61,6 @@ import com.linbit.linstor.core.apis.ResourceDefinitionApi;
 import com.linbit.linstor.core.apis.ResourceGroupApi;
 import com.linbit.linstor.core.apis.SnapshotApi;
 import com.linbit.linstor.core.apis.SnapshotDefinitionListItemApi;
-import com.linbit.linstor.core.apis.SnapshotShippingListItemApi;
 import com.linbit.linstor.core.apis.SnapshotVolumeApi;
 import com.linbit.linstor.core.apis.SnapshotVolumeDefinitionApi;
 import com.linbit.linstor.core.apis.StorPoolApi;
@@ -1096,18 +1095,6 @@ public class Json
         snapshot.props.putAll(snapshot.snapshot_definition_props);
 
         return snapshot;
-    }
-
-    public static JsonGenTypes.SnapshotShippingStatus apiToSnapshotShipping(
-        SnapshotShippingListItemApi snapshotShippingListItemApi
-    )
-    {
-        JsonGenTypes.SnapshotShippingStatus shippingStatus = new JsonGenTypes.SnapshotShippingStatus();
-        shippingStatus.snapshot = apiToSnapshot(snapshotShippingListItemApi);
-        shippingStatus.from_node_name = snapshotShippingListItemApi.getSourceNodeName();
-        shippingStatus.to_node_name = snapshotShippingListItemApi.getTargetNodeName();
-        shippingStatus.status = snapshotShippingListItemApi.getShippingStatus();
-        return shippingStatus;
     }
 
     public static JsonGenTypes.ResourceConnection apiToResourceConnection(

controller/src/main/java/com/linbit/linstor/api/rest/v1/serializer/JsonGenTypes.java
@@ -1250,15 +1250,6 @@ public class JsonGenTypes
         public List<String> delete_namespaces = Collections.emptyList();
     }
 
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    public static class SnapshotShippingStatus
-    {
-        public @Nullable Snapshot snapshot;
-        public @Nullable String from_node_name;
-        public @Nullable String to_node_name;
-        public @Nullable String status;
-    }
-
     /**
      * Objects holding one or multiple SnapshotVolumeNode objects for the given node
      */
@@ -1334,27 +1325,6 @@ public class JsonGenTypes
          */
         public List<String> nodes = Collections.emptyList();
         public Map<String, String> stor_pool_rename = Collections.emptyMap();
-    }
-
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    public static class SnapshotShipping
-    {
-        /**
-         * Node where to ship the snapshot from
-         */
-        public String from_node;
-        /**
-         * NetInterface of the source node
-         */
-        public @Nullable String from_nic;
-        /**
-         * Node where to ship the snapshot
-         */
-        public String to_node;
-        /**
-         * NetInterface of the destination node
-         */
-        public @Nullable String to_nic;
     }
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)

controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/CtrlApiCallHandler.java
@@ -23,7 +23,6 @@ import com.linbit.linstor.core.apis.ResourceConnectionApi;
 import com.linbit.linstor.core.apis.ResourceDefinitionApi;
 import com.linbit.linstor.core.apis.ResourceGroupApi;
 import com.linbit.linstor.core.apis.SnapshotDefinitionListItemApi;
-import com.linbit.linstor.core.apis.SnapshotShippingListItemApi;
 import com.linbit.linstor.core.apis.StorPoolDefinitionApi;
 import com.linbit.linstor.core.apis.VolumeDefinitionWithCreationPayload;
 import com.linbit.linstor.core.apis.VolumeGroupApi;
@@ -91,7 +90,6 @@ public class CtrlApiCallHandler
     private final CtrlNetIfApiCallHandler netIfApiCallHandler;
     private final CtrlWatchApiCallHandler watchApiCallHandler;
     private final CtrlSnapshotApiCallHandler snapshotApiCallHandler;
-    private final CtrlSnapshotShippingApiCallHandler snapshotShippingApiCallHandler;
     private final CtrlSnapshotRestoreVlmDfnApiCallHandler snapshotRestoreVlmDfnApiCallHandler;
     private final CtrlDrbdProxyModifyApiCallHandler drbdProxyModifyApiCallHandler;
     private final CtrlKvsApiCallHandler kvsApiCallHandler;
@@ -122,7 +120,6 @@ public class CtrlApiCallHandler
         CtrlNetIfApiCallHandler netIfApiCallHandlerRef,
         CtrlWatchApiCallHandler watchApiCallHandlerRef,
         CtrlSnapshotApiCallHandler snapshotApiCallHandlerRef,
-        CtrlSnapshotShippingApiCallHandler snapshotShippingApiCallHandlerRef,
         CtrlSnapshotRestoreVlmDfnApiCallHandler snapshotRestoreVlmDfnApiCallHandlerRef,
         CtrlDrbdProxyModifyApiCallHandler drbdProxyModifyApiCallHandlerRef,
         CtrlKvsApiCallHandler kvsApiCallHandlerRef,
@@ -151,7 +148,6 @@ public class CtrlApiCallHandler
         netIfApiCallHandler = netIfApiCallHandlerRef;
         watchApiCallHandler = watchApiCallHandlerRef;
         snapshotApiCallHandler = snapshotApiCallHandlerRef;
-        snapshotShippingApiCallHandler = snapshotShippingApiCallHandlerRef;
         snapshotRestoreVlmDfnApiCallHandler = snapshotRestoreVlmDfnApiCallHandlerRef;
         drbdProxyModifyApiCallHandler = drbdProxyModifyApiCallHandlerRef;
         kvsApiCallHandler = kvsApiCallHandlerRef;
@@ -1055,48 +1051,6 @@ public class CtrlApiCallHandler
                 nodeNames, resourceNames);
         }
         return listSnapshotDefinitions;
-    }
-
-    public ArrayList<SnapshotShippingListItemApi> listSnapshotShippings(
-        List<String> nodeNames,
-        List<String> resourceNames,
-        List<String> snapshotNames,
-        List<String> status
-    )
-    {
-        ArrayList<SnapshotShippingListItemApi> listSnapshotShippings;
-        try (LockGuard lg = lockGuardFactory.build(READ, RSC_DFN_MAP))
-        {
-            listSnapshotShippings = snapshotShippingApiCallHandler.listSnapshotShippings(
-                nodeNames,
-                resourceNames,
-                snapshotNames,
-                status
-            );
-        }
-        return listSnapshotShippings;
-    }
-
-    public Flux<ApiCallRc> modifySnapshotDefinition(
-        String rscNameStr,
-        String snapshotNameStr,
-        Map<String, String> overrideProps,
-        Set<String> deletePropKeys,
-        Set<String> deletePropNamespaces
-    )
-    {
-        Flux<ApiCallRc> flux;
-        try (LockGuard lg = lockGuardFactory.build(WRITE, RSC_DFN_MAP))
-        {
-            flux = snapshotApiCallHandler.modify(
-                rscNameStr,
-                snapshotNameStr,
-                overrideProps,
-                deletePropKeys,
-                deletePropNamespaces
-            );
-        }
-        return flux;
     }
 
     /**

controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/CtrlBackupShippingAbortHandler.java
@@ -15,11 +15,7 @@ import com.linbit.linstor.core.apicallhandler.ScopeRunner;
 import com.linbit.linstor.core.apicallhandler.response.ApiDatabaseException;
 import com.linbit.linstor.core.apicallhandler.response.ApiRcException;
 import com.linbit.linstor.core.identifier.RemoteName;
-import com.linbit.linstor.core.identifier.ResourceName;
 import com.linbit.linstor.core.objects.Node;
-import com.linbit.linstor.core.objects.Resource;
-import com.linbit.linstor.core.objects.ResourceConnection;
-import com.linbit.linstor.core.objects.ResourceDefinition;
 import com.linbit.linstor.core.objects.Snapshot;
 import com.linbit.linstor.core.objects.SnapshotDefinition;
 import com.linbit.linstor.core.objects.remotes.S3Remote;
@@ -35,7 +31,6 @@ import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -44,7 +39,7 @@ import com.amazonaws.services.s3.model.AmazonS3Exception;
 import reactor.core.publisher.Flux;
 
 @Singleton
-public class CtrlSnapshotShippingAbortHandler
+public class CtrlBackupShippingAbortHandler
 {
     private final AccessContext apiCtx;
     private final ScopeRunner scopeRunner;
@@ -59,7 +54,7 @@ public class CtrlSnapshotShippingAbortHandler
     private final Provider<CtrlRemoteApiCallHandler> ctrlRemoteApiCallHandler;
 
     @Inject
-    public CtrlSnapshotShippingAbortHandler(
+    public CtrlBackupShippingAbortHandler(
         @ApiContext AccessContext apiCtxRef,
         ScopeRunner scopeRunnerRef,
         LockGuardFactory lockguardFactoryRef,
@@ -125,51 +120,6 @@ public class CtrlSnapshotShippingAbortHandler
         return flux;
     }
 
-    public Flux<ApiCallRc> abortSnapshotShippingPrivileged(ResourceDefinition rscDfn)
-    {
-        return scopeRunner
-            .fluxInTransactionalScope(
-                "Abort snapshot shipments of rscDfn",
-                lockGuardFactory.create()
-                    .read(LockObj.NODES_MAP)
-                    .write(LockObj.RSC_DFN_MAP)
-                    .buildDeferred(),
-                () -> abortSnapshotShippingPrivilegedInTransaction(rscDfn)
-            );
-    }
-
-    private Flux<ApiCallRc> abortSnapshotShippingPrivilegedInTransaction(ResourceDefinition rscDfn)
-    {
-        Flux<ApiCallRc> flux = Flux.empty();
-        try
-        {
-
-            for (SnapshotDefinition snapDfn : rscDfn.getSnapshotDfns(apiCtx))
-            {
-                if (
-                    snapDfn.getFlags().isSet(apiCtx, SnapshotDefinition.Flags.SHIPPING) &&
-                        !snapDfn.getFlags().isSet(apiCtx, SnapshotDefinition.Flags.BACKUP)
-                )
-                {
-                    flux = flux.concatWith(
-                        snapDelHandlerProvider.get()
-                            .deleteSnapshot(
-                                snapDfn.getResourceName(),
-                                snapDfn.getName(),
-                                null
-                            )
-                    );
-                }
-            }
-            ctrlTransactionHelper.commit();
-        }
-        catch (AccessDeniedException exc)
-        {
-            throw new ImplementationError(exc);
-        }
-        return flux;
-    }
-
     public Flux<ApiCallRc> abortBackupShippingPrivileged(SnapshotDefinition snapDfn, boolean abortMultiPartRef)
     {
         return scopeRunner
@@ -209,48 +159,6 @@ public class CtrlSnapshotShippingAbortHandler
             throw new ImplementationError(exc);
         }
         return flux;
-    }
-
-    public void markSnapshotShippingAborted(SnapshotDefinition snapDfnRef)
-    {
-        try
-        {
-            snapDfnRef.getFlags().disableFlags(
-                apiCtx,
-                SnapshotDefinition.Flags.SHIPPING,
-                SnapshotDefinition.Flags.SHIPPING_CLEANUP,
-                SnapshotDefinition.Flags.SHIPPED
-            );
-            snapDfnRef.getFlags().enableFlags(apiCtx, SnapshotDefinition.Flags.SHIPPING_ABORT);
-            ResourceName rscName = snapDfnRef.getResourceName();
-
-            Collection<Snapshot> snapshots = snapDfnRef.getAllSnapshots(apiCtx);
-            for (Snapshot snap : snapshots)
-            {
-                Resource rsc1 = snap.getNode().getResource(apiCtx, rscName);
-                for (Snapshot snap2 : snapshots)
-                {
-                    if (snap != snap2)
-                    {
-                        Resource rsc2 = snap2.getNode().getResource(apiCtx, rscName);
-
-                        ResourceConnection rscConn = rsc1.getAbsResourceConnection(apiCtx, rsc2);
-                        if (rscConn != null)
-                        {
-                            rscConn.setSnapshotShippingNameInProgress(null);
-                        }
-                    }
-                }
-            }
-        }
-        catch (AccessDeniedException exc)
-        {
-            throw new ImplementationError(exc);
-        }
-        catch (DatabaseException exc)
-        {
-            throw new ApiDatabaseException(exc);
-        }
     }
 
     private Flux<ApiCallRc> abortBackupShippings(SnapshotDefinition snapDfn, boolean abortMultiPartRef)
@@ -370,5 +278,4 @@ public class CtrlSnapshotShippingAbortHandler
             throw new ApiDatabaseException(exc);
         }
     }
-
 }

commit 8970d7006fb815d4b951216837fd0bb73d5796ab

AutoSnap: Fix 'Keep' prop
Regression introduced in 04aa2656e0905a0ae140c46f6eb7ee68b11dfcaa.

controller/src/main/java/com/linbit/linstor/core/apicallhandler/controller/CtrlSnapshotCrtApiCallHandler.java
@@ -333,7 +333,8 @@ public class CtrlSnapshotCrtApiCallHandler
                 )
             );
             flux = Flux.<ApiCallRc>just(responses)
-                .concatWith(postCreateSnapshot(createSimpleSnapshotRequestPrivileged(snapDfn), true));
+                .concatWith(postCreateSnapshot(createSimpleSnapshotRequestPrivileged(snapDfn), true))
+                .concatWith(ctrlSnapshotDeleteApiCallHandler.cleanupOldAutoSnapshots(rscDfn));
         }
         return flux;
     }